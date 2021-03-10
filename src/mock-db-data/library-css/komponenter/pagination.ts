1bfaf16474e4d281bddd430cc062889bbd657ec0
Bernt Andersen <bernt.andersen@fhi.no>
2021-03-10 14:45:27 +0100

    Update pagination-example

--- src/mock-db-data/library-css/komponenter/pagination.ts
+++ src/mock-db-data/library-css/komponenter/pagination.ts
@@ -8,11 +8,21 @@ export const Pagination: LibraryExample[] = [{
   exampleHtml: `
 <nav aria-label="Page navigation example">
   <ul class="pagination">
-    <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item disabled">
+      <span class="page-link">
+        <span aria-hidden="true">&laquo;</span>
+      </span>
+    </li>
+    <li class="page-item active" aria-current="page">
+      <span class="page-link">1</span>
+    </li>
     <li class="page-item"><a class="page-link" href="#">2</a></li>
     <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item"><a class="page-link" href="#">Next</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#" aria-label="Neste">
+        <span aria-hidden="true">&raquo;</span>
+      </a>
+    </li>
   </ul>
 </nav>
 `,
