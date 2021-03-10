8a75e48cb335b58857bc9ceab71e5ad914f3f7ef
Bernt Andersen <bernt@proand.no>
2021-03-10 13:44:54 +0100

    Feature/add checkbox and radio examples (#11)

---
+++ src/mock-db-data/library-css/komponenter/form-checkbox.ts
@@ -0,0 +1,15 @@
+import { LibraryExample } from 'src/app/shared/models/library-example.model';
+import { LibraryMenuLevel2Categories } from 'src/app/_felles/konstanter/library-menu-level-2-categories';
+
+const categoryNames = LibraryMenuLevel2Categories.cssKomponentCategories;
+
+export const FormCheckbox: LibraryExample[] = [{
+  title: 'Checkbox',
+  exampleHtml: `
+<div class="custom-control custom-checkbox">
+  <input type="checkbox" class="custom-control-input" id="customCheck1">
+  <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
+</div>
+`,
+  category: categoryNames.skjema
+}];
---
+++ src/mock-db-data/library-css/komponenter/form-radio.ts
@@ -0,0 +1,28 @@
+import { LibraryExample } from 'src/app/shared/models/library-example.model';
+import { LibraryMenuLevel2Categories } from 'src/app/_felles/konstanter/library-menu-level-2-categories';
+
+const categoryNames = LibraryMenuLevel2Categories.cssKomponentCategories;
+
+export const FormRadio: LibraryExample[] = [{
+  title: 'Radioknapp',
+  exampleHtml: `
+<div class="custom-control custom-radio">
+  <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
+  <label class="custom-control-label" for="customRadio1">Toggle this custom radio</label>
+</div>
+<div class="custom-control custom-radio">
+  <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
+  <label class="custom-control-label" for="customRadio2">Or toggle this other custom radio</label>
+</div>
+<br>
+<div class="custom-control custom-radio custom-control-inline">
+  <input type="radio" id="customRadioInline1" name="customRadioInline1" class="custom-control-input">
+  <label class="custom-control-label" for="customRadioInline1">Toggle this custom radio</label>
+</div>
+<div class="custom-control custom-radio custom-control-inline">
+  <input type="radio" id="customRadioInline2" name="customRadioInline1" class="custom-control-input">
+  <label class="custom-control-label" for="customRadioInline2">Or toggle this other custom radio</label>
+</div>
+`,
+  category: categoryNames.skjema
+}];
