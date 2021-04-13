c8223aff756567e8e52efd3d026822ea59076253
Bernt Andersen <bernt@proand.no>
2021-04-13 11:12:15 +0200

    Feature/remove angular stuff (#16)
    * Add info about Angular-bibliotek
    
    * Temporarily remove FhiBtnShortcut (until it can be implementet without font awesome)

--- src/mock-db-data/library-css/css-komponenter.data.ts
+++ src/mock-db-data/library-css/css-komponenter.data.ts
@@ -3,7 +3,7 @@ import { LibraryExample } from 'src/app/shared/models/library-example.model';
 import { FhiBtnPrimary } from './komponenter/fhi-btn-primary';
 import { FhiBtnSecondary } from './komponenter/fhi-btn-secondary';
 import { FhiBtnFlat } from './komponenter/fhi-btn-flat';
-import { FhiBtnShortcut } from './komponenter/fhi-btn-shorcut';
+// import { FhiBtnShortcut } from './komponenter/fhi-btn-shorcut';
 import { FormInput } from './komponenter/form-input';
 import { FormTextarea } from './komponenter/form-textarea';
 import { FormRadio } from './komponenter/form-radio';
@@ -16,7 +16,7 @@ export const CssKomponenterData: LibraryExample[] = [
   ...FhiBtnPrimary,
   ...FhiBtnSecondary,
   ...FhiBtnFlat,
-  ...FhiBtnShortcut,
+  // ...FhiBtnShortcut,
   ...FormCheckbox,
   ...FormRadio,
   ...FormInput,
