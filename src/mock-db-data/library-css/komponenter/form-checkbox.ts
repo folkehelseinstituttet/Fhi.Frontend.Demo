7f6c22d0374a5ed3e88583a1427f47ca0cef10a1
Karl Granli <karl@gran.li>
2021-10-01 14:14:15 +0200

    Updated checkbox and radio

--- src/mock-db-data/library-css/komponenter/form-checkbox.ts
+++ src/mock-db-data/library-css/komponenter/form-checkbox.ts
@@ -7,27 +7,10 @@ export const FormCheckbox: LibraryExample[] = [{
   title: 'Checkbox',
   exampleHtml: `
 <div class="form-check">
-  <input class="form-check-input" type="checkbox" value="" id="customCheck1">
-  <label class="form-check-label" for="customCheck1">
+  <input class="form-check-input" type="checkbox" value="" id="customCheck">
+  <label class="form-check-label" for="customCheck">
     Check this custom checkbox
   </label>
-</div>
-
-<div class="form-check">
-  <input class="form-check-input" type="checkbox" value="" id="customCheck2" checked>
-  <label class="form-check-label" for="customCheck2">
-    Checked custom checkbox
-  </label>
-</div>
-
-<div class="custom-control custom-checkbox">
-  <input type="checkbox" class="custom-control-input" id="customCheck">
-  <label class="custom-control-label" for="customCheck">Check this custom checkbox</label>
-</div>
-
-<div class="custom-control custom-checkbox">
-  <input type="checkbox" class="custom-control-input" id="customCheck" checked>
-  <label class="custom-control-label" for="customCheck">Check this custom checkbox</label>
 </div>
 `,
   category: categoryNames.skjema
--- src/mock-db-data/library-css/komponenter/form-radio.ts
+++ src/mock-db-data/library-css/komponenter/form-radio.ts
@@ -6,32 +6,23 @@ const categoryNames = LibraryMenuLevel2Categories.cssKomponentCategories;
 export const FormRadio: LibraryExample[] = [{
   title: 'Radioknapp',
   exampleHtml: `
-<div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
-  <label class="form-check-label" for="flexRadioDefault1">Default radio</label>
+<div class="form-check mb-1">
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="customRadio1">
+  <label class="form-check-label" for="customRadio1">Default radio</label>
 </div>
 <div class="form-check">
-  <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked>
-  <label class="form-check-label" for="flexRadioDefault2">Default checked radio</label>
-</div>
-
-<div class="custom-control custom-radio mt-2">
-  <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
-  <label class="custom-control-label" for="customRadio1">Toggle this custom radio</label>
-</div>
-<div class="custom-control custom-radio">
-  <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input" checked>
-  <label class="custom-control-label" for="customRadio2">Or toggle this other custom radio</label>
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="customRadio2" checked>
+  <label class="form-check-label" for="customRadio2">Default checked radio</label>
 </div>
 `,
   exampleMarkdown: `
 <div class="form-check">
-  <input type="radio" id="customRadio1" name="customRadio" class="forrm-check-input">
-  <label class="form-check-label" for="customRadio1">Toggle this custom radio</label>
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="customRadio1">
+  <label class="form-check-label" for="customRadio1">Default radio</label>
 </div>
 <div class="form-check">
-  <input type="radio" id="customRadio2" name="customRadio" class="forrm-check-input">
-  <label class="form-check-label" for="customRadio2">Or toggle this other custom radio</label>
+  <input class="form-check-input" type="radio" name="flexRadioDefault" id="customRadio2" checked>
+  <label class="form-check-label" for="customRadio2">Default checked radio</label>
 </div>
 `,
   category: categoryNames.skjema
