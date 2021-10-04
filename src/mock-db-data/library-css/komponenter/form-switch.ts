4ba6e69704794be99cfe9c6dc14926dbb955c3d6
Karl Granli <karl@gran.li>
2021-10-04 15:58:07 +0200

    Switch toggler updated

--- src/mock-db-data/library-css/komponenter/form-switch.ts
+++ src/mock-db-data/library-css/komponenter/form-switch.ts
@@ -5,13 +5,14 @@ const categoryNames = LibraryMenuLevel2Categories.cssKomponentCategories;
 
 export const FormSwitch: LibraryExample[] = [{
   title: 'Switchknapp',
-  exampleHtml: `<div class="custom-control custom-switch">
-  <input type="checkbox" class="custom-control-input" id="customSwitch1">
-  <label class="custom-control-label" for="customSwitch1">Toggle this switch element</label>
+  exampleHtml: `
+<div class="form-check form-switch">
+  <input class="form-check-input visually-hidden" type="checkbox" id="flexSwitchCheckDefault">
+  <label class="form-check-label" for="flexSwitchCheckDefault">Toggle this switch element</label>
 </div>
-<div class="custom-control custom-switch">
-  <input type="checkbox" class="custom-control-input" disabled id="customSwitch2">
-  <label class="custom-control-label" for="customSwitch2">Disabled switch element</label>
+<div class="form-check form-switch">
+  <input class="form-check-input visually-hidden" type="checkbox" id="flexSwitchCheckChecked2" disabled>
+  <label class="form-check-label" for="flexSwitchCheckChecked2">Disabled switch element</label>
 </div>
 `,
   category: categoryNames.skjema
