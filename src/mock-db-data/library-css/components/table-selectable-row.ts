56d19b63627733c2feb4661189c6c8bcfaef1e90
Karl Granli <karl@gran.li>
2021-10-28 16:01:14 +0200

    Selectable rows component added

--- src/mock-db-data/library-css/components/table-selectable-row.ts
+++ src/mock-db-data/library-css/components/table-selectable-row.ts
@@ -1,7 +1,7 @@
 import { LibraryExample } from 'src/app/shared/models/library-example.model';
 import { LibraryMenuLevel2Categories } from 'src/app/_felles/konstanter/library-menu-level-2-categories';
 
-const categoryNames = LibraryMenuLevel2Categories.cssComponentCategories;
+const categoryNames = LibraryMenuLevel2Categories.cssKomponentCategories;
 
 export const TableSelectableRow: LibraryExample[] = [{
   title: 'Tabell - selekterbar rad',
@@ -229,5 +229,5 @@ exampleMarkdown: `
   </table>
 </div>
 `,
-  category: categoryNames.table
+  category: categoryNames.tabell
 }];
