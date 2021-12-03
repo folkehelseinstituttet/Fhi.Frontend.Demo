39c39bd17c14e6e2a96c1309d0409d347d4a884c
Karl Granli <karl@gran.li>
2021-12-03 10:18:12 +0100

    Renaming file

--- src/mock-db-data/library-css/css-icons.data.ts
+++ src/mock-db-data/library-css/css-icons.data.ts
@@ -1,6 +1,6 @@
 import { LibraryExample } from 'src/app/shared/models/library-example.model';
 
-import { IconsSet } from './icons/icons-set';
+import { IconsSet } from './icons/icon-set';
 
 export const CssIconsData: LibraryExample[] = [
   ...IconsSet
