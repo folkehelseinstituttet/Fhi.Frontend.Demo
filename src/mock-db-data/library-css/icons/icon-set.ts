a7f3293ebbb4641c51c63fec5a267a17f0b429b7
Karl Granli <karl@gran.li>
2021-12-06 13:46:55 +0100

    Commented out icons not yet available

--- src/mock-db-data/library-css/icons/icon-set.ts
+++ src/mock-db-data/library-css/icons/icon-set.ts
@@ -8,66 +8,67 @@ export const IconsSet: LibraryExample[] = [{
   title: 'Icon set',
   exampleHtml: `
 <div class="flex flex-wrap ds-icons-wrapper">
-  <i class="icon-alcohol-drugs" title="icon-alcohol-drugs"></i>
-  <i class="icon-bell-regular" title="icon-bell-regular"></i>
-  <i class="icon-cancer" title="icon-cancer"></i>
-  <i class="icon-cancer-cell" title="icon-cancer-cell"></i>
-  <i class="icon-cancer-cell-2" title="icon-cancer-cell-2"></i>
-  <i class="icon-cardio" title="icon-cardio"></i>
-  <i class="icon-cards" title="icon-cards"></i>
-  <i class="icon-check-circle-regular" title="icon-check-circle-regular"></i>
+  <!-- <i class="icon-alcohol-drugs" title="icon-alcohol-drugs"></i> -->
+  <!-- <i class="icon-bell-regular" title="icon-bell-regular"></i> -->
+  <!-- <i class="icon-cancer" title="icon-cancer"></i> -->
+  <!-- <i class="icon-cancer-cell" title="icon-cancer-cell"></i> -->
+  <!-- <i class="icon-cancer-cell-2" title="icon-cancer-cell-2"></i> -->
+  <!-- <i class="icon-cardio" title="icon-cardio"></i> -->
+  <!-- <i class="icon-cards" title="icon-cards"></i> -->
+  <!-- <i class="icon-check-circle-regular" title="icon-check-circle-regular"></i> -->
   <i class="icon-chevron-down" title="icon-chevron-down"></i>
   <i class="icon-chevron-down-fat" title="icon-chevron-down-fat"></i>
   <i class="icon-chevron-expand" title="icon-chevron-expand"></i>
   <i class="icon-chevron-up" title="icon-chevron-up"></i>
-  <i class="icon-circle-regular" title="icon-circle-regular"></i>
-  <i class="icon-clap" title="icon-clap"></i>
-  <i class="icon-description" title="icon-description"></i>
-  <i class="icon-diabetes" title="icon-diabetes"></i>
-  <i class="icon-ellipsis-v" title="icon-ellipsis-v"></i>
-  <i class="icon-envelope-regular" title="icon-envelope-regular"></i>
-  <i class="icon-environment" title="icon-environment"></i>
-  <i class="icon-file-excel-regular" title="icon-file-excel-regular"></i>
-  <i class="icon-insight" title="icon-insight"></i>
-  <i class="icon-muscle" title="icon-muscle"></i>
-  <i class="icon-no-handwash" title="icon-no-handwash"></i>
-  <i class="icon-population" title="icon-population"></i>
-  <i class="icon-question-circle-regular" title="icon-question-circle-regular"></i>
-  <i class="icon-suicide" title="icon-suicide"></i>
-  <i class="icon-swap" title="icon-swap"></i>
-  <i class="icon-trash-alt-regular" title="icon-trash-alt-regular"></i>
-  <i class="icon-user-regular" title="icon-user-regular"></i>
+  <!-- <i class="icon-circle-regular" title="icon-circle-regular"></i> -->
+  <!-- <i class="icon-clap" title="icon-clap"></i> -->
+  <!-- <i class="icon-description" title="icon-description"></i> -->
+  <!-- <i class="icon-diabetes" title="icon-diabetes"></i> -->
+  <!-- <i class="icon-ellipsis-v" title="icon-ellipsis-v"></i> -->
+  <!-- <i class="icon-envelope-regular" title="icon-envelope-regular"></i> -->
+  <!-- <i class="icon-environment" title="icon-environment"></i> -->
+  <!-- <i class="icon-file-excel-regular" title="icon-file-excel-regular"></i> -->
+  <!-- <i class="icon-insight" title="icon-insight"></i> -->
+  <!-- <i class="icon-muscle" title="icon-muscle"></i> -->
+  <!-- <i class="icon-no-handwash" title="icon-no-handwash"></i> -->
+  <!-- <i class="icon-population" title="icon-population"></i> -->
+  <!-- <i class="icon-question-circle-regular" title="icon-question-circle-regular"></i> -->
+  <!-- <i class="icon-suicide" title="icon-suicide"></i> -->
+  <!-- <i class="icon-swap" title="icon-swap"></i> -->
+  <!-- <i class="icon-trash-alt-regular" title="icon-trash-alt-regular"></i> -->
+  <!-- <i class="icon-user-regular" title="icon-user-regular"></i> -->
 </div>
 `,
   exampleMarkdown: `
-<i class="icon-alcohol-drugs"></i>
-<i class="icon-bell-regular"></i>
-<i class="icon-cancer"></i>
-<i class="icon-cancer-cell"></i>
-<i class="icon-cancer-cell-2"></i>
-<i class="icon-cardio"></i>
-<i class="icon-cards"></i>
-<i class="icon-check-circle-regular"></i>
+<!-- commented out icons will be available shortly -->
+<!-- <i class="icon-alcohol-drugs"></i> -->
+<!-- <i class="icon-bell-regular"></i> -->
+<!-- <i class="icon-cancer"></i> -->
+<!-- <i class="icon-cancer-cell"></i> -->
+<!-- <i class="icon-cancer-cell-2"></i> -->
+<!-- <i class="icon-cardio"></i> -->
+<!-- <i class="icon-cards"></i> -->
+<!-- <i class="icon-check-circle-regular"></i> -->
 <i class="icon-chevron-down"></i>
 <i class="icon-chevron-down-fat"></i>
 <i class="icon-chevron-expand"></i>
 <i class="icon-chevron-up"></i>
-<i class="icon-circle-regular"></i>
-<i class="icon-clap"></i>
-<i class="icon-description"></i>
-<i class="icon-diabetes"></i>
-<i class="icon-ellipsis-v"></i>
-<i class="icon-envelope-regular"></i>
-<i class="icon-environment"></i>
-<i class="icon-file-excel-regular"></i>
-<i class="icon-insight"></i>
-<i class="icon-muscle"></i>
-<i class="icon-no-handwash"></i>
-<i class="icon-population"></i>
-<i class="icon-question-circle-regular"></i>
-<i class="icon-suicide"></i>
-<i class="icon-swap"></i>
-<i class="icon-trash-alt-regular"></i>
-<i class="icon-user-regular"></i>
+<!-- <i class="icon-circle-regular"></i> -->
+<!-- <i class="icon-clap"></i> -->
+<!-- <i class="icon-description"></i> -->
+<!-- <i class="icon-diabetes"></i> -->
+<!-- <i class="icon-ellipsis-v"></i> -->
+<!-- <i class="icon-envelope-regular"></i> -->
+<!-- <i class="icon-environment"></i> -->
+<!-- <i class="icon-file-excel-regular"></i> -->
+<!-- <i class="icon-insight"></i> -->
+<!-- <i class="icon-muscle"></i> -->
+<!-- <i class="icon-no-handwash"></i> -->
+<!-- <i class="icon-population"></i> -->
+<!-- <i class="icon-question-circle-regular"></i> -->
+<!-- <i class="icon-suicide"></i> -->
+<!-- <i class="icon-swap"></i> -->
+<!-- <i class="icon-trash-alt-regular"></i> -->
+<!-- <i class="icon-user-regular"></i> -->
 `
 }];
