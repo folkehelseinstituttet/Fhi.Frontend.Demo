194fcba76704e7993f5a91f800e9f810cda45e7e
Karl Granli <karl@gran.li>
2021-12-14 11:14:53 +0100

    Update table-selectable-row.ts (#60)
    Translate example to english

--- src/mock-db-data/library-css/components/table-selectable-row.ts
+++ src/mock-db-data/library-css/components/table-selectable-row.ts
@@ -4,7 +4,7 @@ import { LibraryMenuLevel2Categories } from 'src/app/_common/constants/library-m
 const categoryNames = LibraryMenuLevel2Categories.cssComponentCategories;
 
 export const TableSelectableRow: LibraryExample[] = [{
-  title: 'Tabell - selekterbar rad',
+  title: 'Table - selectable row',
   exampleHtml: `
 <p>(Example is just showing visuals for current state, there's no toggle functionality here.)</p>
 
@@ -16,16 +16,16 @@ export const TableSelectableRow: LibraryExample[] = [{
           <input class="form-check-input" id="selectAllRows" type="checkbox" />
           <label for="selectAllRows" class="visually-hidden">Velg alle rader</label>
         </th>
-        <th scope="col" tabindex="0" aria-sort="ascending" aria-label="Sorter tabellen synkende basert på Fornavn">
-          <span class="text-decoration-underline">Fornavn</span>
+        <th scope="col" tabindex="0" aria-sort="ascending" aria-label="Sort the table descending based on First name">
+          <span class="text-decoration-underline">First name</span>
           <i class="icon-chevron-up"></i>
         </th>
-        <th scope="col" tabindex="0" aria-sort="none" aria-label="Sorter tabellen stigende basert på Etternavn">
-          <span class="text-decoration-underline">Etternavn</span>
+        <th scope="col" tabindex="0" aria-sort="none" aria-label="Sort the table ascending based on Last name">
+          <span class="text-decoration-underline">Last name</span>
           <i class="icon-chevron-expand"></i>
         </th>
-        <th scope="col" tabindex="0" aria-sort="none" aria-label="Sorter tabellen stigende basert på Rolle">
-          <span class="text-decoration-underline">Rolle</span>
+        <th scope="col" tabindex="0" aria-sort="none" aria-label="Sort the table ascending based on Role">
+          <span class="text-decoration-underline">Role</span>
           <i class="icon-chevron-expand"></i>
         </th>
       </tr>
@@ -35,7 +35,7 @@ export const TableSelectableRow: LibraryExample[] = [{
       <tr>
         <th>
           <input class="form-check-input" id="row1" type="checkbox" />
-          <label for="row1" class="visually-hidden">Velg denne raden</label>
+          <label for="row1" class="visually-hidden">Select this row</label>
         </th>
         <td>Elida</td>
         <td>Egge</td>
@@ -44,7 +44,7 @@ export const TableSelectableRow: LibraryExample[] = [{
       <tr aria-selected="true">
         <th>
           <input class="form-check-input" id="row2" type="checkbox" checked />
-          <label for="row2" class="visually-hidden">Velg denne raden</label>
+          <label for="row2" class="visually-hidden">Select this row</label>
         </th>
         <td>Elisabeth</td>
         <td>Kristiansen</td>
@@ -53,25 +53,25 @@ export const TableSelectableRow: LibraryExample[] = [{
       <tr aria-selected="true">
         <th>
           <input class="form-check-input" id="row3" type="checkbox" checked />
-          <label for="row3" class="visually-hidden">Velg denne raden</label>
+          <label for="row3" class="visually-hidden">Select this row</label>
         </th>
         <td>Henrik</td>
         <td>Olsen</td>
-        <td>Assistent</td>
+        <td>Assistant</td>
       </tr>
       <tr>
         <th>
           <input class="form-check-input" id="row4" type="checkbox" />
-          <label for="row4" class="visually-hidden">Velg denne raden</label>
+          <label for="row4" class="visually-hidden">Select this row</label>
         </th>
         <td>Marie</td>
         <td>Mul</td>
-        <td>Regissør</td>
+        <td>Director</td>
       </tr>
       <tr>
         <th>
           <input class="form-check-input" id="row5" type="checkbox" />
-          <label for="row5" class="visually-hidden">Velg denne raden</label>
+          <label for="row5" class="visually-hidden">Select this row</label>
         </th>
         <td>Sigurd</td>
         <td>Bråten</td>
@@ -80,20 +80,112 @@ export const TableSelectableRow: LibraryExample[] = [{
       <tr>
         <th>
           <input class="form-check-input" id="row5" type="checkbox" />
-          <label for="row5" class="visually-hidden">Velg denne raden</label>
+          <label for="row5" class="visually-hidden">Select this row</label>
         </th>
         <td>Tore</td>
         <td>Gudmundsen</td>
-        <td>Assistent</td>
+        <td>Assistant</td>
       </tr>
       <tr>
         <th>
           <input class="form-check-input" id="row5" type="checkbox" />
-          <label for="row5" class="visually-hidden">Velg denne raden</label>
+          <label for="row5" class="visually-hidden">Select this row</label>
         </th>
         <td>Vegard</td>
         <td>Bugge-Berntsen</td>
-        <td>Lys</td>
+        <td>Light</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+`,
+  exampleMarkdown: `
+<div class="table-responsive">
+  <table class="table table-striped" data-sort-column="2">
+    <thead>
+      <tr>
+        <th scope="col">
+          <input class="form-check-input" id="selectAllRows" type="checkbox" />
+          <label for="selectAllRows" class="visually-hidden">Velg alle rader</label>
+        </th>
+        <th scope="col" tabindex="0" aria-sort="ascending" aria-label="Sort the table descending based on First name">
+          <span class="text-decoration-underline">First name</span>
+          <i class="icon-chevron-up"></i>
+        </th>
+        <th scope="col" tabindex="0" aria-sort="none" aria-label="Sort the table ascending based on Last name">
+          <span class="text-decoration-underline">Last name</span>
+          <i class="icon-chevron-expand"></i>
+        </th>
+        <th scope="col" tabindex="0" aria-sort="none" aria-label="Sort the table ascending based on Role">
+          <span class="text-decoration-underline">Role</span>
+          <i class="icon-chevron-expand"></i>
+        </th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <tr>
+        <th>
+          <input class="form-check-input" id="row1" type="checkbox" />
+          <label for="row1" class="visually-hidden">Select this row</label>
+        </th>
+        <td>Elida</td>
+        <td>Egge</td>
+        <td>Producer</td>
+      </tr>
+      <tr aria-selected="true">
+        <th>
+          <input class="form-check-input" id="row2" type="checkbox" checked />
+          <label for="row2" class="visually-hidden">Select this row</label>
+        </th>
+        <td>Elisabeth</td>
+        <td>Kristiansen</td>
+        <td>Script</td>
+      </tr>
+      <tr aria-selected="true">
+        <th>
+          <input class="form-check-input" id="row3" type="checkbox" checked />
+          <label for="row3" class="visually-hidden">Select this row</label>
+        </th>
+        <td>Henrik</td>
+        <td>Olsen</td>
+        <td>Assistant</td>
+      </tr>
+      <tr>
+        <th>
+          <input class="form-check-input" id="row4" type="checkbox" />
+          <label for="row4" class="visually-hidden">Select this row</label>
+        </th>
+        <td>Marie</td>
+        <td>Mul</td>
+        <td>Director</td>
+      </tr>
+      <tr>
+        <th>
+          <input class="form-check-input" id="row5" type="checkbox" />
+          <label for="row5" class="visually-hidden">Select this row</label>
+        </th>
+        <td>Sigurd</td>
+        <td>Bråten</td>
+        <td>Designer</td>
+      </tr>
+      <tr>
+        <th>
+          <input class="form-check-input" id="row5" type="checkbox" />
+          <label for="row5" class="visually-hidden">Select this row</label>
+        </th>
+        <td>Tore</td>
+        <td>Gudmundsen</td>
+        <td>Assistant</td>
+      </tr>
+      <tr>
+        <th>
+          <input class="form-check-input" id="row5" type="checkbox" />
+          <label for="row5" class="visually-hidden">Select this row</label>
+        </th>
+        <td>Vegard</td>
+        <td>Bugge-Berntsen</td>
+        <td>Light</td>
       </tr>
     </tbody>
   </table>
