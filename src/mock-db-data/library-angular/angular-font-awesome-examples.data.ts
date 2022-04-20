d613e5821e676dfd59d79cef3ee56d36f75cbbf9
Karl Granli <karl@gran.li>
2022-04-20 11:02:55 +0200

    Removing more angular library stuff

--- src/mock-db-data/library-angular/angular-font-awesome-examples.data.ts
+++
@@ -1,7 +0,0 @@
-import { LibraryExample } from 'src/app/shared/models/library-example.model';
-
-import { FhiBtnShortcut } from './font-awesome-examples/fhi-btn-shortcut/fhi-btn-shortcut';
-
-export const AngularFontAwesomeExamplesData: LibraryExample[] = [
-  ...FhiBtnShortcut
-];
