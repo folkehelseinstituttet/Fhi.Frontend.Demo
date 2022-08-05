5243260689f3dc244301784bff4b7c13c6b2ae59
Karl Granli <karl@gran.li>
2022-08-05 13:17:52 +0200

    Update toasts.ts

--- src/mock-db-data/library-css/components/toasts.ts
+++ src/mock-db-data/library-css/components/toasts.ts
@@ -6,17 +6,17 @@ const categoryNames = LibraryMenuLevel2Categories.cssComponentCategories;
 export const Toast: LibraryExample[] = [{
   title: 'Toast',
   exampleHtml: `
-<div class="position-relative" style="border:2px solid #cfd0cb; background-color:#eee; height:500px;">
+<div class="position-relative" style="border:2px solid #cfd0cb; background-color:#fbfbfb; height:550px;">
     <div class="toast-container top-0 end-0 p-3">
 
         <div id="liveToast" class="toast show" role="alert" aria-live="assertive" aria-atomic="true">
             <div class="toast-header">
-                <strong class="me-auto">Regular toast</strong>
+                <strong class="me-auto">Generic toast</strong>
                 <small>Just now</small>
                 <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
             </div>
             <div class="toast-body">
-                Dette er en kort og konsis driftsmelding eller endringsinformasjon.
+                Generic, short informational message.
             </div>
         </div>
 
@@ -59,18 +59,26 @@ export const Toast: LibraryExample[] = [{
   exampleMarkdown: `
 <div class="toast-container top-0 end-0 p-3">
 
+    <!-- theme class might be added here [text-bg-success-light | text-bg-danger-light | text-bg-warning-light] -->
     <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
         <div class="toast-header">
-            <strong class="me-auto">Regular toast</strong>
+            <strong class="me-auto">Generic toast</strong>
             <small>Just now</small>
+
+            <!--  data-bs-dismiss="toast" if Bootstrap JS -->
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
         <div class="toast-body">
-            Dette er en kort og konsis driftsmelding eller endringsinformasjon.
+            Generic, short informational message.
         </div>
     </div>
 
 </div>
 `,
+documentationMarkdown: `
+<p>The toast is using Bootstrap 5 markup. For functionality you can either include Bootstrap JS to your project or write your own. Remember to include the <abbr title="accessibility">a11y</abbr> principles.</p>
+
+<p>Refer to Bootstrap documentation for details.<br>Google search: <a href="https://www.google.com/search?q=bootstrap+toasts+site%3Agetbootstrap.com">Bootstrap Toast</a></p>`
+,
   category: categoryNames.tools
 }];
