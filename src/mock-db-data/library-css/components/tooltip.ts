5e26fa0961b314fc9b867fa7e9d776bb0f9c226f
Karl Granli <karl@gran.li>
2022-08-15 12:56:13 +0200

    Tooltip example added (#95)
    * Tooltip example added

---
+++ src/mock-db-data/library-css/components/tooltip.ts
@@ -0,0 +1,33 @@
+import { LibraryExample } from 'src/app/shared/models/library-example.model';
+import { LibraryMenuLevel2Categories } from 'src/app/_common/constants/library-menu-level-2-categories';
+import { BootstrapBaseUrl, NgBootstrapBaseUrl } from 'src/app/library-css/constants/bootstrap-base-urls';
+import { LibraryItemType } from 'src/app/library-css/constants/library-item-type';
+
+const categoryNames = LibraryMenuLevel2Categories.cssComponentCategories;
+
+const itemTitle = 'Tooltip';
+
+export const Tooltip: LibraryExample[] = [{
+  title: itemTitle,
+  type: LibraryItemType.ngBootstrap,
+  codeMarkdown: `
+<button type="button" class="btn fhi-btn-outline-secondary" ngbTooltip="This is the tooltip.">
+  Hover for tooltip
+</button>
+
+`,
+  documentationHtml: `
+<p>
+  Bootstrap documentation for
+  <a href="${BootstrapBaseUrl}/${itemTitle.toLocaleLowerCase()}s">Tooltip</a>
+</p>
+<p>
+  The Tooltip is implemented as a
+  <a href="${NgBootstrapBaseUrl}/${itemTitle.toLocaleLowerCase()}">ngBootstrap Tooltip</a>
+  in the FHI Designsystem.
+</p>
+<p>
+  If you use another Javascript framework, you will have to find a suitable 3. party library.
+</p>`,
+  category: categoryNames.widgets
+}];
