d349540ff94e6102e895a857ea6daf6f5217c6c7
Bernt Andersen <bernt.andersen@fhi.no>
2022-08-16 16:22:16 +0200

    Add new url constants

--- src/app/_common/constants/url-paths.ts
+++ src/app/_common/constants/url-paths.ts
@@ -1,6 +1,15 @@
 export class UrlPaths {
   static developer = 'developer';
 
+  static colors = 'colors';
+  static typography = 'typography';
+  static icons = 'icons';
+  static components = 'components';
+  static modules = 'modules';
+  static pageTemplates = 'page-templates';
+
+
+  // deprecated
   static cssLibrary = 'css';
   static cssLibraryPageTemplates = 'page-templates';
   static cssLibraryModules = 'modules';
