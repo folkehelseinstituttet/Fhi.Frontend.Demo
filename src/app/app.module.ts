b112dae4d0b7a9c992ae29e023d8c8995b2aa3c9
Bernt Andersen <bernt.andersen@fhi.no>
2022-08-18 16:24:26 +0200

    Delete duplicate SpinnerComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,9 @@ import { SharedModule } from './shared/shared.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SpinnerComponent } from './views/shared/spinner/spinner.component';
 
 @NgModule({
-  declarations: [AppComponent, SpinnerComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
