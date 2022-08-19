d9c031ae03899ddd855d4ec467acf3ddf2a56055
Bernt Andersen <bernt.andersen@fhi.no>
2022-08-19 11:28:16 +0200

    Add imports and exports for dynamic examples

--- src/app/views/shared/dynamic-library-examples/dynamic-library-examples.module.ts
+++ src/app/views/shared/dynamic-library-examples/dynamic-library-examples.module.ts
@@ -4,16 +4,32 @@ import { SharedModule } from 'src/app/shared/shared.module';
 
 import { DynamicLibraryExampleComponent } from './dynamic-library-example/dynamic-library-example.component';
 
+import { AccordionExampleComponent } from './ng-bootstrap-examples/accordion-example/accordion-example.component';
+import { ToastExampleComponent } from './ng-bootstrap-examples/toast-example/toast-example.component';
+import { TooltipExampleComponent } from './ng-bootstrap-examples/tooltip-example/tooltip-example.component';
+
+const NGB_EXAMPLES = [
+  AccordionExampleComponent,
+  ToastExampleComponent,
+  TooltipExampleComponent
+];
+
+const FHI_EXAMPLES = [
+];
 
 @NgModule({
   declarations: [
-    DynamicLibraryExampleComponent
+    DynamicLibraryExampleComponent,
+    ...NGB_EXAMPLES,
+    ...FHI_EXAMPLES
   ],
   imports: [
-    SharedModule
+    SharedModule,
   ],
   exports: [
-    DynamicLibraryExampleComponent
+    DynamicLibraryExampleComponent,
+    ...NGB_EXAMPLES,
+    ...FHI_EXAMPLES
   ]
 })
 export class DynamicLibraryExamplesModule { }
