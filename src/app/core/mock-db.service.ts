a72b6fdd0013c14adcfc37b8e37698d8c4976c7d
Bernt Andersen <bernt.andersen@fhi.no>
2022-08-22 10:36:09 +0200

    Rename mock-db-data -> MOCK_DB_DATA

--- src/app/core/mock-db.service.ts
+++ src/app/core/mock-db.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
-import { CssPageTemplatesData } from 'src/mock-db-data/library-css/css-page-templates.data';
-import { CssModulesData } from 'src/mock-db-data/library-css/css-modules.data';
-import { CssComponentsData } from 'src/mock-db-data/library-css/css-components.data';
-import { CssColorsFontsData } from 'src/mock-db-data/library-css/css-colors-fonts.data';
-import { CssIconsData } from 'src/mock-db-data/library-css/css-icons.data';
+import { CssPageTemplatesData } from 'src/MOCK_DB_DATA/library-css/css-page-templates.data';
+import { CssModulesData } from 'src/MOCK_DB_DATA/library-css/css-modules.data';
+import { CssComponentsData } from 'src/MOCK_DB_DATA/library-css/css-components.data';
+import { CssColorsFontsData } from 'src/MOCK_DB_DATA/library-css/css-colors-fonts.data';
+import { CssIconsData } from 'src/MOCK_DB_DATA/library-css/css-icons.data';
 
 
 @Injectable({
