16eb1870f071a39fa04b17cf0d4913b0f2be2987
Bernt Andersen <bernt.andersen@fhi.no>
2022-08-22 15:30:21 +0200

    Add data source for LibrarySecondLevelMenuComponent

--- src/app/core/mock-db.service.ts
+++ src/app/core/mock-db.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { InMemoryDbService } from 'angular-in-memory-web-api';
 
+import { LibrarySecondLevelMenuData } from 'src/MOCK_DB_DATA/library-second-level-menu.data';
+
 import { CssPageTemplatesData } from 'src/MOCK_DB_DATA/library-css/css-page-templates.data';
 import { CssModulesData } from 'src/MOCK_DB_DATA/library-css/css-modules.data';
 import { CssComponentsData } from 'src/MOCK_DB_DATA/library-css/css-components.data';
@@ -14,6 +16,8 @@ import { CssIconsData } from 'src/MOCK_DB_DATA/library-css/css-icons.data';
 export class MockDbService implements InMemoryDbService {
   createDb(): object {
     return {
+      LibrarySecondLevelMenuData,
+
       CssPageTemplatesData,
       CssModulesData,
       CssComponentsData,
--- src/app/views/shared/library/library-second-level-menu/library-second-level-menu.component.ts
+++ src/app/views/shared/library/library-second-level-menu/library-second-level-menu.component.ts
@@ -1,4 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { LibrarySecondLevelMenuDataService } from './library-second-level-menu-data.service';
 
 import { MenuItem } from 'src/app/models/menu-item.model';
 import { LibraryItemFilter } from '../models/library-item-filter.model';
@@ -7,11 +10,30 @@ import { LibraryItemFilter } from '../models/library-item-filter.model';
   selector: 'app-library-second-level-menu',
   templateUrl: './library-second-level-menu.component.html'
 })
-export class LibrarySecondLevelMenuComponent {
+export class LibrarySecondLevelMenuComponent implements OnInit {
 
-  @Input() menuItems: MenuItem[];
-
+  menuItems!: MenuItem[];
+  menuItemsLoaded = false;
   activeNavTab = 0;
   libraryItemFilter: LibraryItemFilter = { name: '' };
+
+  private subscription: Subscription = new Subscription();
+
+  constructor(
+    private menuDataService: LibrarySecondLevelMenuDataService
+  ) { }
+
+  ngOnInit() {
+    this.subscription.add(this.menuDataService.getMenuItems()
+      .subscribe(menuItems => {
+        this.menuItems = menuItems;
+        this.menuItemsLoaded = true;
+      },
+      error => this.getErrorMessage(error)));
+  }
+
+  private getErrorMessage(error: object) {
+    console.log(error);
+  }
 
 }
