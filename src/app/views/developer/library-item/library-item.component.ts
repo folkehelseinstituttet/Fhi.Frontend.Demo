214386a4cf34a46bdfe989c5212dadaaf55fda6a
Bernt Andersen <bernt.andersen@fhi.no>
2022-08-22 10:04:49 +0200

    Add constants for Example, Documentation and Code

--- src/app/views/developer/library-item/library-item.component.ts
+++ src/app/views/developer/library-item/library-item.component.ts
@@ -5,6 +5,12 @@ import { ViewportScroller } from '@angular/common';
 import { LibraryItem, LibraryItemType } from '../../shared/library/models/library-item.model';
 import { MenuItem } from 'src/app/models/menu-item.model';
 
+const MenuItemName = {
+  example: 'Example',
+  documentation: 'Documentation',
+  code: 'Code'
+};
+
 @Component({
   selector: 'app-library-item',
   templateUrl: './library-item.component.html'
@@ -23,6 +29,7 @@ export class LibraryItemComponent implements OnInit {
   itemTypeHtml = LibraryItemType.html;
   activeMenuItemByDefault = 0;
   navTabMenuItems: MenuItem[];
+  MenuItemName = MenuItemName;
 
   constructor(
     private activatedRoute: ActivatedRoute,
@@ -64,17 +71,17 @@ export class LibraryItemComponent implements OnInit {
     const menuItems: MenuItem[] = [];
 
     menuItems[n++] = {
-      name: 'Example',
+      name: MenuItemName.example,
       link: null
     };
     if (this.documentationHtml !== undefined) {
       menuItems[n++] = {
-        name: 'Documentation',
+        name: MenuItemName.documentation,
         link: null
       };
     }
     menuItems[n++] = {
-      name: 'Code',
+      name: MenuItemName.code,
       link: null
     };
     return menuItems;
