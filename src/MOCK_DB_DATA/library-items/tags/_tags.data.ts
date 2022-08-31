fa34b080ac019a16b99e93464dc92a665f4ffe86
Karl Granli <karl@gran.li>
2022-08-31 13:43:48 +0200

    Added tags with state

--- src/MOCK_DB_DATA/library-items/tags/_tags.data.ts
+++ src/MOCK_DB_DATA/library-items/tags/_tags.data.ts
@@ -1,9 +1,11 @@
 import { LibraryItem } from 'src/app/views/shared/library/models/library-item.model';
 
 import { TagCategory } from './tag-category';
+import { TagState } from './tag-state';
 
 export const TagsData: LibraryItem[] = [
-  ...TagCategory
+  ...TagCategory,
+  ...TagState
 ];
 
 
---
+++ src/MOCK_DB_DATA/library-items/tags/tag-state.ts
@@ -0,0 +1,52 @@
+import { LibraryItem, LibraryItemType } from 'src/app/views/shared/library/models/library-item.model';
+import { BootstrapComponentsBaseUrl, NgBootstrapComponentsBaseUrl } from '../../bootstrap-base-urls';
+
+export const TagState: LibraryItem[] = [{
+  id: 'tag',
+  title: 'Tag - state',
+  type: LibraryItemType.html,
+  exampleHtml: getExampleHtml(),
+  codeHtml: getCodeHtml(),
+  documentationHtml: getDocumentationHtml()
+}];
+
+
+/*
+ * Return value is ignored if LibraryItemType is not html
+ */
+function getExampleHtml(): string {
+  return `
+<p>
+  <span class="fhi-tag text-bg-info">Info tag</span>
+</p>
+<p>
+  <span class="fhi-tag text-bg-success">Success tag</span>
+</p>
+<p>
+  <span class="fhi-tag text-bg-warning">Warning tag</span>
+</p>
+<p>
+  <span class="fhi-tag text-bg-error">Error tag</span>
+</p>
+`;
+}
+
+/*
+ * Return empty string to use a copy of exampleHtml as codeHtml.
+ * Return null to remove Code from library-item.
+ */
+function getCodeHtml(): string | null {
+  return `
+<span class="fhi-tag text-bg-info">Info tag</span>
+<span class="fhi-tag text-bg-success">Success tag</span>
+<span class="fhi-tag text-bg-warning">Warning tag</span>
+<span class="fhi-tag text-bg-error">Error tag</span>
+`;
+}
+
+/*
+ * Return null to remove Code from library-item.
+ */
+function getDocumentationHtml(): string | null {
+  return null;
+}
