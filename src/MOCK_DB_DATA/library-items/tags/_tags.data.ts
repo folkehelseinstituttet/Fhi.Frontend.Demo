ccec0097fe5f8493e1271096c651dd406f1c6254
Karl Granli <karl@gran.li>
2022-08-31 15:54:39 +0200

    Added filter option tag

--- src/MOCK_DB_DATA/library-items/tags/_tags.data.ts
+++ src/MOCK_DB_DATA/library-items/tags/_tags.data.ts
@@ -2,9 +2,11 @@ import { LibraryItem } from 'src/app/views/shared/library/models/library-item.mo
 
 import { TagCategory } from './tag-category';
 import { TagState } from './tag-state';
+import { TagFilterOption } from './tag-filter-option';
 
 export const TagsData: LibraryItem[] = [
   ...TagCategory,
+  ...TagFilterOption,
   ...TagState
 ];
 
---
+++ src/MOCK_DB_DATA/library-items/tags/tag-filter-option.ts
@@ -0,0 +1,39 @@
+import { LibraryItem, LibraryItemType } from 'src/app/views/shared/library/models/library-item.model';
+import { BootstrapComponentsBaseUrl, NgBootstrapComponentsBaseUrl } from '../../bootstrap-base-urls';
+
+export const TagFilterOption: LibraryItem[] = [{
+  id: 'tag',
+  title: 'Tag - filter option',
+  type: LibraryItemType.fhiAngular,
+  exampleHtml: getExampleHtml(),
+  codeHtml: getCodeHtml(),
+  documentationHtml: getDocumentationHtml()
+}];
+
+
+/*
+ * Return value is ignored if LibraryItemType is not html
+ */
+function getExampleHtml(): string {
+  return ``;
+}
+
+/*
+ * Return empty string to use a copy of exampleHtml as codeHtml.
+ * Return null to remove Code from library-item.
+ */
+function getCodeHtml(): string | null {
+  return `
+<button class="fhi-tag fhi-tag--filter-option" *ngIf="filterActive" (click)="filterActive = false">
+  Filter option
+  <i class="icon-xmark"></i>
+</button>
+`;
+}
+
+/*
+ * Return null to remove Code from library-item.
+ */
+function getDocumentationHtml(): string | null {
+  return null;
+}
