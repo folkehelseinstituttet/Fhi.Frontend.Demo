49eacdcf5ae06a94ddc8601ebfe780f1fe730f1a
Karl Granli <karl@gran.li>
2022-09-02 13:24:52 +0200

    Added tag multiple select

--- src/MOCK_DB_DATA/library-items/tags/_tags.data.ts
+++ src/MOCK_DB_DATA/library-items/tags/_tags.data.ts
@@ -4,12 +4,14 @@ import { TagCategory } from './tag-category';
 import { TagState } from './tag-state';
 import { TagFilterOption } from './tag-filter-option';
 import { TagSingleSelection } from './tag-single-selection';
+import { TagMultipleSelection } from './tag-multiple-selection';
 
 export const TagsData: LibraryItem[] = [
   ...TagCategory,
   ...TagFilterOption,
   ...TagState,
   ...TagSingleSelection,
+  ...TagMultipleSelection,
 ];
 
 
---
+++ src/MOCK_DB_DATA/library-items/tags/tag-multiple-selection.ts
@@ -0,0 +1,49 @@
+import { LibraryItem, LibraryItemType } from 'src/app/views/shared/library/models/library-item.model';
+import { BootstrapComponentsBaseUrl, NgBootstrapComponentsBaseUrl } from '../../bootstrap-base-urls';
+
+export const TagMultipleSelection: LibraryItem[] = [{
+  id: 'tag',
+  title: 'Tag - multiple selection',
+  type: LibraryItemType.html,
+  exampleHtml: getExampleHtml(),
+  codeHtml: getCodeHtml(),
+  documentationHtml: getDocumentationHtml()
+}];
+
+
+/*
+ * Return value is ignored if LibraryItemType is not html
+ */
+function getExampleHtml(): string {
+  return `
+<div class="fhi-tag-selector">
+  <input class="fhi-tag-selector__input" type="checkbox" id="mltpl-opt-1">
+  <label class="fhi-tag-selector__label" for="mltpl-opt-1"><i class="icon-check"></i> Option 1</label>
+</div>
+
+<div class="fhi-tag-selector">
+  <input class="fhi-tag-selector__input" type="checkbox" id="mltpl-opt-2">
+  <label class="fhi-tag-selector__label" for="mltpl-opt-2"><i class="icon-check"></i> Option 2</label>
+</div>
+
+<div class="fhi-tag-selector">
+  <input class="fhi-tag-selector__input" type="checkbox" id="mltpl-opt-3">
+  <label class="fhi-tag-selector__label" for="mltpl-opt-3"><i class="icon-check"></i> Option 3</label>
+</div>
+`;
+}
+
+/*
+ * Return empty string to use a copy of exampleHtml as codeHtml.
+ * Return null to remove Code from library-item.
+ */
+function getCodeHtml(): string | null {
+  return ``;
+}
+
+/*
+ * Return null to remove Code from library-item.
+ */
+function getDocumentationHtml(): string | null {
+  return null;
+}
