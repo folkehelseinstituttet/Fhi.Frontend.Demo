36722e039b7e7c659f2080d0f6eebdf5f3abe9ca
Bernt Andersen <bernt.andersen@fhi.no>
2022-09-14 12:56:02 +0200

    Add ids for alert

--- src/app/views/shared/library/library-item-ids.ts
+++ src/app/views/shared/library/library-item-ids.ts
@@ -2,4 +2,7 @@ export class LibraryItemIds {
   static AccordionDefault = 'accordiondefault';
   static AccordionFlush = 'accordionflush';
   static FhiAccordionFlush = 'fhiaccordionflush';
+
+  static AlertDefault = 'alertdefault';
+  static AlertDismissible = 'alertdismissible';
 }
