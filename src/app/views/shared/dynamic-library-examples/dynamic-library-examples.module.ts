57f49bd7d6c1ffeea575d758278e485d4738ce9c
Bernt Andersen <bernt.andersen@fhi.no>
2022-09-19 09:42:31 +0200

    Move fhi-examples

--- src/app/views/shared/dynamic-library-examples/dynamic-library-examples.module.ts
+++ src/app/views/shared/dynamic-library-examples/dynamic-library-examples.module.ts
@@ -7,22 +7,23 @@ import { DynamicLibraryExampleComponent } from './dynamic-library-example.compon
 import { AccordionExampleComponent } from './ng-bootstrap-examples/accordion-example/accordion-example.component';
 import { AlertDismissibleExampleComponent } from './ng-bootstrap-examples/alert-dismissible-example/alert-dismissible-example.component';
 import { ModalExampleComponent } from './ng-bootstrap-examples/modal-example/modal-example.component';
-import { SearchExampleComponent } from './fhi-angular-examples/search-example/search-example.component';
-import { TagExampleComponent } from './fhi-angular-examples/tag-example/tag-example.component';
 import { ToastExampleComponent } from './ng-bootstrap-examples/toast-example/toast-example.component';
 import { TooltipExampleComponent } from './ng-bootstrap-examples/tooltip-example/tooltip-example.component';
+
+import { SearchExampleComponent } from './fhi-angular-examples/search-example/search-example.component';
+import { TagExampleComponent } from './fhi-angular-examples/tag-example/tag-example.component';
 
 const NGB_EXAMPLES = [
   AccordionExampleComponent,
   AlertDismissibleExampleComponent,
   ModalExampleComponent,
-  SearchExampleComponent,
-  TagExampleComponent,
   ToastExampleComponent,
   TooltipExampleComponent
 ];
 
 const FHI_EXAMPLES = [
+  SearchExampleComponent,
+  TagExampleComponent
 ];
 
 @NgModule({
