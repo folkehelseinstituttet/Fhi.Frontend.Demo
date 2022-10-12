815d7aaeee7851b8fc660064dc24f40578710a89
Karl Granli <karl@gran.li>
2022-10-12 12:17:33 +0200

    Update article.component.ts

--- src/app/views/shared/article/article.component.ts
+++ src/app/views/shared/article/article.component.ts
@@ -41,7 +41,7 @@ export class ArticleComponent implements OnInit {
   private findSecondLevelDeveloper() {
     switch (this.urlService.getSegmentPath(1)) {
       case SegmentPaths.visualIdentity:
-        this.title = 'Visuell identitet';
+        this.title = 'Visuell identitet i FHI';
         this.articleHtml = this.getArticleHtmlForDeveloperVisualIdentity();
         break;
 
@@ -56,21 +56,16 @@ export class ArticleComponent implements OnInit {
 
   private getArticleHtmlForDeveloperVisualIdentity(): any {
     return `
-<p>
-  Her er det bare å fylle på med markup...
-</p>
-<p>
-  <a href="">Lenker og what not...</a>
-</p>`;
+<p>For at alle våre produkter skal ha en tydelig og gjenkjennbar profil må farger, ikonbruk, typografi, logo og illustrasjoner henge sammen og brukes konsistent.</p>`;
   }
 
   private getArticleHtmlForDeveloperComponents(): any {
     return `
-<p>Komponentbiblioteket i FHI Designsystem baserer seg på <a href="https://getbootstrap.com/docs/5.2/getting-started/introduction/">Bootstrap 5</a>. Interaktive komponenter benytter <a href="https://ng-bootstrap.github.io/" class="text-nowrap">ng-bootstrap</a> og andre tredjeparts-komponenter der disse finnes. I tillegg er noen skreddersydde Angular-komponenter produsert av FHI.</p>
+<p>Komponentbiblioteket i FHI Designsystem baserer seg på <a href="https://getbootstrap.com/docs/5.2/getting-started/introduction/">Bootstrap 5</a>. Interaktive komponenter benytter <a href="https://ng-bootstrap.github.io/" class="text-nowrap">ng-bootstrap</a> og andre tredjepartskomponenter der disse finnes. I tillegg er noen skreddersydde Angularkomponenter produsert av FHI.</p>
 
-<p>Alle løsninger kan konsumere stilsettet uavhengig av benyttet rammeverk i enhver løsning. Legg til CSS fra FHI Designsystem: <a href="https://www.npmjs.com/package/@folkehelseinstituttet/style">@folkehelseinstituttet/style</a>.</p>
+<p>Alle løsninger kan konsumere stilsettet uavhengig av hvilket rammeverk som er benyttet. Legg til CSS fra FHI Designsystem: <a href="https://www.npmjs.com/package/@folkehelseinstituttet/style">@folkehelseinstituttet/style</a>.</p>
 
-<p>FHI Designsystem bygges i Angular og løsninger som benytter annet frontend-rammeverk og konsumerer stilsettet er da ansvarlige for å generere riktig markup slik at utseendet blir korrekt.</p>
+<p>FHI Designsystem bygges i Angular og løsninger som benytter annet frontendrammeverk og konsumerer stilsettet er da ansvarlige for å generere riktig markup slik at utseendet blir korrekt.</p>
 
 <p><a href="https://github.com/folkehelseinstituttet/Fhi.Frontend.Style/blob/main/CHANGELOG.md">Se changelog for stilsettet her.</a></p>`;
   }
