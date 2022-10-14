66835263638d1f622cc97f851e01726086a980c2
Karl Granli <karl@gran.li>
2022-10-14 15:19:47 +0200

    Dynamic example not yet functional

--- src/app/views/shared/library/library-item-full-screen-button/library-item-full-screen-button.component.ts
+++ src/app/views/shared/library/library-item-full-screen-button/library-item-full-screen-button.component.ts
@@ -2,6 +2,8 @@ import { HtmlParser } from '@angular/compiler';
 import { Component, OnInit, Input } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
+// import { LibraryItem, LibraryItemType } from '../../../shared/models/library-item.model';
+
 @Component({
   selector: 'app-library-item-full-screen-button',
   templateUrl: './library-item-full-screen-button.component.html',
