3e1ccfa599a885fc013d6d9d6353b00f16f882f1
Bernt Andersen <bernt.andersen@fhi.no>
2022-10-17 09:32:53 +0200

    Add new component

---
+++ src/app/views/developer/library-item-full-srceen/library-item-full-srceen.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-library-item-full-srceen',
+  templateUrl: './library-item-full-srceen.component.html'
+})
+export class LibraryItemFullSrceenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
