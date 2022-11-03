dbd7f70a99240014ece9125f233e52344de610c4
Bernt Andersen <bernt@proand.no>
2022-11-03 10:12:20 +0100

    New/npm package for angular components (#170)
    * Add ./projects/fhi-angular-components
    * Add github action for deploy of fhi-angular-components to @folkehelseinstituttet/angular-components
    * Update CONTRIBUTING and README

---
+++ projects/fhi-angular-components/CONTRIBUTING.md
@@ -0,0 +1,3 @@
+# How to contribute
+
+More info here: [Fhi.Frontend.Demo/CONTRIBUTING.md](https://github.com/folkehelseinstituttet/Fhi.Frontend.Demo/blob/dev/CONTRIBUTING.md)
---
+++ projects/fhi-angular-components/src/lib/fhi-multiselect/fhi-multiselect.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fhi-multiselect',
+  templateUrl: './fhi-multiselect.component.html',
+  styleUrls: ['./fhi-multiselect.component.scss']
+})
+export class FhiMultiselectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
