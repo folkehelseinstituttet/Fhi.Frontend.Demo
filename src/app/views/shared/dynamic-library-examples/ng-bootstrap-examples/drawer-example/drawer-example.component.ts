69bb8464cbc9ee5801e9342fd4e0765fb943029e
Karl Granli <karl@gran.li>
2022-11-04 15:00:13 +0100

    Added example with overlayed drawer

--- src/app/views/shared/dynamic-library-examples/ng-bootstrap-examples/drawer-example/drawer-example.component.ts
+++ src/app/views/shared/dynamic-library-examples/ng-bootstrap-examples/drawer-example/drawer-example.component.ts
@@ -5,19 +5,36 @@ import { Component, ElementRef, Input, ViewChild } from '@angular/core';
   templateUrl: './drawer-example.component.html'
 })
 export class DrawerExampleComponent {
-  @ViewChild('DrawerTrigger') drawerTrigger!: ElementRef;
-  @ViewChild('DrawerContent') drawerContent!: ElementRef;
-  @ViewChild('mainContentArea') mainContentArea!: ElementRef;
+  @ViewChild('drawerContent') drawerContent: ElementRef;
+  @ViewChild('drawerTrigger') drawerTrigger: ElementRef;
+  @ViewChild('drawerContent2') drawerContent2: ElementRef;
+  @ViewChild('drawerTrigger2') drawerTrigger2: ElementRef;
 
   @Input() itemId!: string;
   @Input() itemIds!: any;
 
+  drawerHeight:number = 0;
+  drawerHeight2:number = 0;
   drawerIsOpen:boolean = false;
+  drawerIsOpen2:boolean = false;
 
   toggleDrawer() {
     this.drawerIsOpen = !this.drawerIsOpen;
+
     if (this.drawerIsOpen) {
-      console.log(this.drawerContent.nativeElement.offsetHeight);
+      this.drawerHeight = this.drawerContent.nativeElement.offsetHeight + this.drawerTrigger.nativeElement.offsetHeight;
+    } else {
+      this.drawerHeight = 0;
+    }
+  }
+
+  toggleDrawer2() {
+    this.drawerIsOpen2 = !this.drawerIsOpen2;
+    
+    if (this.drawerIsOpen2) {
+      this.drawerHeight2 = this.drawerContent2.nativeElement.offsetHeight + this.drawerTrigger2.nativeElement.offsetHeight;
+    } else {
+      this.drawerHeight2 = 0;
     }
   }
 }
