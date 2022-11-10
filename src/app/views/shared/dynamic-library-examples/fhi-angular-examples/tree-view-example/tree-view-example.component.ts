e94b6ce66defe8572df83fc12663d91c32005d49
Karl Granli <karl@gran.li>
2022-11-10 11:47:07 +0100

    Angular Tree Component added

--- src/app/views/shared/dynamic-library-examples/fhi-angular-examples/tree-view-example/tree-view-example.component.ts
+++ src/app/views/shared/dynamic-library-examples/fhi-angular-examples/tree-view-example/tree-view-example.component.ts
@@ -1,5 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { TreeModule } from '@circlon/angular-tree-component';
 
 @Component({
   selector: 'app-tree-view-example',
@@ -8,4 +7,26 @@ import { TreeModule } from '@circlon/angular-tree-component';
 export class TreeViewExampleComponent {
   @Input() itemId!: string;
   @Input() itemIds!: any;
+
+  nodes:any = [
+    {
+      name: 'root1',
+      children: [
+        { name: 'child1' },
+        { name: 'child2' }
+      ]
+    },
+    {
+      name: 'root2',
+      children: [
+        { name: 'child2.1', children: [] },
+        { name: 'child2.2', children: [
+            {name: 'grandchild2.2.1'}
+          ] }
+      ]
+    },
+    { name: 'root3' },
+    { name: 'root4', children: [] },
+    { name: 'root5', children: null }
+  ];
 }
