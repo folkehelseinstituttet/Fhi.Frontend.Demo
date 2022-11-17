12c3aceca5ad9502f3ae7638f3686f7f5c254491
Karl Granli <karl@gran.li>
2022-11-17 16:24:38 +0100

    Remove flat secondary button

--- src/MOCK_DB_DATA/library-items/buttons/_buttons.data.ts
+++ src/MOCK_DB_DATA/library-items/buttons/_buttons.data.ts
@@ -3,7 +3,6 @@ import { LibraryItem } from 'src/app/views/shared/models/library-item.model';
 import { BtnPrimary } from './btn-primary';
 import { BtnSecondary } from './btn-secondary';
 import { BtnFlat } from './btn-flat';
-import { BtnFlatSecondary } from './btn-flat-secondary';
 import { BtnIcon } from './btn-icon';
 import { BtnShortcut } from './btn-shortcut';
 import { BtnShortcutLink } from './btn-shortcut-link';
@@ -13,7 +12,6 @@ export const ButtonsData: LibraryItem[] = [
   ...BtnPrimary,
   ...BtnSecondary,
   ...BtnFlat,
-  ...BtnFlatSecondary,
   ...BtnIcon,
   ...BtnShortcut,
   ...BtnShortcutLink,
--- src/MOCK_DB_DATA/library-items/buttons/_buttons.ids.ts
+++ src/MOCK_DB_DATA/library-items/buttons/_buttons.ids.ts
@@ -2,7 +2,6 @@ export const ButtonsIds = {
   ButtonPrimary: undefined,
   ButtonSecondary: undefined,
   ButtonFlat: undefined,
-  ButtonFlatSecondary: undefined,
   ButtonShortcutLink: undefined,
   ButtonShortcut: undefined,
   ButtonIcon: undefined,
