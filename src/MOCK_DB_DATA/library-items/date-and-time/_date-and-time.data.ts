d573dd5ef7fdd66ea71331c9013d455154efb2cf
Karl Granli <karl@gran.li>
2022-12-06 15:12:35 +0100

    New/datepicker date range (#207)

--- src/MOCK_DB_DATA/library-items/date-and-time/_date-and-time.data.ts
+++ src/MOCK_DB_DATA/library-items/date-and-time/_date-and-time.data.ts
@@ -1,9 +1,11 @@
 import { LibraryItem } from 'src/app/views/shared/models/library-item.model';
 
+import { DateRange } from './date-range';
 import { YearSelection } from './year-selection';
 import { WeekSelection } from './week-selection';
 
-export const DateAndTimeSelectionData: LibraryItem[] = [
+export const DateAndTimeData: LibraryItem[] = [
+  ...DateRange,
   ...YearSelection,
   ...WeekSelection
 ];
