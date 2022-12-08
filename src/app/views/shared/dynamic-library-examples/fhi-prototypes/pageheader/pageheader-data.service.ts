a857260ae4c889e755b5c562a51fb6c5ee7209cd
Karl Granli <karl@gran.li>
2022-12-08 16:05:36 +0100

    Sticky header starting point

--- src/app/views/shared/dynamic-library-examples/fhi-prototypes/pageheader/pageheader-data.service.ts
+++ src/app/views/shared/dynamic-library-examples/fhi-prototypes/pageheader/pageheader-data.service.ts
@@ -2,66 +2,36 @@ import { Injectable } from "@angular/core";
 
 @Injectable()
 export class PrototypePageheaderDataService {
-  getPrototypeMenuItems() {
-    return [
-      {
-        name: 'Folkehelsestatistikk',
-        nameLong: 'Folkehelsestatistikk'
-      },
-      {
-        name: 'Dødsårsaksregisteret',
-        nameLong: 'Dødsårsaksregisteret statistikkbank',
-        subMenu: [
-          {
-            name: 'Alle dødsårsaker'
-          },
-          {
-            name: 'Demens'
-          },
-          {
-            name: 'Hjerte- og karsykdom'
-          },
-          {
-            name: 'Kreft'
-          },
-          {
-            name: 'Selvmord'
-          },
-          {
-            name: 'Ulykker'
-          },
-          {
-            name: 'Dødssted'
-          },
-          {
-            name: 'Fylker'
-          }
-        ]
-      },
-      {
-        name: 'MSIS',
-        nameLong: 'MSIS - Meldesystem for smittsomme sykdommer'
-      },
-      {
-        name: 'SYSVAK',
-        nameLong: 'SYSVAK - Nasjonalt vaksinasjonsregister'
-      },
-      {
-        name: 'Legemiddelregisteret',
-        nameLong: 'Legemiddelregisteret'
-      },
-      {
-        name: 'Medisinsk fødselsregister (MFR)',
-        nameLong: 'Medisinsk fødselsregister (MFR)'
-      },
-      {
-        name: 'Abortregisteret',
-        nameLong: 'Abortregisteret'
-      },
-      {
-        name: 'Hjerte- og karregisteret',
-        nameLong: 'Hjerte- og karregisteret'
-      },
-    ];
+  getNodes() {
+    return {
+      name: 'Dødsårsaksregisteret',
+      nameLong: 'Dødsårsaksregisteret statistikkbank',
+      subMenu: [
+        {
+          name: 'Alle dødsårsaker'
+        },
+        {
+          name: 'Demens'
+        },
+        {
+          name: 'Hjerte- og karsykdom'
+        },
+        {
+          name: 'Kreft'
+        },
+        {
+          name: 'Selvmord'
+        },
+        {
+          name: 'Ulykker'
+        },
+        {
+          name: 'Dødssted'
+        },
+        {
+          name: 'Fylker'
+        }
+      ]
+    }
   }
 }
--- src/app/views/shared/dynamic-library-examples/fhi-prototypes/pageheader/pageheader.component.ts
+++ src/app/views/shared/dynamic-library-examples/fhi-prototypes/pageheader/pageheader.component.ts
@@ -1,20 +1,52 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject } from '@angular/core';
 import { PrototypePageheaderDataService } from './pageheader-data.service';
 
 @Component({
   selector: 'app-prototype-pageheader',
   templateUrl: './pageheader.component.html',
+  styleUrls: ['./pageheader.component.scss'],
   providers: [PrototypePageheaderDataService]
 })
 export class PrototypePageheaderExampleComponent {
   activeLink: number = -1;
   mainMenuIsOpen: boolean = false;
-  prototypeMenuItems: any = [];
+  hideMostContent: boolean = true;
+  currentVerticalScrollPosition: number;
+  currentPosition: number;
+  data: any = [];
 
-  constructor(private dataService: PrototypePageheaderDataService) {}
+  constructor(private dataService: PrototypePageheaderDataService, @Inject(DOCUMENT) private _document: Document) {
+    this._document.addEventListener('scroll', this.onPageContentScrolled);
+  }
+  
+  ngOnDestroy() {
+    this._document.removeEventListener('scroll', this.onPageContentScrolled);
+  }
+  
+  onPageContentScrolled = (e:any) => {
+    let scroll = window.pageYOffset;
+    if (scroll > this.currentVerticalScrollPosition) {
+      console.log('scrollDown');
+    } else {
+      console.log('scrollUp');
+    }
+    this.currentVerticalScrollPosition = scroll;
+  }
+  
+  @HostListener("scroll", ['$event.target'])
+  onContentScrolled(e: HTMLElement) {
+    let scroll = e.scrollTop;
+    if (scroll > this.currentPosition) {
+      console.log("scrollDown");
+    } else {
+      console.log("scrollUp");
+    }
+    this.currentPosition = scroll;
+  }
 
   ngOnInit() {
-    this.prototypeMenuItems = this.dataService.getPrototypeMenuItems();
+    this.data = this.dataService.getNodes();
   }
 
   linkSwitch(num: number) {
