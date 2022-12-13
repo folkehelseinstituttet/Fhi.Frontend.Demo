e57025079a8032af41ea09bc4a14d9900e11181a
Karl Granli <karl@gran.li>
2022-12-13 13:44:53 +0100

    Update prototype-pageheader.ts

--- src/MOCK_DB_DATA/library-items/prototype-pageheader/prototype-pageheader.ts
+++ src/MOCK_DB_DATA/library-items/prototype-pageheader/prototype-pageheader.ts
@@ -26,7 +26,52 @@ function getExampleHtml(): string {
  */
 function getCodeHtml(): string | null {
   return `
-<div>Prototype pageheader</div>`;
+<div class="ds-pageheader-component" #pageheadercomponent>
+  <div class="fhi-pageheader fhi-pageheader--sticky" [ngClass]="{'fhi-pageheader--sticky__no-logo' : logoHidden}">
+    <header class="fhi-header">
+      <div class="fhi-header__brand">
+        <div class="container fhi-header__brand-container">
+          <div class="fhi-header__brand-content">
+            <a class="fhi-header__logo" href="javascript:void(0)" (click)="linkSwitch(-1)">
+              <i class="icon-fhi-logo fhi-header__logo-icon"></i>
+              <span class="visually-hidden">FHI Statistikk</span>
+            </a>
+            <div class="fhi-header__project">
+              <span class="fhi-header__project-name">Statistikk</span>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container p-0 border-bottom">
+        <div class="row">
+          <div class="col-auto">
+            <h1 class="fhi-pageheader__section-title">
+              {{ data.name }}
+            </h1>
+          </div>
+        </div>
+      </div>
+      
+      <div class="container p-0" #pageheadersubmenucontainer>
+        <div class="fhi-pageheader__submenu-container">
+          <ul class="fhi-pageheader__submenu" [ngStyle]="{'width' : submenuOverflow ? submenuWidth + 'px' : 'auto'}" #pageheadersubmenu>
+            <li class="fhi-pageheader__submenu-item" *ngFor="let subMenuItem of data.subMenu; index as i">
+              <a class="btn fhi-btn-menu-item"
+                href="javascript:void(0)"
+                [ngClass]="{'fhi-btn-menu-item--active': activeLink === i}"
+                (click)="linkSwitch(i)">
+                <i class="icon-population"></i>
+                <span class="btn__text">{{ subMenuItem.name }}</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  </div>
+</div>
+`;
 }
 
 /*
