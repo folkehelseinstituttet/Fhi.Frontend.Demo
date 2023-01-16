22df47a644b40fac96655d83ea55f027c8c4c630
Karl Granli <karl@gran.li>
2023-01-16 13:11:15 +0100

    Dynamic example added

--- src/MOCK_DB_DATA/library-items/forms/switch.ts
+++ src/MOCK_DB_DATA/library-items/forms/switch.ts
@@ -4,7 +4,7 @@ import { LibraryItem, LibraryItemType } from 'src/app/views/shared/models/librar
 export const Switch: LibraryItem[] = [{
   id: LibraryItemIds.FormSwitch,
   title: 'Switch',
-  type: LibraryItemType.html,
+  type: LibraryItemType.fhiAngular,
   exampleHtml: getExampleHtml(),
   codeHtml: getCodeHtml(),
   documentationHtml: getDocumentationHtml()
@@ -15,32 +15,7 @@ export const Switch: LibraryItem[] = [{
  * Return value is ignored if LibraryItemType is not html
  */
 function getExampleHtml(): string {
-  return `
-<div class="form-check form-switch mb-2">
-  <input class="form-check-input visually-hidden" type="checkbox" id="flexSwitchCheckDefault">
-  <label class="form-check-label" for="flexSwitchCheckDefault">
-    Skru på/av denne bryteren
-  </label>
-</div>
-<div class="form-check form-switch mb-2">
-  <input class="form-check-input visually-hidden" type="checkbox" id="flexSwitchCheckChecked2" checked>
-  <label class="form-check-label" for="flexSwitchCheckChecked2">
-    Skru av/på denne bryteren
-  </label>
-</div>
-<p class="pt-5">Deaktiverte brytere</p>
-<div class="form-check form-switch mb-2">
-  <input class="form-check-input visually-hidden" type="checkbox" id="flexSwitchCheckChecked3" disabled>
-  <label class="form-check-label" for="flexSwitchCheckChecked3">
-    Deaktivert bryter
-  </label>
-</div>
-<div class="form-check form-switch">
-  <input class="form-check-input visually-hidden" type="checkbox" id="flexSwitchCheckChecked4" checked disabled>
-  <label class="form-check-label" for="flexSwitchCheckChecked4">
-    Deaktivert bryter som er påskrudd
-  </label>
-</div>`;
+  return ``;
 }
 
 /*
---
+++ src/app/views/shared/dynamic-library-examples/fhi-angular-examples/form-element-examples/form-element-examples.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-element-examples',
+  templateUrl: './form-element-examples.component.html'
+})
+export class FormElementExamplesComponent {
+  
+}
