6a6019eed4374bbae28b6ceda541cef04de61cdb
Bernt Andersen <bernt@proand.no>
2023-02-01 11:51:34 +0100

    New/project fhi angular highcharts (#238)
    Add boilerplate code from CLI and a new Github Action

---
+++ projects/fhi-angular-highcharts/src/lib/fhi-angular-highcharts.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'fhi-angular-highcharts',
+  template: `
+    <p>
+      fhi-angular-highcharts works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class FhiAngularHighchartsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/fhi-angular-highcharts/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of fhi-angular-highcharts
+ */
+
+export * from './lib/fhi-angular-highcharts.service';
+export * from './lib/fhi-angular-highcharts.component';
+export * from './lib/fhi-angular-highcharts.module';
