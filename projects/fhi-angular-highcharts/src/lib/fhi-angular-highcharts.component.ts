1f8c4d8cd6aebf577f623a0cba0e66b3f87053a2
Bernt Andersen <bernt.andersen@fhi.no>
2023-02-10 12:04:03 +0100

    Upgrade Highcharts version

--- projects/fhi-angular-highcharts/src/lib/fhi-angular-highcharts.component.ts
+++ projects/fhi-angular-highcharts/src/lib/fhi-angular-highcharts.component.ts
@@ -1,20 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, ViewEncapsulation } from '@angular/core';
+import { first } from 'rxjs/operators';
+import * as Highcharts from 'highcharts'; // https://github.com/highcharts/highcharts/issues/14183, hvis Highcharts endrer fra CommonJS til ECMAScript modules i fremtidig versjon, fjern følgende fra angular.json: "allowedCommonJsDependencies": ["highcharts"]
+import { Options, Chart } from 'highcharts'; // https://github.com/highcharts/highcharts/issues/14183, hvis Highcharts endrer fra CommonJS til ECMAScript modules i fremtidig versjon, fjern følgende fra angular.json: "allowedCommonJsDependencies": ["highcharts"]
+import HighchartsExporting from 'highcharts/modules/exporting';
+import HighchartsOfflineExporting from 'highcharts/modules/offline-exporting';
+import HighchartsExportData from 'highcharts/modules/export-data';
+import HighchartsMap from 'highcharts/modules/map';
+
+// import { FhiHighchartsConfig } from './fhi-highcharts-config.model';
+// import { FhiHighchartsOptionsService } from './services/fhi-highcharts-options.service';
+// import { FhiHighchartsTableService } from './services/fhi-highcharts-table.service';
+// import { FhiHighchartsChartInstanceService } from './services/fhi-highcharts-chart-instance.service';
+// import { FhiHighchartsCsvService } from './services/fhi-highcharts-csv.service';
+// import { FhiHighchartsDownloadService } from './services/fhi-highcharts-download.service';
+// import { FhiHighchartsGeoJsonService } from "./services/fhi-highcharts-geo-json.service";
+// import { Diagramtype, Diagramtyper } from './fhi-highcharts-diagramtyper';
+enum DiagramTemplates { chart = 'chart', map = 'map', table = 'table' };
 
 @Component({
   selector: 'fhi-angular-highcharts',
-  template: `
-    <p>
-      fhi-angular-highcharts works!
-    </p>
-  `,
-  styles: [
-  ]
+  templateUrl: './fhi-angular-highcharts.component.html'
 })
-export class FhiAngularHighchartsComponent implements OnInit {
+export class FhiAngularHighchartsComponent {
+
+  highcharts = Highcharts;
+  currentDiagramTemplate!: string;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
