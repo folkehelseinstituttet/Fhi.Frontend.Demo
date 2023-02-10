dadf14eb19342d4ff60528c017e9707025881b19
Bernt Andersen <bernt.andersen@fhi.no>
2023-02-10 13:37:43 +0100

    Add markup and the rest of the component code

--- projects/fhi-angular-highcharts/src/lib/fhi-angular-highcharts.component.ts
+++ projects/fhi-angular-highcharts/src/lib/fhi-angular-highcharts.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { first } from 'rxjs/operators';
 import * as Highcharts from 'highcharts'; // https://github.com/highcharts/highcharts/issues/14183, hvis Highcharts endrer fra CommonJS til ECMAScript modules i fremtidig versjon, fjern følgende fra angular.json: "allowedCommonJsDependencies": ["highcharts"]
 import { Options, Chart } from 'highcharts'; // https://github.com/highcharts/highcharts/issues/14183, hvis Highcharts endrer fra CommonJS til ECMAScript modules i fremtidig versjon, fjern følgende fra angular.json: "allowedCommonJsDependencies": ["highcharts"]
@@ -7,7 +7,7 @@ import HighchartsOfflineExporting from 'highcharts/modules/offline-exporting';
 import HighchartsExportData from 'highcharts/modules/export-data';
 import HighchartsMap from 'highcharts/modules/map';
 
-// TODO: rename (either FhiAngularHighcharts or just Highcharts)
+// TODO: rename (either FhiAngularHighcharts or just Highcharts, or maybe no prefix at all...)
 import { FhiHighchartsConfig } from './fhi-highcharts-config.model';
 import { FhiHighchartsOptionsService } from './services/fhi-highcharts-options.service';
 import { FhiHighchartsTableService } from './services/fhi-highcharts-table.service';
@@ -23,13 +23,96 @@ enum DiagramTemplates { chart = 'chart', map = 'map', table = 'table' };
 
 @Component({
   selector: 'fhi-angular-highcharts',
-  templateUrl: './fhi-angular-highcharts.component.html'
+  templateUrl: './fhi-angular-highcharts.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class FhiAngularHighchartsComponent {
 
   highcharts = Highcharts;
+  options!: Options;
+  diagramTemplates = DiagramTemplates;
   currentDiagramTemplate!: string;
+  showDefaultChartTemplate = true;
+  allMapsLoaded = false;
+  tableTitle!: string;
+  tableHeaderRow = new Array();
+  tableBodyRows = new Array();
 
-  constructor() { }
+  @Input() config!: FhiHighchartsConfig;
+
+  constructor(
+    private changeDetector: ChangeDetectorRef,
+    private optionsService: FhiHighchartsOptionsService,
+    private chartInstanceService: FhiHighchartsChartInstanceService,
+    private csvService: FhiHighchartsCsvService,
+    private tableService: FhiHighchartsTableService,
+    private downloadService: FhiHighchartsDownloadService,
+    private geoJsonService: FhiHighchartsGeoJsonService
+  ) {
+    HighchartsExporting(Highcharts);
+    HighchartsOfflineExporting(Highcharts);
+    HighchartsExportData(Highcharts);
+    HighchartsMap(Highcharts);
+  }
+
+  ngOnChanges() {
+    this.currentDiagramTemplate = this.getCurrentDiagramTemplate(this.config.diagramtype);
+    this.options = this.optionsService.updateOptions(this.config, this.allMapsLoaded);
+
+    if (this.currentDiagramTemplate === this.diagramTemplates.table) {
+      this.updateTable(this.options);
+    }
+    if (this.config.diagramtype.isMap) {
+      this.checkIfMapIsLoaded(this.config.diagramtype);
+    }
+  }
+
+  onChartInstance(chart: Chart) {
+    this.chartInstanceService.chart = chart;
+    this.downloadService.setConfig(this.config);
+    this.csvService.csv = chart.getCSV();
+  }
+
+  private updateTable(options: Highcharts.Options) {
+    this.tableHeaderRow = this.tableService.getHeaderRow(options);
+    this.tableBodyRows = this.tableService.getDataRows(options);
+    this.csvService.csv = this.tableService.getCsv(options);
+    this.tableTitle = this.config.title;
+    this.changeDetector.detectChanges();
+  }
+
+  private updateMap(map: any) {
+    this.geoJsonService.updateMapFeatures(map);
+    this.allMapsLoaded = true;
+    this.options = this.optionsService.updateOptions(this.config, this.allMapsLoaded);
+    this.changeDetector.detectChanges();
+  }
+
+  private checkIfMapIsLoaded(diagramtype: DiagramType) {
+    if (Highcharts.maps[diagramtype.id]) {
+      this.updateMap(Highcharts.maps[diagramtype.id]);
+    } else {
+      this.loadMap(diagramtype);
+    }
+  }
+
+  private loadMap(diagramtype: DiagramType) {
+    this.geoJsonService.getMap(diagramtype.mapFile)
+      .pipe(first()).subscribe(map => {
+        this.geoJsonService.addMapToHighcharts(Highcharts, map, diagramtype.id);
+        this.updateMap(map);
+      });
+  }
+
+  private getCurrentDiagramTemplate(diagramtype: DiagramType): string {
+    if (diagramtype.id === DiagramTypes.table.id) {
+      return DiagramTemplates.table;
+    }
+    if (diagramtype.isMap) {
+      return DiagramTemplates.map;
+    }
+    this.showDefaultChartTemplate = !this.showDefaultChartTemplate;
+    return DiagramTemplates.chart
+  }
 
 }
