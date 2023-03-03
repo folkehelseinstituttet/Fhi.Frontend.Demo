9a05b941303e581d3cd4269a4fc47ecd1efdbbdf
Bernt Andersen <bernt.andersen@fhi.no>
2023-03-03 17:09:07 +0100

    Remove boilerplate service

--- projects/fhi-angular-highcharts/src/public-api.ts
+++ projects/fhi-angular-highcharts/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of fhi-angular-highcharts
  */
 
-export * from './lib/fhi-angular-highcharts.service';
 export * from './lib/fhi-angular-highcharts.component';
 export * from './lib/fhi-angular-highcharts.module';
 
