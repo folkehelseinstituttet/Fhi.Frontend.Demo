64bd04f7ce05fc75e94b41184804090be8b94ce9
Karl Granli <karl@gran.li>
2023-03-07 11:26:22 +0100

    Drag & Drop

--- src/app/views/shared/dynamic-library-examples/fhi-angular-examples/treeview-example/treeview-example.component.ts
+++ src/app/views/shared/dynamic-library-examples/fhi-angular-examples/treeview-example/treeview-example.component.ts
@@ -1,4 +1,6 @@
 import { Component, Input } from '@angular/core';
+import { ITreeState, ITreeOptions } from '@circlon/angular-tree-component';
+import { v4 } from 'uuid';
 
 
 @Component({
@@ -6,6 +8,7 @@ import { Component, Input } from '@angular/core';
   templateUrl: './treeview-example.component.html'
 })
 export class TreeViewExampleComponent {
+
   @Input() itemId!: string;
   @Input() itemIds!: any;
 
@@ -33,4 +36,47 @@ export class TreeViewExampleComponent {
     },
     { name: 'rot 3' }
   ];
+
+  state: ITreeState = {
+    expandedNodeIds: {
+      1: true,
+      2: true
+    },
+    hiddenNodeIds: {},
+    activeNodeIds: {}
+  };
+
+  options: ITreeOptions = {
+    allowDrag: (node) => node.isLeaf,
+    getNodeClone: (node) => ({
+      ...node.data,
+      id: v4(),
+      name: `copy of ${node.data.name}`
+    })
+  };
+
+  nodes = [
+    {
+      id: 1,
+      name: 'root1',
+      children: [
+        { name: 'child1' },
+        { name: 'child2' }
+      ]
+    },
+    {
+      name: 'root2',
+      id: 2,
+      children: [
+        { name: 'child2.1', children: [] },
+        { name: 'child2.2', children: [
+          {name: 'grandchild2.2.1'}
+        ] }
+      ]
+    },
+    { name: 'root3' },
+    { name: 'root4', children: [] },
+    { name: 'root5', children: null }
+  ];
+
 }
