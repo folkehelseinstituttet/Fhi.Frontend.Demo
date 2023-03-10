ea7954567a8f2da7e98e3518d07cee253a17dea3
Karl Granli <karl@gran.li>
2023-03-10 10:39:45 +0100

    Update generate-icon-list.js

--- src/generate-icon-list.js
+++ src/generate-icon-list.js
@@ -1,13 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-const pathToListOfGeneratedIcons = path.join(__dirname, '..', '..', 'Fhi.Frontend.Style', 'src', 'icons', '_icon-map.GENERATED.scss');
-// const listOfGenaratedIcons = fs.readFile(pathToListOfGeneratedIcons);
+const pathToListOfGeneratedIcons = path.join(__dirname, '..', '..', 'Fhi.Frontend.Demo', 'Fhi.Frontend.Style', 'src', 'fhi', 'icons', '_icon-map.GENERATED.scss');
 
-fs.readFile(pathToListOfGeneratedIcons, (err, data) => {
-  if (err) throw err;
-  console.log(data);
+let listOfGeneratedIcons = fs.readFile(pathToListOfGeneratedIcons, { encoding: 'utf-8' }, (error, data) => {
+  if (error) throw error;
+  return data;
 });
+console.log(listOfGeneratedIcons);
 
 const generateIconList = function() {
   // console.log(listOfGenaratedIcons);
