3f741a120bd176663a6023fc7c7755bbcee1c535
Bernt Andersen <bernt.andersen@fhi.no>
2023-03-15 16:14:10 +0100

    Update import paths

--- projects/fhi-angular-highcharts/src/public-api.ts
+++ projects/fhi-angular-highcharts/src/public-api.ts
@@ -5,6 +5,5 @@
 export * from './lib/fhi-angular-highcharts.component';
 export * from './lib/fhi-angular-highcharts.module';
 
-export { FhiDiagramSerie } from './lib/fhi-diagram/fhi-diagram-options.model';
-export { FhiDiagramOptions } from './lib/fhi-diagram/fhi-diagram-options.model';
-export { FhiDiagramTypes } from './lib/fhi-diagram/fhi-diagram-types';
+export * from './lib/fhi-diagram/fhi-diagram-options.model';
+export * from './lib/fhi-diagram/fhi-diagram-types';
