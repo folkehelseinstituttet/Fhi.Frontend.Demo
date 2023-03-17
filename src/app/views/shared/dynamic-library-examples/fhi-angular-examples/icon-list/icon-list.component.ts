e98a40189008f663134cf69d937041d29af37699
Bernt Andersen <bernt.andersen@fhi.no>
2023-03-17 12:31:02 +0100

    Stripped version

--- src/app/views/shared/dynamic-library-examples/fhi-angular-examples/icon-list/icon-list.component.ts
+++ src/app/views/shared/dynamic-library-examples/fhi-angular-examples/icon-list/icon-list.component.ts
@@ -1,49 +1,24 @@
-import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { Clipboard } from '@angular/cdk/clipboard';
-import { includedIcons } from 'src/MOCK_DB_DATA/library-items/icons/icon-set.GENERATED';
+import { includedIcons as Icons } from 'src/MOCK_DB_DATA/library-items/icons/icon-set.GENERATED';
 
 @Component({
   selector: 'app-icon-list',
   templateUrl: './icon-list.component.html'
 })
 export class IconListComponent {
-  @ViewChild('iconFilterInput') iconFilterInput: ElementRef;
-  
-  includedIcons: any = includedIcons;
-  filteredIcons: any = [...includedIcons];
-  iconFilter: string = '';
-  iconSizeSelected: string = 'md';
+  icons = [...Icons];
+  iconSizeSelected = 'md';
+  iconSizes = [
+    { size: 'sm', px: '16'},
+    { size: 'md', px: '24'},
+    { size: 'lg', px: '32'},
+    { size: 'xl', px: '40'}
+  ];
+  searchInput!: string;
 
-  constructor(private clipboard: Clipboard) {}
-
-  onFilterIcons(event: KeyboardEvent) {
-    this.filteredIcons = includedIcons.filter(x => x.includes(this.iconFilter));
-
-    if (event.key === 'Escape') {
-      this.onResetFilter();
-    }
-  }
-
-  onCopyByFocus(event: KeyboardEvent, iconToCopy: string) {
-    if (event.key === 'Enter') {
-      this.onCopyIconClass(iconToCopy);
-    }
-  }
-
-  onCopyIconClass(iconToCopy: string) {
-    console.log(iconToCopy, this.iconSizeSelected);
-    const iconTag = `<i class="icon-${iconToCopy} icon-${this.iconSizeSelected}"></i>`;
-    this.clipboard.copy(iconTag);
-  }
-
-  onResetFilter() {
-    this.iconFilter = '';
-    this.filteredIcons = [...includedIcons];
-    this.iconFilterInput.nativeElement.focus();
-  }
-
-  onChangeSize(iconSize: string) {
+  changeSize(iconSize: string) {
     this.iconSizeSelected = iconSize;
   }
+
 }
