808664447e8e4ff82c10d3ea83ab454486005646
Karl Granli <karl@gran.li>
2023-03-28 16:07:24 +0200

    Enhancement/improve presentation - preparing for styles v5 (#307)

--- src/MOCK_DB_DATA/library-items/color/colors-bootstrap.ts
+++ src/MOCK_DB_DATA/library-items/color/colors-bootstrap.ts
@@ -25,9 +25,6 @@ function getExampleHtml(): string {
   <div class="ds-color-palette__code ms-4 p-2">
     ${hex}
   </div>
-  <div class="ds-color-palette__code ms-4 p-2 d-none d-lg-block">
-    $${color}
-  </div>
 </div>`;
   }
 
