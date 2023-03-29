8e929257d3c709cd2b7b91e1b8f9749d44c28600
Bernt Andersen <bernt.andersen@fhi.no>
2023-03-29 10:07:03 +0200

    Reset format

--- src/app/views/shared/dynamic-library-examples/fhi-angular-examples/fhi-angular-highcharts-example/fhi-angular-highcharts-example.component.ts
+++ src/app/views/shared/dynamic-library-examples/fhi-angular-examples/fhi-angular-highcharts-example/fhi-angular-highcharts-example.component.ts
@@ -1,62 +1,61 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, Input, OnInit } from '@angular/core';
 
-import { MockDataService } from "./mock-data.service";
-import { MockData } from "./mock-data";
+import { MockDataService } from './mock-data.service';
+import { MockData } from './mock-data';
 
-import {
-  FhiDiagramOptions,
-  FhiDiagramTypes,
-  FhiDiagramType,
-  FhiDiagramTypeNavs,
-} from "@folkehelseinstituttet/angular-highcharts";
+import { FhiDiagramOptions, FhiDiagramTypes, FhiDiagramType, FhiDiagramTypeNavs } from '@folkehelseinstituttet/angular-highcharts';
+
 
 @Component({
-  selector: "app-fhi-angular-highcharts-example",
-  templateUrl: "./fhi-angular-highcharts-example.component.html",
+  selector: 'app-fhi-angular-highcharts-example',
+  templateUrl: './fhi-angular-highcharts-example.component.html'
 })
 export class FhiAngularHighchartsExampleComponent implements OnInit {
+
   @Input() itemId!: string;
   @Input() itemIds!: any;
 
   dataIsLoading = false;
   dataIsLoaded = false;
   diagramOptions: FhiDiagramOptions = {
-    title: "Dødsfall etter årsak, 2008 - 2018",
+    title: 'Dødsfall etter årsak, 2008 - 2018',
     diagramType: FhiDiagramTypes.table,
-    data: [],
-  };
+    data: []
+  }
 
-  constructor(private highchartsDataService: MockDataService) {}
+  constructor(private highchartsDataService: MockDataService) { }
 
   ngOnInit() {
     this.dataIsLoading = true;
     this.dataIsLoaded = false;
-    this.highchartsDataService.getData(MockData.TwoSeriesAar).subscribe({
-      next: (data) => {
-        if (this.itemId === this.itemIds.Highcharts) {
-          this.diagramOptions = {
-            ...this.diagramOptions,
-            data: data,
-            diagramType: FhiDiagramTypes.column,
-          };
-        } else {
-          this.diagramOptions = {
-            ...this.diagramOptions,
-            data: data,
-            diagramTypeNav: FhiDiagramTypeNavs.default,
-          };
-        }
-        this.dataIsLoading = false;
-        this.dataIsLoaded = true;
-      },
-      error: (e) => console.error(e),
-    });
+    this.highchartsDataService.getData(MockData.TwoSeriesAar)
+      .subscribe({
+        next: (data) => {
+          if (this.itemId === this.itemIds.Highcharts) {
+            this.diagramOptions = {
+              ...this.diagramOptions,
+              data: data,
+              diagramType: FhiDiagramTypes.column
+            };
+          } else {
+            this.diagramOptions = {
+              ...this.diagramOptions,
+              data: data,
+              diagramTypeNav: FhiDiagramTypeNavs.default
+            };
+          }
+          this.dataIsLoading = false;
+          this.dataIsLoaded = true;
+        },
+        error: (e) => console.error(e)
+      });
   }
 
   onDiagramTypeNav(diagramType: FhiDiagramType) {
     this.diagramOptions = {
       ...this.diagramOptions,
-      diagramType: diagramType,
+      diagramType: diagramType
     };
   }
+
 }
