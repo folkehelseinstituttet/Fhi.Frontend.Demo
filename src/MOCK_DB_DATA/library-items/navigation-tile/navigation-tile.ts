d94c7471286701eccefb8838160c9e33cb3694d2
Karl Granli <karl@gran.li>
2023-04-25 14:08:59 +0200

    Update navigation-tile.ts

--- src/MOCK_DB_DATA/library-items/navigation-tile/navigation-tile.ts
+++ src/MOCK_DB_DATA/library-items/navigation-tile/navigation-tile.ts
@@ -17,17 +17,17 @@ export const NavigationTile: LibraryItem[] = [{
 function getExampleHtml(): string {
   return `
 <div class="row gx-3 gx-sm-5">
-  <div class="col-auto col-sm-12 col-lg-6 mb-3 mb-md-5 mb-lg-0">
+  <div class="col-auto col-md-6 mb-3 mb-md-5">
     <a href="javascript:void(0)" class="fhi-navigation-tile">
-      <span class="fhi-navigation-tile__icon">
-        <i class="icon-environment"></i>
-      </span>
       <p class="fhi-navigation-tile__heading">Overskrift</p>
-      <p class="fhi-navigation-tile__description">Kort, beskrivende tekst som ikke går over mer enn tre linjer.</p>
+      <p class="fhi-navigation-tile__description">Kort, beskrivende tekst.</p>
+      <span class="fhi-navigation-tile__icon">
+        <i class="icon-check"></i>
+      </span>
     </a>
   </div>
 
-  <div class="col-auto col-sm-12 col-lg-6">
+  <div class="col-auto col-md-6 mb-3 mb-md-5">
     <a href="javascript:void(0)" class="fhi-navigation-tile">
       <p class="fhi-navigation-tile__heading">Overskrift</p>
       <p class="fhi-navigation-tile__description">Kort, beskrivende tekst som ikke går over mer enn tre linjer.</p>
@@ -42,17 +42,12 @@ function getExampleHtml(): string {
  */
 function getCodeHtml(): string | null {
   return `
-<a href="" class="fhi-navigation-tile">
+<a href="#" class="fhi-navigation-tile">
+  <p class="fhi-navigation-tile__heading">Overskrift</p>
+  <p class="fhi-navigation-tile__description">Kort, beskrivende tekst.</p>
   <span class="fhi-navigation-tile__icon">
-    <i class="icon-environment"></i>
+    <i class="icon-[name]"></i>
   </span>
-  <p class="fhi-navigation-tile__heading">Overskrift</p>
-  <p class="fhi-navigation-tile__description">Kort, beskrivende tekst som ikke går over mer enn tre linjer.</p>
-</a>
-
-<a href="" class="fhi-navigation-tile">
-  <p class="fhi-navigation-tile__heading">Overskrift</p>
-  <p class="fhi-navigation-tile__description">Kort, beskrivende tekst som ikke går over mer enn tre linjer.</p>
 </a>`;
 }
 
@@ -60,5 +55,7 @@ function getCodeHtml(): string | null {
  * Return null to remove Documentation from library-item.
  */
 function getDocumentationHtml(): string | null {
-  return null;
+  return `
+<p>Overskrift er påkrevd for å benytte denne komponenten, beskrivelse og ikon er valgfrie (teknisk sett).</p>
+<p>Dersom man ikke trenger/ønsker beskrivelse og ikon er nok ikke dette riktig komponent å benytte.</p>`;
 }
