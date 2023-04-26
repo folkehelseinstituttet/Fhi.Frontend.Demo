d63bad887fd7909fbe72489579f6c3403f3b5241
Bernt Andersen <bernt@proand.no>
2023-04-26 10:00:40 +0200

    Enhancement/highcharts table headers (#322)
    Create header rows based on the comma seperated header from input data

--- projects/fhi-angular-highcharts/src/lib/services/table.service.ts
+++ projects/fhi-angular-highcharts/src/lib/services/table.service.ts
@@ -1,16 +1,46 @@
 import { Injectable } from '@angular/core';
 
-import { Data, FhiDiagramSerie } from '../fhi-diagram/fhi-diagram.models';
+import { Data, FhiDiagramSerie, TableHeaderCell } from '../fhi-diagram/fhi-diagram.models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TableService {
 
-  getHeaderRow(series: FhiDiagramSerie[]): string[] {
-    const tableHeaderRow = series.map(s => s.name) as string[];
-    tableHeaderRow.unshift('');
-    return tableHeaderRow;
+  getHeaderRows(series: FhiDiagramSerie[]): TableHeaderCell[][] {
+    const commaSeparatedHeaderRow = series.map(s => s.name) as string[];
+    const tableHeaderRowCount = commaSeparatedHeaderRow[0].split(',').length;
+    const tableHeaderRows: TableHeaderCell[][] = new Array(tableHeaderRowCount);
+
+    for (let j = 0; j < tableHeaderRows.length; j++) {
+      const isLastRow = j + 1 === tableHeaderRowCount;
+      let previousCellName: string;
+      let colspanDivider = 0;
+      tableHeaderRows[j] = new Array(commaSeparatedHeaderRow.length);
+
+      for (let i = 0; i < commaSeparatedHeaderRow.length; i++) {
+        const splitHeader = commaSeparatedHeaderRow[i].split(',');
+        const currentCellName = splitHeader[j];
+
+        if (!isLastRow && currentCellName !== previousCellName) {
+          tableHeaderRows[j][i] = {
+            name: currentCellName,
+            colspan: commaSeparatedHeaderRow.length / 3
+          };
+          colspanDivider = colspanDivider + 1;
+        }
+        if (isLastRow) {
+          tableHeaderRows[j][i] = { name: currentCellName };
+        }
+        previousCellName = currentCellName;
+      }
+      tableHeaderRows[j].forEach(i => {
+        i.colspan = commaSeparatedHeaderRow.length / colspanDivider
+      });
+      tableHeaderRows[j].unshift(undefined); // Label column
+    }
+    tableHeaderRows[0][0] = { rowspan: tableHeaderRowCount }; // Label column
+    return tableHeaderRows;
   }
 
   getDataRows(series: FhiDiagramSerie[]): string[][] {
@@ -19,7 +49,7 @@ export class TableService {
 
     // Generate first column in rows
     for (let j = 0; j < dataArray.length; j++) {
-      tableBodyRows[j] = new Array(series.length + 1); // +1 = Label column
+      tableBodyRows[j] = new Array(series.length + 1); // + 1 = Label column
       tableBodyRows[j][0] = dataArray[j].name;
     }
 
@@ -27,7 +57,7 @@ export class TableService {
     for (let i = 0; i < series.length; i++) {
       dataArray = this.getDataArray(series[i]);
       for (let j = 0; j < dataArray.length; j++) {
-        tableBodyRows[j][i+1] = dataArray[j].y;
+        tableBodyRows[j][i + 1] = dataArray[j].y;
       }
     }
     return tableBodyRows;
