d3a66252aee76d2000c8fef798e68f462c7c0511
Bernt Andersen <bernt.andersen@fhi.no>
2023-06-13 11:13:53 +0200

    More documentation

--- src/MOCK_DB_DATA/library-items/highcharts/highcharts.ts
+++ src/MOCK_DB_DATA/library-items/highcharts/highcharts.ts
@@ -52,8 +52,9 @@ function getDocumentationHtml(): string | null {
 </p>
 
 <h3>API</h3>
+
 <h4>Inputs</h3>
-<div class="table-responsive mb-3">
+<div class="table-responsive mb-5">
   <table class="table table-striped">
     <thead>
       <tr>
@@ -70,30 +71,47 @@ function getDocumentationHtml(): string | null {
         <td><code>FhiDiagramOptions</code></td>
         <td>-</td>
         <td>yes</td>
-        <td>All properties needed to render a table, chart or map.</td>
+        <td>Alle properties som trengs for å rendre en tabell, graf eller et kart.</td>
       </tr>
+   </tbody>
+  </table>
+</div>
+
+<h4>FhiDiagramOptions, detaljer</h4>
+<div class="table-responsive mb-5">
+  <table class="table table-striped">
+    <thead>
       <tr>
-        <th>diagramOptions.creditsHref</th>
+        <th scope="col">Property</th>
+        <th scope="col">Type</th>
+        <th scope="col">Default</th>
+        <th scope="col">Required</th>
+        <th scope="col">Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>creditsHref</th>
         <td><code>string</code></td>
         <td>-</td>
         <td>no</td>
         <td>Lenke til kildehenvisning i footer.</td>
       </tr>
       <tr>
-        <th>diagramOptions.creditsText</th>
+        <th>creditsText</th>
         <td><code>string</code></td>
         <td>-</td>
         <td>no</td>
         <td>Kildehenvisning i footer.</td>
       </tr>
       <tr>
-        <th>diagramOptions.diagramTypeId</th>
+        <th>diagramTypeId</th>
         <td><code>string</code></td>
         <td>table</td>
         <td>no</td>
         <td>
           Default diagramtype når modul laster.
-          Lovlige diagramtyper:
+          Lovlige diagramTypeId'er:
           <ul>
             <li><code>table</code></li>
             <li><code>line</code></li>
@@ -102,6 +120,19 @@ function getDocumentationHtml(): string | null {
             <li><code>columnStacked</code></li>
             <li><code>barStacked</code></li>
             <li><code>pie</code></li>
+          </ul>
+        </td>
+      </tr>
+      <tr>
+        <th>diagramTypeNavId</th>
+        <td><code>string</code></td>
+        <td>-</td>
+        <td>no</td>
+        <td>
+          Id'en til den menyen en ønsker. HVis utelatt så rendres ingen meny.
+          Lovlige diagramTypeNavId'er:
+          <ul>
+            <li><code>default</code></li>
           </ul>
         </td>
       </tr>
@@ -115,19 +146,18 @@ function getDocumentationHtml(): string | null {
     <thead>
       <tr>
         <th scope="col">Output</th>
-        <th scope="col">Type</th>
-        <th scope="col">Default</th>
-        <th scope="col">Required</th>
+        <th scope="col">Event type</th>
         <th scope="col">Description</th>
       </tr>
     </thead>
     <tbody>
       <tr>
-        <th>[items]</th>
-        <td><code>Array&lt;FhiTreeViewNavigationItem&gt;</code></td>
-        <td><code>[]</code></td>
-        <td>yes</td>
-        <td>All items in your navigation tree.</td>
+        <th>diagramTypeNavigation</th>
+        <td><code>string</code></td>
+        <td>
+          Hvis diagramtypemeny vises vil dette eventet fyre hver gang en navigasjon utføres.
+          Eventverdien er en av de lovlige <code>diagramTypeId</code>'ene.
+        </td>
       </tr>
    </tbody>
   </table>
