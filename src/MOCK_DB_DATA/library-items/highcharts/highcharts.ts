588420177766c08e10fa31d10527547a1ab96454
Bernt Andersen <bernt.andersen@fhi.no>
2023-06-13 09:53:52 +0200

    Doc in progress

--- src/MOCK_DB_DATA/library-items/highcharts/highcharts.ts
+++ src/MOCK_DB_DATA/library-items/highcharts/highcharts.ts
@@ -32,10 +32,105 @@ function getCodeHtml(): string | null {
  */
 function getDocumentationHtml(): string | null {
   return `
+<h3>Kom i gang</h3>
 <p>
-  Informasjon om hvordan ta i bruk denne modulen finner du her:
   <a href="https://www.npmjs.com/package/@folkehelseinstituttet/angular-highcharts">
     @folkehelseinstituttet/angular-highcharts
   </a>
-</p>`;
+</p>
+<h4>Lenke til kildekode</h4>
+<p>
+  <a href="https://github.com/folkehelseinstituttet/Fhi.Frontend.Demo/tree/dev/projects/fhi-angular-highcharts">
+    projects/fhi-angular-highcharts
+  </a>
+</p>
+<h4>Lenke til demokoden</h4>
+<p>
+  <a href="https://github.com/folkehelseinstituttet/Fhi.Frontend.Demo/blob/dev/src/app/views/shared/dynamic-library-examples/fhi-angular-examples/fhi-angular-highcharts-example/fhi-angular-highcharts-example.component.ts">
+    fhi-angular-highcharts-example/fhi-angular-highcharts-example.component.ts
+  </a>
+</p>
+
+<h3>API</h3>
+<h4>Inputs</h3>
+<div class="table-responsive mb-3">
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">Input</th>
+        <th scope="col">Type</th>
+        <th scope="col">Default</th>
+        <th scope="col">Required</th>
+        <th scope="col">Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>diagramOptions</th>
+        <td><code>FhiDiagramOptions</code></td>
+        <td>-</td>
+        <td>yes</td>
+        <td>All properties needed to render a table, chart or map.</td>
+      </tr>
+      <tr>
+        <th>diagramOptions.creditsHref</th>
+        <td><code>string</code></td>
+        <td>-</td>
+        <td>no</td>
+        <td>Lenke til kildehenvisning i footer.</td>
+      </tr>
+      <tr>
+        <th>diagramOptions.creditsText</th>
+        <td><code>string</code></td>
+        <td>-</td>
+        <td>no</td>
+        <td>Kildehenvisning i footer.</td>
+      </tr>
+      <tr>
+        <th>diagramOptions.diagramTypeId</th>
+        <td><code>string</code></td>
+        <td>table</td>
+        <td>no</td>
+        <td>
+          Default diagramtype når modul laster.
+          Lovlige diagramtyper:
+          <ul>
+            <li><code>table</code></li>
+            <li><code>line</code></li>
+            <li><code>column</code></li>
+            <li><code>bar</code></li>
+            <li><code>columnStacked</code></li>
+            <li><code>barStacked</code></li>
+            <li><code>pie</code></li>
+          </ul>
+        </td>
+      </tr>
+   </tbody>
+  </table>
+</div>
+
+<h4>Outputs</h3>
+<div class="table-responsive">
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">Output</th>
+        <th scope="col">Type</th>
+        <th scope="col">Default</th>
+        <th scope="col">Required</th>
+        <th scope="col">Description</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th>[items]</th>
+        <td><code>Array&lt;FhiTreeViewNavigationItem&gt;</code></td>
+        <td><code>[]</code></td>
+        <td>yes</td>
+        <td>All items in your navigation tree.</td>
+      </tr>
+   </tbody>
+  </table>
+</div>`;
+
 }
