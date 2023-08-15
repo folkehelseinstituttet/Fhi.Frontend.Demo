16ff25c6d7de0d2b4d1c40f3a302158f79720f6f
Karl Granli <karl@gran.li>
2023-08-15 13:19:06 +0200

    Examples with interactive intent

--- src/MOCK_DB_DATA/library-items/tags/tag-state-dot.ts
+++ src/MOCK_DB_DATA/library-items/tags/tag-state-dot.ts
@@ -16,30 +16,63 @@ export const TagStateDot: LibraryItem[] = [{
  */
 function getExampleHtml(): string {
   return `
-<p>
-  <span class="fhi-tag fhi-tag--state-dot">
-    <i class="fhi-tag--state-dot__icon--info"></i>
-    System 1 er klart
-  </span>
-</p>
-<p>
-  <span class="fhi-tag fhi-tag--state-dot">
-    <i class="fhi-tag--state-dot__icon--on"></i>
-    System 2 slått på
-  </span>
-</p>
-<p>
-  <span class="fhi-tag fhi-tag--state-dot">
-    <i class="fhi-tag--state-dot__icon--warning"></i>
-    System 3 - advarsel
-  </span>
-</p>
-<p>
-  <span class="fhi-tag fhi-tag--state-dot">
-    <i class="fhi-tag--state-dot__icon--off"></i>
-    System 4 - av
-  </span>
-</p>`;
+<div class="row">
+  <div class="col-auto mb-5 mb-sm-0">
+    <h3 class="h5 mb-3">Kun tekst</h3>
+    <p>
+      <span class="fhi-tag fhi-tag--state-dot">
+        <i class="fhi-tag--state-dot__icon--info"></i>
+        System 1 er klart
+      </span>
+    </p>
+    <p>
+      <span class="fhi-tag fhi-tag--state-dot">
+        <i class="fhi-tag--state-dot__icon--on"></i>
+        System 2 slått på
+      </span>
+    </p>
+    <p>
+      <span class="fhi-tag fhi-tag--state-dot">
+        <i class="fhi-tag--state-dot__icon--warning"></i>
+        System 3 - advarsel
+      </span>
+    </p>
+    <p>
+      <span class="fhi-tag fhi-tag--state-dot">
+        <i class="fhi-tag--state-dot__icon--off"></i>
+        System 4 - av
+      </span>
+    </p>
+  </div>
+
+  <div class="col-auto">
+    <h3 class="h5 mb-3">Knapp/lenke</h3>
+    <p>
+      <button class="fhi-tag fhi-tag--state-dot">
+        <i class="fhi-tag--state-dot__icon--info"></i>
+        <span class="fhi-tag--state-dot__text">System 1 er klart</span>
+      </button>
+    </p>
+    <p>
+      <a href="#" class="fhi-tag fhi-tag--state-dot">
+        <i class="fhi-tag--state-dot__icon--on"></i>
+        <span class="fhi-tag--state-dot__text">System 2 slått på</span>
+      </a>
+    </p>
+    <p>
+      <button class="fhi-tag fhi-tag--state-dot">
+        <i class="fhi-tag--state-dot__icon--warning"></i>
+        <span class="fhi-tag--state-dot__text">System 3 - advarsel</span>
+      </button>
+    </p>
+    <p>
+      <button class="fhi-tag fhi-tag--state-dot">
+        <i class="fhi-tag--state-dot__icon--off"></i>
+        <span class="fhi-tag--state-dot__text">System 4 - av</span>
+      </button>
+    </p>
+  </div>
+</div>`;
 }
 
 /*
@@ -53,15 +86,15 @@ function getCodeHtml(): string | null {
   System 1 er klart
 </span>
 
-<span class="fhi-tag fhi-tag--state-dot">
+<a href="#" class="fhi-tag fhi-tag--state-dot">
   <i class="fhi-tag--state-dot__icon--on"></i>
-  System 2 slått på
-</span>
+  <span class="fhi-tag--state-dot__text">System 2 slått på</span>
+</a>
 
-<span class="fhi-tag fhi-tag--state-dot">
+<button class="fhi-tag fhi-tag--state-dot">
   <i class="fhi-tag--state-dot__icon--warning"></i>
-  System 3 - advarsel
-</span>
+  <span class="fhi-tag--state-dot__text">System 3 - advarsel</span>
+</button>
 
 <span class="fhi-tag fhi-tag--state-dot">
   <i class="fhi-tag--state-dot__icon--off"></i>
