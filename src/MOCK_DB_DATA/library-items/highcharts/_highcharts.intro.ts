a7aa5fc4295ada07907af2782c59a948652c48b2
Bernt Andersen <bernt.andersen@fhi.no>
2023-09-29 08:40:34 +0200

    Update text in Highcharts doc

--- src/MOCK_DB_DATA/library-items/highcharts/_highcharts.intro.ts
+++ src/MOCK_DB_DATA/library-items/highcharts/_highcharts.intro.ts
@@ -5,12 +5,16 @@ export const HighchartsIntro = `
   Folkehelseintitutte har valgt å bruker Highcharts for å vise diagrammer med FHI-design.
 </p>
 <p>
-  For å gjøre det så enkelt som mulig å vise diagrammer i en Angular applikasjonen er
-  det blitt utviklet en egen modul som tilgjengliggjør et veldig begrenset, men gjennomtenkt utvalg av det Highcharts har å tilby.
+  For å gjøre det så enkelt som mulig å vise diagrammer i en Angular applikasjonen på en slik
+  måte som våre designere ønsker, er det blitt utviklet en komponent som tilgjengeliggjør et
+  bearbeidet utvalg av det <a href="https://www.highcharts.com">Highcharts</a> har å tilby.
+  Den baserer seg på <a href="https://github.com/highcharts/highcharts-angular">Highcharts Angular</a>.
 <p>
 </p>
-  Siden det å vise diagrammer i en web-applikasjon er utfordrene å få til på en god måte, så er denne modulen i stadig utvikling,
-  vi jobber med å fikse feil og mangler vi vet om, og håper alle som tar denne modulen i bruk vil hjelpe oss med å gjøre den bedre.
+  Det å vise diagrammer i en web-applikasjon er i utgangspunktet utfordrene å få til på en god måte.
+  Ekstra utfordrene blir det siden vi må støtte dynamiske data, i tillegg til riktig design og universell
+  utforming. Vi jobber med å fikse feil og mangler vi vet om, og håper alle som tar i bruk
+  "FHI Angular Highcharts" vil hjelpe oss med å gjøre denne komponenten bedre.
 </p>
 
 <h2 class="h3">Nyttige lenker</h2>
@@ -34,16 +38,6 @@ export const HighchartsIntro = `
   <li>
     <a href="${CONST.FhiAngularHighchartsNpmUrl}">
       NPM-pakke
-    </a>
-  </li>
-  <li>
-    <a href="https://www.highcharts.com">
-      highcharts.com
-    </a>
-  </li>
-  <li>
-    <a href="https://github.com/highcharts/highcharts-angular">
-      Official minimal Highcharts wrapper for Angular
     </a>
   </li>
 </ul>`;
