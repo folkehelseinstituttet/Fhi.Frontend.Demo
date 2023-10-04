8f0291402a62be25e49bc10c312fa67181807e3b
Karl Granli <karl@gran.li>
2023-10-04 12:52:57 +0200

    Basic setup

--- projects/fhi-angular-components/src/lib/fhi-week-selector/fhi-week-selector.component.ts
+++ projects/fhi-angular-components/src/lib/fhi-week-selector/fhi-week-selector.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { NgbAlertModule, NgbDateParserFormatter, NgbDatepickerI18n, NgbDatepickerModule, NgbDateStruct } from '@ng-bootstrap/ng-bootstrap';
+import { format, formatISO, isValid, toDate } from 'date-fns';
 
 @Component({
   selector: 'fhi-week-selector',
   templateUrl: './fhi-week-selector.component.html',
-  standalone: true
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbDatepickerModule,
+    NgbAlertModule
+  ]
 })
 export class FhiWeekSelectorComponent {
+  @Input() date?: string;
+  @Input() label: string = 'Velg uke';
+  @Input() maximumDate?: string;
+  @Input() minimumDate?: string;
 
+  @Output() weekSelect = new EventEmitter<string>();
+
+  errorMsg: string = '';
+  maxDate: NgbDateStruct;
+  minDate: NgbDateStruct;
+  model: NgbDateStruct;
+  uniqueId: string = 'weekselector_' + Math.random().toString(36).substring(2, 20);
+  week: string;
+  weekIsValid: boolean = true;
+
+  ngOnInit() {
+    
+  }
+
+  onDirectInputWeek() {
+
+  }
+
+  onWeekSelection(date: any) {
+    console.log(this.week);
+  }
 }
