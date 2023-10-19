c8551d3b27214cfe69856afeecb92cfe37c69d34
Karl Granli <karl@gran.li>
2023-10-19 14:40:01 +0200

    Cleanup after merge from other branch

--- projects/fhi-angular-components/src/lib/fhi-year-selector/fhi-year-selector.component.ts
+++ projects/fhi-angular-components/src/lib/fhi-year-selector/fhi-year-selector.component.ts
@@ -15,22 +15,16 @@ export class FhiYearSelectorComponent {
 
   @Output() yearSelect = new EventEmitter<string>();
 
-  // Review comment: No need for years as @input, what we need is maxYear and minYear
   yearList: FhiAutosuggestItem[] = [];
 
   selectedYear: FhiAutosuggestItem;
   selectedYearId: number;
-
-  // Review comment: fixed "missing id bug" in FhiAutosuggestComponent, so uniqueId 
-  //                 (and [labelForId]) can be skippet FhiYearSelectorComponent...
-  // uniqueId: string = 'yearselect_' + Math.random().toString(36).substring(2, 20);
 
   ngOnInit() {
     this.populateYearList();
   }
   
   onSelectedItemChange(selectedYearId: number) {
-    // console.log('selectedYearId', selectedYearId);
     this.selectedYear = this.yearList
       .find((year: FhiAutosuggestItem) => year.id === selectedYearId);
     this.yearSelect.emit(this.selectedYear.name);
@@ -40,8 +34,6 @@ export class FhiYearSelectorComponent {
     for (let i = 1900; i <= 2050; i++) {
       const year: string = `${i}`;
       this.yearList.push({
-        // Review comment: My bad... the model FhiAutosuggestItem was wrong all the time; string-id's
-        // has never worked, which is why you got problems with the emit-value.
         id: toNumber(year),
         name: year
       });
@@ -49,14 +41,6 @@ export class FhiYearSelectorComponent {
     if (this.year) {
       this.selectedYear = this.yearList.find((year: FhiAutosuggestItem) => year.name === this.year);
       this.selectedYearId = this.selectedYear.id;
-      // Review comment: we don't emit anything before user has interacted
-      // this.selectInitialYear();
     }
   }
-
-  // private selectInitialYear() {
-  //   this.selectedYear = this.yearList.find((year: FhiAutosuggestItem) => year.name === this.year);
-  //   this.selectedYearId = toNumber(this.selectedYear['id']);
-  //   this.yearSelect.emit(this.selectedYear['name']);
-  // }
 }
