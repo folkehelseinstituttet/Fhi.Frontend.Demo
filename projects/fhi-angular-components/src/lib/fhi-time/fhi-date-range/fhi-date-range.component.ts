af98d164d6d36efa747ebb41e8462f9d4c1b8d79
Karl Granli <karl@gran.li>
2023-10-27 13:55:50 +0200

    Markup/styling start

--- projects/fhi-angular-components/src/lib/fhi-time/fhi-date-range/fhi-date-range.component.ts
+++ projects/fhi-angular-components/src/lib/fhi-time/fhi-date-range/fhi-date-range.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { FhiDatepickerComponent } from '@folkehelseinstituttet/angular-components';
+import { FhiDatepickerComponent } from '../../fhi-datepicker/fhi-datepicker.component';
 
 @Component({
   selector: 'fhi-date-range',
@@ -10,5 +10,6 @@ import { FhiDatepickerComponent } from '@folkehelseinstituttet/angular-component
   templateUrl: './fhi-date-range.component.html',
 })
 export class FhiDateRangeComponent {
-
+  @Input() labelFromDate: string = 'Fra dato';
+  @Input() labelToDate: string = 'Til dato';
 }
