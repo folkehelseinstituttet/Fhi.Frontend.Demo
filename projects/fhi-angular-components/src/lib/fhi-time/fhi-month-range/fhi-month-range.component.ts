5285edfc0a8e8b45cc57ab6071c8638b310518da
Karl Granli <karl@gran.li>
2023-11-06 15:27:03 +0100

    Month range without month selector

--- projects/fhi-angular-components/src/lib/fhi-time/fhi-month-range/fhi-month-range.component.ts
+++ projects/fhi-angular-components/src/lib/fhi-time/fhi-month-range/fhi-month-range.component.ts
@@ -1,35 +1,58 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { toNumber } from 'lodash-es';
+import { isValid } from 'date-fns';
 
-import { FhiMonthSelectorComponent } from '../fhi-month-selector/fhi-month-selector.component';
+import { FhiAutosuggestModule } from '../../fhi-autosuggest/fhi-autosuggest.module';
+import { FhiAutosuggestItem } from '../../fhi-autosuggest/fhi-autosuggest.model';
+import { FhiYearSelectorComponent } from '../../fhi-year-selector/fhi-year-selector.component';
 import { FhiConstantsService } from '../../shared-services/fhi-constants.service';
 
 @Component({
   selector: 'fhi-month-range',
   standalone: true,
-  imports: [ CommonModule, FhiMonthSelectorComponent ],
+  imports: [
+    CommonModule,
+    FhiAutosuggestModule,
+    FhiYearSelectorComponent
+  ],
   templateUrl: './fhi-month-range.component.html',
   providers: [ FhiConstantsService ]
 })
 export class FhiMonthRangeComponent {
-  @Input() fieldsetLegendFrom: string = 'Fra';
-  @Input() fieldsetLegendTo: string = 'Til';
-  @Input() labelMonthFrom: string = 'måned';
-  @Input() labelMonthTo: string = 'måned';
-  @Input() labelYearFrom: string = 'år';
-  @Input() labelYearTo: string = 'år';
   @Input() maxYear: number = this.FHI_CONSTANTS.MAX_YEAR;
   @Input() minYear: number = this.FHI_CONSTANTS.MIN_YEAR;
-
+  
   @Output() monthRangeSelect = new EventEmitter<Object>();
-
+  
+  fieldsetLegendFrom: string = 'Fra måned';
+  fieldsetLegendTo: string = 'Til måned';
+  labelMonthFrom: string = 'måned';
+  labelMonthTo: string = 'måned';
+  labelYearFrom: string = 'år';
+  labelYearTo: string = 'år';
   maxYearFrom: number;
   minYearTo: number;
-  monthFrom: string;
-  monthTo: string;
+  monthListFull: FhiAutosuggestItem[] = [
+    { id: 1, name: 'Januar' },
+    { id: 2, name: 'Februar' },
+    { id: 3, name: 'Mars' },
+    { id: 4, name: 'April' },
+    { id: 5, name: 'Mai' },
+    { id: 6, name: 'Juni' },
+    { id: 7, name: 'Juli' },
+    { id: 8, name: 'August' },
+    { id: 9, name: 'September' },
+    { id: 10, name: 'Oktober' },
+    { id: 11, name: 'November' },
+    { id: 12, name: 'Desember'}
+  ];
+  monthList: FhiAutosuggestItem[] = this.monthListFull;
+  monthFrom: number;
+  monthTo: number;
   validRange: boolean = true;
+  yearFrom: string;
+  yearTo: string;
 
   constructor(private FHI_CONSTANTS: FhiConstantsService) {}
 
@@ -38,28 +61,42 @@ export class FhiMonthRangeComponent {
     this.minYearTo = this.minYear;
   }
 
+  onYearFromSelect(event: any) {
+    this.yearFrom = event;
+    this.minYearTo = event;
+    this.checkValidity();
+  }
+
+  onYearToSelect(event: any) {
+    this.yearTo = event;
+    this.maxYearFrom = event;
+    this.checkValidity();
+  }
+
   onMonthFromSelect(event: any) {
     this.monthFrom = event;
-    const dashPos = event.indexOf('-');
-    this.minYearTo = toNumber(event.substring(0, dashPos));
     this.checkValidity();
   }
 
   onMonthToSelect(event: any) {
     this.monthTo = event;
-    const dashPos = event.indexOf('-');
-    this.maxYearFrom = toNumber(event.substring(0, dashPos));
     this.checkValidity();
   }
 
   private checkValidity() {
-    const fromMonth: any = new Date(this.monthFrom);
-    const toMonth: any = new Date(this.monthTo);
+    const fromMonth: any = new Date(Number(this.yearFrom), this.monthFrom -1);
+    const toMonth: any = new Date(Number(this.yearTo), this.monthTo, 0);
     
     if (toMonth - fromMonth >= 0) {
       this.monthRangeSelect.emit({
-        fromYearMonth: this.monthFrom,
-        toYearMonth: this.monthTo
+        start: {
+          year: Number(this.yearFrom),
+          month: this.monthFrom
+        },
+        end: {
+          year: Number(this.yearTo),
+          month: this.monthTo
+        }
       });
       this.validRange = true;
     }
