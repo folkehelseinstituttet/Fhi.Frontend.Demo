d98ee3a49ffc967db1c115a061225d8533e303d0
Karl Granli <karl@gran.li>
2023-11-07 16:46:44 +0100

    New/angular components/month range (#413)

--- src/MOCK_DB_DATA/library-items/time-selectors/_time-selectors.data.ts
+++ src/MOCK_DB_DATA/library-items/time-selectors/_time-selectors.data.ts
@@ -7,6 +7,7 @@ import { TimeSelectorDatepicker } from './time-selector-datepicker';
 import { TimeSelectorDateAndTime } from './time-selector-date-and-time';
 import { TimeSelectorDateRange } from './time-selector-date-range';
 import { TimeSelectorMonth } from './time-selector-month';
+import { TimeSelectorMonthRange } from './time-selector-month-range';
 import { TimeSelectorWeek } from './time-selector-week';
 import { TimeSelectorWeekRange } from './time-selector-week-range';
 import { TimeSelectorYear } from './time-selector-year';
@@ -17,6 +18,7 @@ const TimeSelectors: LibraryItem[] = [
   ...TimeSelectorDateAndTime,
   ...TimeSelectorDateRange,
   ...TimeSelectorMonth,
+  ...TimeSelectorMonthRange,
   ...TimeSelectorWeek,
   ...TimeSelectorWeekRange,
   ...TimeSelectorYear,
---
+++ src/MOCK_DB_DATA/library-items/time-selectors/time-selector-month-range.ts
@@ -0,0 +1,34 @@
+import { LibraryItem, LibraryItemType } from 'src/app/views/shared/models/library-item.model';
+import { LibraryItemsSharedData as ITEMS } from '../library-items-shared-data';
+
+export const TimeSelectorMonthRange: LibraryItem[] = [{
+  id: ITEMS.TimeSelectorMonthRange.id,
+  title: ITEMS.TimeSelectorMonthRange.title,
+  type: LibraryItemType.angular,
+  exampleHtml: getExampleHtml(),
+  codeHtml: getCodeHtml(),
+  documentationHtml: getDocumentationHtml()
+}];
+
+
+/*
+ * Return value is ignored if LibraryItemType is not html
+ */
+function getExampleHtml(): string {
+  return ``;
+}
+  
+/*
+ * Return empty string to use a copy of exampleHtml as codeHtml.
+ * Return null to remove Code from library-item.
+ */
+function getCodeHtml(): string | null {
+  return `<fhi-month-range></fhi-month-range>`;
+}
+
+/*
+ * Return null to remove Documentation from library-item.
+ */
+function getDocumentationHtml(): string | null {
+  return null;
+}
