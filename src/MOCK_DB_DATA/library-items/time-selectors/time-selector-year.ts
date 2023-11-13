ad5068c33bf346e331ec5f273953da723aad6580
Bernt Andersen <bernt.andersen@fhi.no>
2023-11-13 15:41:08 +0100

    Update to Prettier formatting (TimeSelectorsComponent)

--- src/MOCK_DB_DATA/library-items/time-selectors/time-selector-year.ts
+++ src/MOCK_DB_DATA/library-items/time-selectors/time-selector-year.ts
@@ -1,15 +1,16 @@
 import { LibraryItem, LibraryItemType } from 'src/app/views/shared/models/library-item.model';
 import { LibraryItemsSharedData as ITEMS } from '../library-items-shared-data';
 
-export const TimeSelectorYear: LibraryItem[] = [{
-  id: ITEMS.TimeSelectorYear.id,
-  title: ITEMS.TimeSelectorYear.title,
-  type: LibraryItemType.angular,
-  exampleHtml: getExampleHtml(),
-  codeHtml: getCodeHtml(),
-  documentationHtml: getDocumentationHtml()
-}];
-
+export const TimeSelectorYear: LibraryItem[] = [
+  {
+    id: ITEMS.TimeSelectorYear.id,
+    title: ITEMS.TimeSelectorYear.title,
+    type: LibraryItemType.angular,
+    exampleHtml: getExampleHtml(),
+    codeHtml: getCodeHtml(),
+    documentationHtml: getDocumentationHtml(),
+  },
+];
 
 /*
  * Return value is ignored if LibraryItemType is not html
@@ -17,17 +18,19 @@ export const TimeSelectorYear: LibraryItem[] = [{
 function getExampleHtml(): string {
   return ``;
 }
-  
+
 /*
  * Return empty string to use a copy of exampleHtml as codeHtml.
  * Return null to remove Code from library-item.
  */
 function getCodeHtml(): string | null {
   return `
-<fhi-year-selector [label]="'Velg år'"
-                   [initialYearList]="yearList"
-                   [initialSelectedYear]="'2011'"
-                   (selectedYearChange)="selectYear($event)"></fhi-year-selector>`;
+<fhi-year-selector 
+  [label]="'Velg år'" 
+  [year]="'2011'" 
+  (yearSelect)="onYearSelect($event)"
+></fhi-year-selector>
+`;
 }
 
 /*
