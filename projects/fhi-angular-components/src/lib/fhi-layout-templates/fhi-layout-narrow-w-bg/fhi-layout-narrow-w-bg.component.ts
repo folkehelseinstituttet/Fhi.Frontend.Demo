3c70a8c739d17813cfb9b2282265f5d489c283d3
Karl Granli <karl@gran.li>
2023-11-24 10:15:11 +0100

    Renaming selector

--- projects/fhi-angular-components/src/lib/fhi-layout-templates/fhi-layout-narrow-w-bg/fhi-layout-narrow-w-bg.component.ts
+++ projects/fhi-angular-components/src/lib/fhi-layout-templates/fhi-layout-narrow-w-bg/fhi-layout-narrow-w-bg.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'fhi-layout-simple-narrow',
+  selector: 'fhi-layout-narrow-w-bg',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './fhi-layout-narrow-w-bg.component.html',
