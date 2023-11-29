50168d6996bf5c3435033ea9a7e970fe155e3c50
Bernt Andersen <bernt.andersen@fhi.no>
2023-11-29 18:33:40 +0100

    Reuse minYear

--- projects/fhi-angular-components/src/lib/fhi-time/shared/time.constants.ts
+++ projects/fhi-angular-components/src/lib/fhi-time/shared/time.constants.ts
@@ -8,7 +8,7 @@ export class TimeConstants {
   static maxYear = getYear(this.dateToday);
 
   // TODO: maxWeek need to use the week-utility to check for 53 weeks in year...
-  static minWeek = { year: 1900, week: 1 };
+  static minWeek = { year: this.minYear, week: 1 };
   static maxWeek = { year: this.maxYear, week: 52 };
 
   static minDate = new Date(this.minYear, 0);
