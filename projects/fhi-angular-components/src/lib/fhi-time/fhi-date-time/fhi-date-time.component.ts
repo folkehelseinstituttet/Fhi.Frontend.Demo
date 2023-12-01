5f2fdd99ade27fe55c5982d2a8da0fad9f9440ee
Bernt Andersen <bernt@proand.no>
2023-12-01 09:48:58 +0100

    Enhancement/fhi-time--more-doc (#429)

--- projects/fhi-angular-components/src/lib/fhi-time/fhi-date-time/fhi-date-time.component.ts
+++ projects/fhi-angular-components/src/lib/fhi-time/fhi-date-time/fhi-date-time.component.ts
@@ -1,9 +1,12 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { FhiDatepickerComponent } from '../fhi-datepicker/fhi-datepicker.component';
-import { formatISO, isValid, parseISO, getTime } from 'date-fns';
+import { FhiDateTime } from '../shared/models/fhi-date-time.model';
+import { FhiDate } from '../shared/models/fhi-date.model';
+import { toNumber } from 'lodash-es';
+import { FhiTime } from '../shared/models/fhi-time.model';
 
 @Component({
   standalone: true,
@@ -11,100 +14,127 @@ import { formatISO, isValid, parseISO, getTime } from 'date-fns';
   templateUrl: './fhi-date-time.component.html',
   imports: [CommonModule, FhiDatepickerComponent, FormsModule],
 })
-export class FhiDateTimeComponent {
-  @Input() date?: string;
-  @Input() label?: string = 'Velg dato og tidspunkt';
-  @Input() initialTime?: string;
+export class FhiDateTimeComponent implements OnInit {
+  @Input() label = 'Velg dato og tidspunkt';
+  @Input() dateTime: FhiDateTime;
+  // @Input() minDateTime: FhiDateTime; // TODO
+  // @Input() maxDateTime: FhiDateTime; // TODO
 
-  @Output() dateAndTimeSelect = new EventEmitter<string>();
+  @Output() dateTimeSelect = new EventEmitter<FhiDateTime>();
 
+  date!: FhiDate;
+  isValid = true;
+  timeModel!: string;
+  unvalidatedDate!: FhiDate;
+
+  errorMsg: string;
   thisTimeId: string = 'time_' + Math.random().toString(36).substring(2, 20);
 
-  currentDateTimeString: string;
-  dateSelected: string;
-  dateAndTimeIsValid = true;
-  dateAndTimeSelected: string;
-  errorMsg: string;
-  timeEntered: string;
-
   ngOnInit() {
-    if (this.initialTime) {
-      this.timeEntered = this.initialTime;
+    if (this.dateTime) {
+      this.setInitDate();
+      this.setInitTime();
     }
   }
 
-  onGetDate(date: string) {
-    this.dateSelected = date;
-    this.concatenateDateAndTime();
+  onDateSelect(date: FhiDate) {
+    this.unvalidatedDate = date;
+    this.validateAndEmit();
   }
 
-  onTimeChange(event: any) {
-    const key = event.key;
-    const allowedKeys = '0,1,2,3,4,5,6,7,8,9,:';
-
-    if (allowedKeys.indexOf(key) >= 0) {
-      if (this.timeEntered.length <= 5) {
-        this.currentDateTimeString = this.timeEntered;
-
-        if (key !== ':' && this.timeEntered.length === 2) {
-          this.timeEntered = this.timeEntered + ':';
-        }
-
-        if (key === ':') {
-          if (this.timeEntered.length === 1) {
-            this.timeEntered = '';
-          }
-          if (this.timeEntered.length === 2) {
-            this.timeEntered = '0' + this.timeEntered;
-          }
-          if (this.timeEntered.length > 3) {
-            this.timeEntered = this.timeEntered.substring(0, 3);
-          }
-        }
-      } else {
-        this.timeEntered = this.currentDateTimeString;
-      }
-
-      if (this.timeEntered.length === 5) {
-        this.checkIfTimeIsValid();
-        this.errorMsg = 'Ugyldig tid';
-      }
-    } else {
-      if (key.length === 1) {
-        this.timeEntered = this.timeEntered.replace(key, '');
-      }
-      if (key === 'Meta') {
-        const timePattern = new RegExp('^[0-9][0-9][:][0-9][0-9]$');
-        if (!timePattern.test(this.timeEntered)) {
-          this.timeEntered = undefined;
-        }
-      }
-    }
-    if (this.timeEntered === '') {
-      this.timeEntered = undefined;
-      this.dateAndTimeIsValid = true;
-    }
-    this.concatenateDateAndTime();
+  onBlur() {
+    this.validateAndEmit();
   }
 
-  private checkIfTimeIsValid() {
-    function isValidDate(dateString: string) {
-      return !isNaN(Date.parse(dateString));
-    }
-    const timeToCheck = this.dateSelected + 'T' + this.timeEntered;
-
-    this.dateAndTimeIsValid = isValidDate(timeToCheck);
+  onEnter() {
+    this.validateAndEmit();
   }
 
-  private concatenateDateAndTime() {
-    this.dateAndTimeSelected = this.dateSelected + 'T' + this.timeEntered;
-    const parsedISO = parseISO(this.dateAndTimeSelected);
-
-    if (isValid(parsedISO)) {
-      const formattedISO = formatISO(parsedISO).split('+')[0] + 'Z';
-      this.dateAndTimeSelect.emit(formattedISO);
-    } else {
-      this.dateAndTimeSelect.emit(undefined);
+  onFocus() {
+    if (!this.isValid) {
+      this.isValid = true;
     }
+  }
+
+  onKeyup(event: KeyboardEvent) {
+    this.addColon(event.key);
+  }
+
+  private validateAndEmit() {
+    const validDateTime = this.getValidDateTime(this.unvalidatedDate);
+    if (validDateTime !== undefined) {
+      this.isValid = true;
+      this.dateTimeSelect.emit(validDateTime);
+      return;
+    }
+    this.isValid = false;
+    this.errorMsg = this.getInvalidFeedbackText();
+  }
+
+  private getValidDateTime(date: FhiDate | undefined): FhiDateTime | undefined {
+    if (date === undefined) {
+      return;
+    }
+    if (!this.isValidDateString()) {
+      return;
+    }
+    const time = this.getValidTime();
+    if (!time) {
+      return;
+    }
+    return {
+      date: date,
+      time: time,
+    };
+  }
+
+  private getValidTime(): FhiTime | undefined {
+    const parts = this.timeModel.split(':');
+    const hour = toNumber(parts[0]);
+    const minute = toNumber(parts[1]);
+    if (isNaN(hour) || isNaN(minute)) {
+      return;
+    }
+    if (hour > 23 || minute > 59) {
+      return;
+    }
+    return { hour: hour, minute: minute };
+  }
+
+  private isValidDateString() {
+    const timePattern = new RegExp('^[0-9][0-9][:][0-9][0-9]$');
+    if (!timePattern.test(this.timeModel)) {
+      return false;
+    }
+    return true;
+  }
+
+  private setInitDate() {
+    this.date = this.dateTime.date;
+    this.unvalidatedDate = this.dateTime.date;
+  }
+
+  private setInitTime() {
+    let hour = this.dateTime.time.hour.toString();
+    let minute = this.dateTime.time.minute.toString();
+    hour = hour.length > 1 ? hour : hour.padStart(2, '0');
+    minute = minute.length > 1 ? minute : minute.padStart(2, '0');
+    this.timeModel = `${hour}:${minute}`;
+  }
+
+  private addColon(eventKey: string) {
+    const keys = ['Backspace', 'Delete', ':'];
+    if (
+      !keys.find((key) => key === eventKey) &&
+      !this.timeModel.includes(':') &&
+      this.timeModel.length === 2
+    ) {
+      this.timeModel = this.timeModel + ':';
+    }
+  }
+
+  private getInvalidFeedbackText(): string {
+    return `Du har lagt inn et klokkeslett som er ugylidg, eller har feil format.
+Korrekt format er <strong>tt:mm</strong>`;
   }
 }
---
+++ projects/fhi-angular-components/src/lib/fhi-time/fhi-datepicker/README.md
@@ -0,0 +1,19 @@
+# FHI Datepicker
+
+## API
+
+### Input
+
+| Input              | Type      | Default    | Required | Description |
+| ------------------ | --------- | ---------- | -------- | ----------- |
+| `id`               | `string`  | random     | no       | Id to associate control with label. |
+| `label`            | `string`  | `Velg uke` | no       | Label above the ng-datepicker input. |
+| `date`             | `FhiDate` | -          | no       | Object containing year, month and day as numbers. |
+| `minDate`          | `FhiDate` | -          | no       | Object containing year, month and day as numbers for the first possible date to select. |
+| `maxDate`          | `FhiDate` | -          | no       | Object containing year, month and day as numbers for the last possible date to select. |
+
+### Output
+
+| Output       | Type      | Description |
+| ------------ | --------- | ----------- |
+| `dateSelect` | `FhiDate` | Object containing year, month and day as numbers. |
