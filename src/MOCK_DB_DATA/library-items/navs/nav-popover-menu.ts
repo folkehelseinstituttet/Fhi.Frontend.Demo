63d9de996697123d0ef7526cf8386ab7a73c0308
yleaxeman <karl@gran.li>
2024-01-10 14:00:42 +0100

    Documentation

--- src/MOCK_DB_DATA/library-items/navs/nav-popover-menu.ts
+++ src/MOCK_DB_DATA/library-items/navs/nav-popover-menu.ts
@@ -1,5 +1,6 @@
 import { LibraryItem, LibraryItemType } from 'src/app/views/shared/models/library-item.model';
 import { LibraryItemsSharedData as ITEMS } from '../library-items-shared-data';
+import { LibraryItemConstants as CONST } from '../library-item-constants';
 
 export const NavPopoverMenu: LibraryItem[] = [
   {
@@ -31,5 +32,30 @@ function getCodeHtml(): string | null {
  * Return null to remove Documentation from library-item.
  */
 function getDocumentationHtml(): string | null {
-  return null;
+  return `
+  <p>
+    For å ta i bruk denne komponenten i en Angular-applikasjon må NPM-pakken
+    <a href="${CONST.FhiAngularComponentsNpmUrl}">@folkehelseinstituttet/angular-components</a>
+    være lagt til som en "dependency".
+  </p>
+  
+  <h2 class="h5">Nyttige lenker</h2>
+  
+  <ul>
+    <li>
+      <a href="${CONST.FhiAngularComponentsGithubLibUrl}/fhi-popover/README.md#API">
+        API-dokumentasjon
+      </a>
+    </li>
+    <li>
+      <a href="${CONST.FhiAngularComponentsGithubLibUrl}/fhi-popover">
+        Kildekode
+      </a>
+    </li>
+    <li>
+      <a href="${CONST.ExampleComponentsGithubUrl}/navs">
+        Demokode
+      </a>
+    </li>
+  </ul>`;
 }
