31d939a3810f39fff215521001e1e80392896915
Karl Granli <karl@gran.li>
2024-01-22 17:02:07 +0100

    Updated presentation of colors used

--- src/MOCK_DB_DATA/library-items/colors/color-system.ts
+++ src/MOCK_DB_DATA/library-items/colors/color-system.ts
@@ -30,55 +30,44 @@ function getExampleHtml(): string {
 
   return `
 <p class="text-muted small mt-2 mb-0">Lysegrå</p>
-${colorHtml('#fbfbfb', 'fhi-grey-light-1')}
-${colorHtml('#f7f6f5', 'fhi-grey-light-2')}
-${colorHtml('#f0edeb', 'fhi-grey-light-3')}
-${colorHtml('#e0dddb', 'fhi-grey-light-4')}
+${colorHtml('#fbfbfb', 'fhi-core-grey-light-1')}
+${colorHtml('#f7f6f5', 'fhi-core-grey-light-2')}
+${colorHtml('#f0edeb', 'fhi-core-grey-light-3')}
+${colorHtml('#e0dddb', 'fhi-core-grey-light-4')}
 
 <p class="text-muted small mt-2 mb-0">Grå</p>
-${colorHtml('#cfd0cb', 'fhi-grey-1')}
-${colorHtml('#9f9f9f', 'fhi-grey-2')}
-${colorHtml('#767676', 'fhi-grey-3')}
-${colorHtml('#222222', 'fhi-grey-4')}
-
-<p class="text-muted small mt-2 mb-0">Charcoal/Antrasitt</p>
-${colorHtml('#333841', 'fhi-charcoal-1')}
+${colorHtml('#cfd0cb', 'fhi-core-grey-1')}
+${colorHtml('#767676', 'fhi-core-grey-3')}
+${colorHtml('#222222', 'fhi-core-grey-4')}
 
 <p class="text-muted small mt-2 mb-0">Blågrå</p>
-${colorHtml('#f5f8fa', 'fhi-blue-grey-1')}
-${colorHtml('#edf4f7', 'fhi-blue-grey-2')}
-${colorHtml('#dbe6ec', 'fhi-blue-grey-3')}
-${colorHtml('#cbd9df', 'fhi-blue-grey-4')}
-${colorHtml('#afc0c7', 'fhi-blue-grey-5')}
-${colorHtml('#879ba5', 'fhi-blue-grey-6')}
-${colorHtml('#435c68', 'fhi-blue-grey-7')}
+${colorHtml('#edf4f7', 'fhi-core-blue-grey-2')}
+${colorHtml('#dbe6ec', 'fhi-core-blue-grey-3')}
+${colorHtml('#cbd9df', 'fhi-core-blue-grey-4')}
+${colorHtml('#435c68', 'fhi-core-blue-grey-7')}
 
 <p class="text-muted small mt-2 mb-0">Blå</p>
-${colorHtml('#0067c4', 'fhi-blue-1')}
-${colorHtml('#0e4485', 'fhi-blue-2')}
-${colorHtml('#393c61', 'fhi-blue-3')}
+${colorHtml('#0067c4', 'fhi-core-blue-1')}
+${colorHtml('#393c61', 'fhi-core-blue-3')}
 
 <p class="text-muted small mt-2 mb-0">Grønn</p>
-${colorHtml('#cceee4', 'fhi-green-1')}
-${colorHtml('#04ab79', 'fhi-green-2')}
-
-<p class="text-muted small mt-2 mb-0">Gul</p>
-${colorHtml('#fffdce', 'fhi-yellow-1')}
-
-<p class="text-muted small mt-2 mb-0">Oransje</p>
-${colorHtml('#feeed9', 'fhi-orange-1')}
-${colorHtml('#f7b665', 'fhi-orange-2')}
+${colorHtml('#cceee4', 'fhi-core-green-1')}
+${colorHtml('#04ab79', 'fhi-core-green-2')}
 
 <p class="text-muted small mt-2 mb-0">Rød</p>
-${colorHtml('#f86559', 'fhi-red-1')}
-${colorHtml('#d14641', 'fhi-red-2')}
+${colorHtml('#f86559', 'fhi-core-red-1')}
+${colorHtml('#d14641', 'fhi-core-red-2')}
 
-<p class="text-muted small mt-2 mb-0">Mørkerosa</p>
-${colorHtml('#de2968', 'fhi-pink-dark-1')}
+<p class="text-muted small mt-2 mb-0">Mørk rosa</p>
+${colorHtml('#de2968', 'fhi-core-pink-dark-1')}
 
-<p class="text-muted small mt-2 mb-0">Lyserosa</p>
-${colorHtml('#ffe3e2', 'fhi-pink-light-1')}
-${colorHtml('#fad1cd', 'fhi-pink-light-2')}`;
+<p class="text-muted small mt-2 mb-0">Lys rosa</p>
+${colorHtml('#ffe3e2', 'fhi-core-pink-light-1')}
+
+<p class="text-muted small mt-2 mb-0">Oransje</p>
+${colorHtml('#feeed9', 'fhi-core-orange-1')}
+${colorHtml('#f7b665', 'fhi-core-orange-2')}
+`;
 }
 
 /*
@@ -95,3 +84,14 @@ function getCodeHtml(): string | null {
 function getDocumentationHtml(): string | null {
   return null;
 }
+
+// ${colorHtml('#9f9f9f', 'fhi-core-grey-2')}
+
+// <p class="text-muted small mt-2 mb-0">Charcoal/Antrasitt</p>
+// ${colorHtml('#333841', 'fhi-core-charcoal-1')}
+
+// <p class="text-muted small mt-2 mb-0">Gul</p>
+// ${colorHtml('#fffdce', 'fhi-core-yellow-1')}
+
+// <p class="text-muted small mt-2 mb-0">Lyserosa</p>
+// ${colorHtml('#fad1cd', 'fhi-core-pink-light-2')}
