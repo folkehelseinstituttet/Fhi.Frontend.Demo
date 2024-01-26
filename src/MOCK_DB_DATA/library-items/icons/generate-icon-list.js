173603c80d1050b27bd71f4ad3a91fb08f812743
Karl Granli <karl@gran.li>
2024-01-26 10:10:14 +0100

    Formatting

--- src/MOCK_DB_DATA/library-items/icons/generate-icon-list.js
+++ src/MOCK_DB_DATA/library-items/icons/generate-icon-list.js
@@ -1,25 +1,49 @@
 const fs = require('fs');
 const path = require('path');
 
-const pathToListOfGeneratedIcons = path.join(__dirname, '..', '..', '..', '..', '..', 'Fhi.Frontend.Demo', 'Fhi.Frontend.Style', 'src', 'fhi', 'icons', 'icon-list.GENERATED.md');
+const pathToListOfGeneratedIcons = path.join(
+  __dirname,
+  '..',
+  '..',
+  '..',
+  '..',
+  '..',
+  'Fhi.Frontend.Demo',
+  'Fhi.Frontend.Style',
+  'src',
+  'fhi',
+  'icons',
+  'icon-list.GENERATED.md',
+);
 const generatedOutputFile = path.join(__dirname, 'icon-set.GENERATED.ts');
 
 let listOfGeneratedIcons;
 
-fs.readFile(pathToListOfGeneratedIcons, { encoding: 'utf-8' }, (error, data) => {
-  if (error) throw error;
-  listOfGeneratedIcons = data;
-});
+fs.readFile(
+  pathToListOfGeneratedIcons,
+  { encoding: 'utf-8' },
+  (error, data) => {
+    if (error) throw error;
+    listOfGeneratedIcons = data;
+  },
+);
 
-const generateIconList = function() {
+const generateIconList = function () {
   listOfGeneratedIcons = listOfGeneratedIcons.replace('```scss', '');
-  listOfGeneratedIcons = listOfGeneratedIcons.replace('$included-icons: (', 'export const IncludedIcons = [').trim();
+  listOfGeneratedIcons = listOfGeneratedIcons
+    .replace('$included-icons: (', 'export const IncludedIcons = [')
+    .trim();
   listOfGeneratedIcons = listOfGeneratedIcons.replace(')', '];');
   listOfGeneratedIcons = listOfGeneratedIcons.replace('```', '');
-  fs.writeFile(generatedOutputFile, listOfGeneratedIcons, { encoding: 'utf-8' }, error => {
-    console.log(generatedOutputFile + '\ngenerated with latest icon set\n');
-    if (error) throw error;
-  });
-}
+  fs.writeFile(
+    generatedOutputFile,
+    listOfGeneratedIcons,
+    { encoding: 'utf-8' },
+    (error) => {
+      console.log(generatedOutputFile + '\ngenerated with latest icon set\n');
+      if (error) throw error;
+    },
+  );
+};
 
-setTimeout(generateIconList, 250);//give time to find and read file
+setTimeout(generateIconList, 250); //give time to find and read file
