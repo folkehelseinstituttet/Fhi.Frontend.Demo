5670656199780557c685c230060ccdcb5aa7791b
yleaxeman <karl@gran.li>
2024-02-07 11:21:42 +0100

    Removed action as an option

--- projects/fhi-angular-highcharts/src/lib/constants-and-enums/diagram-additional-link-type.ts
+++ projects/fhi-angular-highcharts/src/lib/constants-and-enums/diagram-additional-link-type.ts
@@ -1,12 +1,10 @@
 export enum DiagramAdditionalLinkType {
-  action,
   fragment,
   href,
   routerLink,
 }
 
 export const DiagramAdditionalLinkTypeValues = {
-  action: DiagramAdditionalLinkType.action,
   fragment: DiagramAdditionalLinkType.fragment,
   href: DiagramAdditionalLinkType.href,
   routerLink: DiagramAdditionalLinkType.routerLink,
