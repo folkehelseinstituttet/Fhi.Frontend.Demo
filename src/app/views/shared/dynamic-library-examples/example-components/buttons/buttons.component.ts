fa7f9fe440d8571401ab6efab2eeccc7283d683c
yleaxeman <karl@gran.li>
2024-02-28 13:48:31 +0100

    Examples updated and documented

--- src/app/views/shared/dynamic-library-examples/example-components/buttons/buttons.component.ts
+++ src/app/views/shared/dynamic-library-examples/example-components/buttons/buttons.component.ts
@@ -14,5 +14,6 @@ export class ButtonsComponent {
   @Input() itemId!: string;
   @Input() items!: LibraryItemsShared;
 
-  activeButton: number;
+  activeButton: number = 1;
+  verticalGroup: boolean = false;
 }
