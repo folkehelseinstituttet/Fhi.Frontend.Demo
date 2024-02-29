873e4f789c2ea2bad7ce9db44926e17130df1561
Karl Granli <karl@gran.li>
2024-02-29 14:16:18 +0100

    Enhancement/button group visuals and dynamic example (#487)

---
+++ src/app/views/shared/dynamic-library-examples/example-components/buttons/buttons.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+import { SharedModule } from 'src/app/shared/shared.module';
+import { LibraryItemsShared } from '../../../models/library-item.model';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-buttons',
+  standalone: true,
+  imports: [NgbDropdownModule, SharedModule],
+  templateUrl: './buttons.component.html',
+})
+export class ButtonsComponent {
+  @Input() itemId!: string;
+  @Input() items!: LibraryItemsShared;
+
+  activeButton: number = 1;
+  verticalGroup: boolean = false;
+}
