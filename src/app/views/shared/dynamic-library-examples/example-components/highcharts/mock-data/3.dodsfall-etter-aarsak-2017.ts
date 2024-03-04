d657aac85d7bd2762b86a1d28a70a9fb162ec9b1
Karl Granli <karl@gran.li>
2024-03-04 16:15:22 +0100

    Release/fhi-angular-components/3.1.1 (#505)

---
+++ src/app/views/shared/dynamic-library-examples/example-components/highcharts/mock-data/3.dodsfall-etter-aarsak-2017.ts
@@ -0,0 +1,26 @@
+export const DodsfallEtterAarsak_2017 = [
+  {
+    name: 'Hjerteinfarkt| Kvinne',
+    data: [{ name: '2017', y: 984 }],
+  },
+  {
+    name: 'Hjerteinfarkt     | Mann',
+    data: [{ name: '2017', y: 1147 }],
+  },
+  {
+    name: 'Annen iskemisk hjertesykdom|Kvinne',
+    data: [{ name: '2017', y: 567 }],
+  },
+  {
+    name: 'Annen iskemisk hjertesykdom | Mann',
+    data: [{ name: '2017', y: 862 }],
+  },
+  {
+    name: 'Karsykdommer i hjernen | Kvinne',
+    data: [{ name: '2017', y: 1372 }],
+  },
+  {
+    name: 'Karsykdommer i hjernen | Mann',
+    data: [{ name: '2017', y: 961 }],
+  },
+];
