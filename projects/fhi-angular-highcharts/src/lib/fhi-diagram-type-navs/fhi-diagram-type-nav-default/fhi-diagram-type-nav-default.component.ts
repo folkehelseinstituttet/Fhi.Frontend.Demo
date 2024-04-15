04c089a45207079f1393115dffcbe40ab78136c2
yleaxeman <karl@gran.li>
2024-04-15 15:40:29 +0200

    First test on navigating

--- projects/fhi-angular-highcharts/src/lib/fhi-diagram-type-navs/fhi-diagram-type-nav-default/fhi-diagram-type-nav-default.component.ts
+++ projects/fhi-angular-highcharts/src/lib/fhi-diagram-type-navs/fhi-diagram-type-nav-default/fhi-diagram-type-nav-default.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Subscription } from 'rxjs';
 
 import { DiagramTypeGroupService } from '../../services/diagram-type-group.service';
 import { DiagramTypeGroup } from '../../models/diagram-type-group.model';
+import { DiagramType } from '../../models/diagram-type.model';
 
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
@@ -16,6 +17,10 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 export class FhiDiagramTypeNavDefaultComponent implements OnInit, OnDestroy {
   private subscription: Subscription = new Subscription();
 
+  currentActiveDiagramType: DiagramType;
+
+  @Output() diagramTypeNavigation = new EventEmitter<DiagramType>();
+
   diagramTypeGroups!: DiagramTypeGroup[];
 
   constructor(private diagramTypeGroupService: DiagramTypeGroupService) {}
@@ -25,6 +30,7 @@ export class FhiDiagramTypeNavDefaultComponent implements OnInit, OnDestroy {
       this.diagramTypeGroupService.diagramTypeGroups$.subscribe({
         next: (diagramTypeGroups) => {
           this.diagramTypeGroups = diagramTypeGroups;
+          this.currentActiveDiagramType = this.findCurrentActiveDiagramType(diagramTypeGroups);
         },
         error: (error) => console.log(error),
       }),
@@ -33,5 +39,21 @@ export class FhiDiagramTypeNavDefaultComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  navigate(diagramType: DiagramType) {
+    this.currentActiveDiagramType.active = false;
+    diagramType.active = true;
+    this.diagramTypeNavigation.emit(diagramType);
+    this.currentActiveDiagramType = diagramType;
+  }
+
+  findCurrentActiveDiagramType(diagramTypeGroup: DiagramTypeGroup[]) {
+    // console.log('find diagram type group', diagramTypeGroup);
+    for (let i = 0; i < diagramTypeGroup.length; i++) {
+      if (diagramTypeGroup[i].diagramType.active === true) {
+        return diagramTypeGroup[i].diagramType;
+      }
+    }
   }
 }
