e3e57517968b573312bfd833cff0256b7f3cc350
Bernt Andersen <bernt.andersen@fhi.no>
2024-05-16 12:42:04 +0200

    Add more info to diagram-type-ids.ts

--- projects/fhi-angular-highcharts/src/lib/constants-and-enums/diagram-type-ids.ts
+++ projects/fhi-angular-highcharts/src/lib/constants-and-enums/diagram-type-ids.ts
@@ -5,6 +5,8 @@ export enum DiagramTypeIds {
   columnStacked,
   line,
   map,
+  mapFylker,
+  mapFylker2019,
   pie,
   table,
 }
@@ -16,6 +18,23 @@ export const DiagramTypeIdValues = {
   columnStacked: DiagramTypeIds[DiagramTypeIds.columnStacked],
   line: DiagramTypeIds[DiagramTypeIds.line],
   map: DiagramTypeIds[DiagramTypeIds.map],
+  mapFylker: DiagramTypeIds[DiagramTypeIds.mapFylker],
+  mapFylker2019: DiagramTypeIds[DiagramTypeIds.mapFylker2019],
   pie: DiagramTypeIds[DiagramTypeIds.pie],
   table: DiagramTypeIds[DiagramTypeIds.table],
 };
+
+export enum ChartTypeIds {
+  bar,
+  barStacked,
+  column,
+  columnStacked,
+  line,
+  pie,
+}
+
+export enum MapTypeIds {
+  map,
+  mapFylker,
+  mapFylker2019,
+}
