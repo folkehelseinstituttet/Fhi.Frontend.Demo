138a7f6a89fe3851541caa9b1e52d580382c00f8
Bernt Andersen <bernt.andersen@fhi.no>
2024-05-28 11:14:14 +0200

    Remove property allMapsLoaded

--- projects/fhi-angular-highcharts/src/lib/models/all-diagram-options.model.ts
+++ projects/fhi-angular-highcharts/src/lib/models/all-diagram-options.model.ts
@@ -1,7 +1,6 @@
 import { FhiDiagramOptions } from './fhi-diagram-options.model';
 
 export interface AllDiagramOptions extends FhiDiagramOptions {
-  allMapsLoaded?: boolean;
   seriesHasDecimalDataPoints?: boolean;
   seriesHasNegativeDataPoints?: boolean;
 }
