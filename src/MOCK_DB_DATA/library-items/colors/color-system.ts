cdf3315f669375a864cb2cefe4e4ed4e29bede2c
Trygve Bernhardt <51702336+trybern@users.noreply.github.com>
2024-08-23 14:49:25 +0200

    Update color-system.ts (#637)
    * Update color-system.ts
    
    Added missing colors, rearranged them to correspond with official identity and removed an intruder. Some names are a bit jalla, but we'll live with that until new web components and system is in place.
    
    * Update _ds-tmp.block.scss
    
    Added temp. css for documentation page

--- src/MOCK_DB_DATA/library-items/colors/color-system.ts
+++ src/MOCK_DB_DATA/library-items/colors/color-system.ts
@@ -34,10 +34,10 @@ ${colorHtml('#fbfbfb', 'fhi-core-grey-light-1')}
 ${colorHtml('#f7f6f5', 'fhi-core-grey-light-2')}
 ${colorHtml('#f0edeb', 'fhi-core-grey-light-3')}
 ${colorHtml('#e0dddb', 'fhi-core-grey-light-4')}
-
-<p class="text-muted small mt-2 mb-0">Grå</p>
 ${colorHtml('#cfd0cb', 'fhi-core-grey-1')}
 ${colorHtml('#767676', 'fhi-core-grey-3')}
+
+<p class="text-muted small mt-2 mb-0">Tekst</p>
 ${colorHtml('#222222', 'fhi-core-grey-4')}
 
 <p class="text-muted small mt-2 mb-0">Blågrå</p>
@@ -46,18 +46,25 @@ ${colorHtml('#dbe6ec', 'fhi-core-blue-grey-3')}
 ${colorHtml('#cbd9df', 'fhi-core-blue-grey-4')}
 ${colorHtml('#879ba5', 'fhi-core-blue-grey-6')}
 ${colorHtml('#435c68', 'fhi-core-blue-grey-7')}
-${colorHtml('#333841', 'fhi-core-charcoal-1')}
 
-<p class="text-muted small mt-2 mb-0">Blå</p>
+<p class="text-muted small mt-2 mb-0">Systemblå</p>
 ${colorHtml('#0067c4', 'fhi-core-blue-1')}
 ${colorHtml('#0e4485', 'fhi-core-blue-2')}
+
+<p class="text-muted small mt-2 mb-0">FHI-blå</p>
+${colorHtml('#d5ebf5', 'fhi-core-blue-fhi4')}
+${colorHtml('#65adc3', 'fhi-core-blue-fhi3')}
+${colorHtml('#386e84', 'fhi-core-blue-fhi2')}
 ${colorHtml('#393c61', 'fhi-core-blue-3')}
 
 <p class="text-muted small mt-2 mb-0">Grønn</p>
 ${colorHtml('#cceee4', 'fhi-core-green-1')}
+${colorHtml('#68cdaf', 'fhi-core-green-1a')}
 ${colorHtml('#04ab79', 'fhi-core-green-2')}
 
 <p class="text-muted small mt-2 mb-0">Rød</p>
+${colorHtml('#fff0f0', 'fhi-core-red-4')}
+${colorHtml('#fbd1cd', 'fhi-core-red-3')}
 ${colorHtml('#f86559', 'fhi-core-red-1')}
 ${colorHtml('#d14641', 'fhi-core-red-2')}
 
@@ -67,6 +74,7 @@ ${colorHtml('#de2968', 'fhi-core-pink-dark-1')}
 
 <p class="text-muted small mt-2 mb-0">Oransje</p>
 ${colorHtml('#feeed9', 'fhi-core-orange-1')}
+${colorHtml('#fee191', 'fhi-core-yellow-2')}
 ${colorHtml('#f7b665', 'fhi-core-orange-2')}
 `;
 }
