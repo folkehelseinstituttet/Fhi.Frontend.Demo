de757395d8596f358512099e25bd2ab41296d9e0
Bernt Andersen <bernt.andersen@fhi.no>
2024-08-29 15:32:22 +0200

    Update demo markup in MOCK_DB_DATA

--- src/MOCK_DB_DATA/library-items/modals/modal.ts
+++ src/MOCK_DB_DATA/library-items/modals/modal.ts
@@ -1,8 +1,5 @@
 import { LibraryItem, LibraryItemType } from 'src/app/views/shared/models/library-item.model';
 import { LibraryItemsSharedData as ITEMS } from '../library-items-shared-data';
-import { LibraryItemConstants as CONST } from '../library-item-constants';
-
-const itemTitle = 'Modal';
 
 export const Modal: LibraryItem[] = [
   {
@@ -28,8 +25,46 @@ function getExampleHtml(): string {
  */
 function getCodeHtml(): string | null {
   return `
-<fhi-modal [modalTitle]="'Overskrift'">
+<!-- Eks. 1 -->
+<fhi-modal>
   <ng-container fhi-modal.button>Åpne modal</ng-container>
+  <ng-container fhi-modal.body><p>Modalvindu innhold.</p></ng-container>
+</fhi-modal>
+
+<!-- Eks. 2 -->
+<fhi-modal
+  [modalTitle]="'Overskrift'"
+  [size]="'sm'"
+  [actionButtons]="[{ name: 'Handling 1' }]"
+  (modalAction)="onModalAction($event)"
+>
+  <ng-container fhi-modal.button>Åpne lite modalvindu</ng-container>
+  <ng-container fhi-modal.body><p>Modalvindu innhold.</p></ng-container>
+</fhi-modal>
+
+<!-- Eks. 3 -->
+<fhi-modal
+  [modalTitle]="'Overskrift'"
+  [actionButtons]="actionButtons"
+  [size]="'fullscreen'"
+  [openModalButtonClass]="'fhi-btn-icon'"
+  (modalAction)="onModalAction($event)"
+>
+  <ng-container fhi-modal.button>
+    <i class="icon-arrows-fullscreen"></i>
+    <span class="btn__text">Åpne fullskjerm modalvindu</span>
+  </ng-container>
+  <ng-container fhi-modal.body>
+    <pre><code>&#64;Input() actionButtons: FhiModalActionButton[] = {{ actionButtons | json }};</code></pre>
+  </ng-container>
+</fhi-modal>
+
+<!-- Eks. 4 -->
+<fhi-popover-menu
+  [items]="[{ name: 'Åpne modal fra foreldrekomponent', action: 'openModal' }]"
+  (actionEvent)="onPopoverActionOpenModalFromParent($event)"
+></fhi-popover-menu>
+<fhi-modal [parentAction]="modalParentAction" (dismissModal)="onDismissModal()">
   <ng-container fhi-modal.body><p>Modalvindu innhold.</p></ng-container>
 </fhi-modal>`;
 }
