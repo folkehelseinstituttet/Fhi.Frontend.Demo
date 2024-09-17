e762aa5ac88fef84c7d0aa6dea3c64d9f20df40a
Bernt Andersen <bernt.andersen@fhi.no>
2024-09-17 18:07:51 +0200

    Add missing fallback in mockDbInterceptor

--- src/app/core/mock-db.interceptor.ts
+++ src/app/core/mock-db.interceptor.ts
@@ -1,12 +1,17 @@
-import { HttpEvent, HttpRequest, HttpResponse } from '@angular/common/http';
+import { HttpEvent, HttpHandlerFn, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 
 import { getMockDbBody } from './mock-db';
 
-export function mockDbInterceptor(req: HttpRequest<unknown>): Observable<HttpEvent<unknown>> {
+export function mockDbInterceptor(
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn,
+): Observable<HttpEvent<unknown>> {
   const { url, method } = req;
 
   if (url.startsWith('api/') && method === 'GET') {
-    return of(new HttpResponse({ status: 200, body: getMockDbBody(url) })).pipe();
+    return of(new HttpResponse({ status: 200, body: getMockDbBody(url) }));
   }
+
+  return next(req);
 }
