1183a14f49c6e4226a8005e3077ab0987e09e4cc
Bernt Andersen <bernt@proand.no>
2024-09-26 10:41:05 +0200

    Internal/Add and info box on the "Komponenter"-landing page, (#708)
    And also improve the implementation of the article page.

--- src/app/views/shared/article/article.component.ts
+++ src/app/views/shared/article/article.component.ts
@@ -3,7 +3,7 @@ import { Subscription } from 'rxjs';
 
 import { UrlService } from 'src/app/services/url.service';
 import { UrlSegment } from 'src/app/url-segment.constants';
-import { LibraryItemConstants as CONST } from 'src/MOCK_DB_DATA/library-items/library-item-constants';
+import { LibraryItem } from '../models/library-item.model';
 
 @Component({
   selector: 'app-article',
@@ -14,13 +14,20 @@ export class ArticleComponent implements OnInit, OnDestroy {
 
   title: string;
   articleHtml: string;
+  currentSegment: string;
+  urlSegment = UrlSegment;
+  items: LibraryItem[] = [];
+  itemsFiltered: LibraryItem[] = [];
+  rootLink!: string;
 
   constructor(private urlService: UrlService) {}
 
   ngOnInit() {
     this.subscription.add(
       this.urlService.URL$.subscribe(() => {
-        this.findTopLevel();
+        this.currentSegment = this.urlService.getSegmentPath(1);
+        this.rootLink = `/developer/${this.currentSegment}/`;
+        this.getCurrentArticleTitle();
       }),
     );
   }
@@ -29,66 +36,11 @@ export class ArticleComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
-  private findTopLevel() {
-    switch (this.urlService.getSegmentPath(0)) {
-      case UrlSegment.developer:
-        this.findSecondLevelDeveloper();
-        break;
-
-      case UrlSegment.designer:
-        this.findSecondLevelDesigner();
+  private getCurrentArticleTitle() {
+    if (this.urlService.getSegmentPath(1) === UrlSegment.visualIdentity) {
+      this.title = 'Visuell identitet i FHI';
+    } else {
+      this.title = 'Komponenter';
     }
-  }
-  private findSecondLevelDeveloper() {
-    switch (this.urlService.getSegmentPath(1)) {
-      case UrlSegment.visualIdentity:
-        this.title = 'Visuell identitet i FHI';
-        this.articleHtml = this.getArticleHtmlForDeveloperVisualIdentity();
-        break;
-
-      case UrlSegment.components:
-        this.title = 'Komponenter';
-        this.articleHtml = this.getArticleHtmlForDeveloperComponents();
-        break;
-
-      case UrlSegment.layoutAndPageTemplates:
-        this.title = 'Layout og sidemaler';
-        this.articleHtml = this.getArticleHtmlForDeveloperLayoutAndPageTemplates();
-        break;
-
-      case UrlSegment.prototypes:
-        this.title = 'Eksempler på bruk';
-        this.articleHtml = this.getArticleHtmlForDeveloperPrototypes();
-        break;
-    }
-  }
-  private findSecondLevelDesigner() {
-    console.log('findSecondLevelDesigner() not implemented...');
-  }
-
-  private getArticleHtmlForDeveloperVisualIdentity(): string {
-    return `
-<p>For at alle våre produkter skal ha en tydelig og gjenkjennbar profil må farger, ikonbruk, typografi, logo og illustrasjoner henge sammen og brukes konsistent.</p>`;
-  }
-
-  private getArticleHtmlForDeveloperComponents(): string {
-    return `
-<p>Komponentbiblioteket i FHI Designsystem baserer seg på <a href="${CONST.BootstrapBaseUrl}">Bootstrap 5</a>. Interaktive komponenter benytter <a href="${CONST.NgBootstrapBaseUrl}" class="text-nowrap">ng-bootstrap</a> og andre tredjepartskomponenter der disse finnes. I tillegg er noen skreddersydde Angularkomponenter produsert av FHI.</p>
-
-<p>Alle løsninger kan konsumere stilsettet uavhengig av hvilket rammeverk som er benyttet. Legg til CSS fra FHI Designsystem: <a href="https://www.npmjs.com/package/@folkehelseinstituttet/style">@folkehelseinstituttet/style</a>.</p>
-
-<p>FHI Designsystem bygges i Angular og løsninger som benytter annet frontendrammeverk er ansvarlige for å generere riktig markup slik at utseendet blir korrekt.</p>
-
-<p><a href="https://github.com/folkehelseinstituttet/Fhi.Frontend.Style/blob/main/CHANGELOG.md">Se changelog for stilsettet her.</a></p>`;
-  }
-
-  private getArticleHtmlForDeveloperLayoutAndPageTemplates(): string {
-    return `
-<p>Her finner du eksempler på hvordan man plasserer innholdet på en side.</p>`;
-  }
-
-  private getArticleHtmlForDeveloperPrototypes(): string {
-    return `
-<p>Her finner du eksempler på større enheter og klikkbare prototyper ofte produsert for spesifikke prosjekter.</p>`;
   }
 }
