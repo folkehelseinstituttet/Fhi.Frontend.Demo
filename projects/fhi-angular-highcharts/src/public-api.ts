6b44976779d450b4022c4a5214fd715b23cfb376
Bernt Andersen <bernt.andersen@fhi.no>
2024-10-11 14:44:32 +0200

    Add a custom version of the diagram type class to the public API Surface

--- projects/fhi-angular-highcharts/src/public-api.ts
+++ projects/fhi-angular-highcharts/src/public-api.ts
@@ -9,3 +9,5 @@ export * from './lib/models/fhi-diagram-flag.model';
 export * from './lib/models/fhi-diagram-options.model';
 export * from './lib/models/fhi-diagram-serie.model';
 export * from './lib/models/fhi-diagram-serie-data.model';
+
+export { FhiDiagramTypes } from './lib/constants-and-enums/fhi-diagram-types';
