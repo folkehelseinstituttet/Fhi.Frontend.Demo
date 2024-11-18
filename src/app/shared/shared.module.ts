e69a841c5dd5048dc7fb1715ebc60b513aa22117
Bjørn-Vegard Thoresen <103988075+yne-bv@users.noreply.github.com>
2024-11-18 16:46:48 +0100

    Make tree view navigation standalone (#747)
    Co-authored-by: Bjørn-Vegard Thoresen <bjorn-vegard.thoresen@fhi.no>

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MarkdownModule } from 'ngx-markdown';
 import { FhiAngularComponentsModule } from '@folkehelseinstituttet/angular-components';
+import { FhiTreeViewNavigationComponent } from '@folkehelseinstituttet/angular-components';
 import { FhiAngularHighchartsModule } from '@folkehelseinstituttet/angular-highcharts';
 
 import { SafePipe } from './pipes/safe.pipe';
@@ -21,6 +22,7 @@ import { SafePipe } from './pipes/safe.pipe';
     NgbModule,
     MarkdownModule.forRoot(),
     FhiAngularComponentsModule,
+    FhiTreeViewNavigationComponent,
     FhiAngularHighchartsModule,
   ],
   exports: [
@@ -32,6 +34,7 @@ import { SafePipe } from './pipes/safe.pipe';
     NgbModule,
     MarkdownModule,
     FhiAngularComponentsModule,
+    FhiTreeViewNavigationComponent,
     FhiAngularHighchartsModule,
   ],
 })
