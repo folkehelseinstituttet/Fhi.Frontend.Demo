218391ab0a41cd1c73214d914304dc384ac8f4fd
Bernt Andersen <bernt.andersen@fhi.no>
2024-11-27 15:01:54 +0100

    Add xAxis options

--- projects/fhi-angular-highcharts/src/lib/services/options.service.ts
+++ projects/fhi-angular-highcharts/src/lib/services/options.service.ts
@@ -8,6 +8,7 @@ import {
   SeriesOptionsType,
   TooltipOptions,
   XAxisLabelsOptions,
+  XAxisOptions,
   YAxisOptions,
 } from 'highcharts';
 
@@ -126,6 +127,8 @@ export class OptionsService {
 
   private updateChartOptions(options: Options): Options {
     options.series = this.getSeriesWithoutFlaggedDataPoints() as SeriesOptionsType[];
+    options.xAxis = this.getXAxis(options.xAxis as XAxisOptions, this.diagramOptions);
+
     if (this.diagramOptions.units?.length === 1) {
       options.yAxis = this.getYAxis(options.yAxis as YAxisOptions, this.diagramOptions.units[0]);
     } else {
@@ -190,6 +193,15 @@ export class OptionsService {
       }
     }
     return tooltip;
+  }
+
+  private getXAxis(xAxis: XAxisOptions, diagramOptions: FhiDiagramOptions): XAxisOptions {
+    xAxis = xAxis ? xAxis : {};
+    xAxis.title.text = diagramOptions.title;
+
+    console.log('diagramOptions', diagramOptions);
+
+    return xAxis;
   }
 
   private getYAxis(yAxis: YAxisOptions, unit?: FhiDiagramUnit): YAxisOptions {
