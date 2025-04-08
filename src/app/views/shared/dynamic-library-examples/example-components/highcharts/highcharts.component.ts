folkehelseinstituttet/Fhi.Frontend.Demo

commit 2efdaf3f04e59f9670e54a26dbc69931bea523c5

Enhancement/highcharts template control flow (#782)

projects/fhi-angular-highcharts/src/lib/fhi-angular-highcharts.component.html
@@ -1,270 +1,275 @@
-<article *ngIf="diagramOptionsInternal">
-  <h1 class="h3 mb-3">{{ diagramOptionsInternal.title }}</h1>
+@if (diagramOptionsInternal) {
+  <article>
+    <h1 class="h3 mb-3">{{ diagramOptionsInternal.title }}</h1>
 
-  @if (diagramOptionsInternal.description) {
-    <p class="mb-4">{{ diagramOptionsInternal.description }}</p>
-  }
+    @if (diagramOptionsInternal.description) {
+      <p class="mb-4">{{ diagramOptionsInternal.description }}</p>
+    }
 
-  @if (showDiagramTypeNav || showDownloadButton || showFullScreenButton || showMetadataButton) {
-    <ng-container *ngTemplateOutlet="diagramControls"></ng-container>
-  }
-  @if (showDiagramTypeDisabledWarning) {
-    <ng-container *ngTemplateOutlet="diagramTypeDisabledWarning"></ng-container>
-  } @else if (showDuplicateSerieNameError) {
-    <ng-container *ngTemplateOutlet="duplicateSerieNameError"></ng-container>
-  } @else {
-    <ng-container *ngTemplateOutlet="diagramAndFooter"></ng-container>
-  }
-</article>
+    @if (showDiagramTypeNav || showDownloadButton || showFullScreenButton || showMetadataButton) {
+      <ng-container *ngTemplateOutlet="diagramControls" />
+    }
+    @if (showDiagramTypeDisabledWarning) {
+      <ng-container *ngTemplateOutlet="diagramTypeDisabledWarning" />
+    } @else if (showDuplicateSerieNameError) {
+      <ng-container *ngTemplateOutlet="duplicateSerieNameError" />
+    } @else {
+      <ng-container *ngTemplateOutlet="diagramAndFooter" />
+    }
+  </article>
 
-<ng-template #diagramAndFooter>
-  <ng-container [ngSwitch]="activeDiagramTypeGroup?.name">
-    <ng-container *ngSwitchCase="diagramTypeGroupNames?.table">
-      <ng-container *ngTemplateOutlet="table"></ng-container>
-    </ng-container>
-    <ng-container *ngSwitchCase="diagramTypeGroupNames?.map">
-      <ng-container *ngIf="showMap">
-        <ng-container *ngTemplateOutlet="map"></ng-container>
-      </ng-container>
-    </ng-container>
-    <ng-container *ngSwitchCase="diagramTypeGroupNames?.chart">
-      <ng-container *ngIf="showDefaultChartTemplate; else chartAnimationHack">
-        <ng-container *ngTemplateOutlet="chart"></ng-container>
-      </ng-container>
-      <!-- chartAnimationHack: to get correct chart animation when switching from one chart type to another -->
-      <ng-template #chartAnimationHack>
-        <ng-container *ngTemplateOutlet="chart"></ng-container>
-      </ng-template>
-    </ng-container>
-  </ng-container>
-  <ng-container *ngIf="showFooter">
-    <ng-container *ngTemplateOutlet="diagramFooter"></ng-container>
-  </ng-container>
-</ng-template>
+  <ng-template #diagramAndFooter>
+    @switch (activeDiagramTypeGroup?.name) {
+      @case (diagramTypeGroupNames?.table) {
+        <ng-container *ngTemplateOutlet="table" />
+      }
+      @case (diagramTypeGroupNames?.map) {
+        @if (showMap) {
+          <ng-container *ngTemplateOutlet="map" />
+        }
+      }
+      @case (diagramTypeGroupNames?.chart) {
+        @if (showDefaultChartTemplate) {
+          <ng-container *ngTemplateOutlet="chart" />
+        } @else {
+          <!-- chartAnimationHack: to get correct chart animation when switching from one chart type to another -->
+          <ng-container *ngTemplateOutlet="chart" />
+        }
+      }
+    }
+    @if (showFooter) {
+      <ng-container *ngTemplateOutlet="diagramFooter" />
+    }
+  </ng-template>
 
-<ng-template #diagramControls>
-  <div class="fhi-diagram-controls">
-    <div class="d-flex flex-wrap">
-      <div class="fhi-diagram-controls__navigation" *ngIf="showDiagramTypeNav">
-        <fhi-diagram-type-nav-default
-          [diagramTypeGroups]="diagramTypeGroups"
-          (diagramTypeNavigation)="onDiagramTypeNavigation($event)"
-        ></fhi-diagram-type-nav-default>
-      </div>
-      <div class="flex-fill mb-3 d-flex">
-        <button
-          class="btn fhi-btn-icon fhi-diagram-controls__metadata"
-          *ngIf="showMetadataButton"
-          (click)="onMetadataButtonClick()"
-        >
-          <i class="icon-info-circle"></i>
-          <span class="btn__text">Om dataene</span>
-        </button>
-        <ng-container *ngIf="showFullScreenButton && !showDiagramTypeDisabledWarning">
-          <ng-container *ngTemplateOutlet="diagramModal"></ng-container>
-        </ng-container>
-        <div class="ms-auto">
-          <fhi-popover-menu
-            *ngIf="showDownloadButton"
-            [items]="[{ name: 'Last ned SVG', action: 'downloadSvg', icon: 'download' }]"
-            (actionEvent)="onControlsPopoverMenuAction($event)"
-          ></fhi-popover-menu>
+  <ng-template #diagramControls>
+    <div class="fhi-diagram-controls">
+      <div class="d-flex flex-wrap">
+        @if (showDiagramTypeNav) {
+          <div class="fhi-diagram-controls__navigation">
+            <fhi-diagram-type-nav-default
+              [diagramTypeGroups]="diagramTypeGroups"
+              (diagramTypeNavigation)="onDiagramTypeNavigation($event)"
+            ></fhi-diagram-type-nav-default>
+          </div>
+        }
+        <div class="flex-fill mb-3 d-flex">
+          @if (showMetadataButton) {
+            <button class="btn fhi-btn-icon fhi-diagram-controls__metadata" click="onMetadataButtonClick()">
+              <i class="icon-info-circle"></i>
+              <span class="btn__text">Om dataene</span>
+            </button>
+          }
+          @if (showFullScreenButton && !showDiagramTypeDisabledWarning) {
+            <ng-container *ngTemplateOutlet="diagramModal" />
+          }
+          @if (showDownloadButton) {
+            <div class="ms-auto">
+              <fhi-popover-menu
+                [items]="[{ name: 'Last ned SVG', action: 'downloadSvg', icon: 'download' }]"
+                (actionEvent)="onControlsPopoverMenuAction($event)"
+              ></fhi-popover-menu>
+            </div>
+          }
         </div>
       </div>
     </div>
-  </div>
-</ng-template>
+  </ng-template>
 
-<ng-template #diagramModal>
-  <fhi-modal
-    [openModalButtonClass]="'fhi-btn-icon fhi-btn-icon--circular'"
-    [modalTitle]="diagramOptionsInternal.title"
-    [size]="'fullscreen'"
-  >
-    <ng-container fhi-modal.button>
-      <i class="icon-arrows-fullscreen"></i>
-      <span class="visually-hidden">Fullskjerm </span>
-    </ng-container>
-    <ng-container fhi-modal.body>
-      @if (diagramOptionsInternal.description) {
-        <p>{{ diagramOptionsInternal.description }}</p>
-      }
-      <ng-container *ngTemplateOutlet="diagramAndFooter"></ng-container>
-    </ng-container>
-  </fhi-modal>
-</ng-template>
-
-<ng-template #chart>
-  <div class="fhi-highcharts-chart">
-    <highcharts-chart
-      [Highcharts]="highcharts"
-      [options]="highchartsOptions"
-      (chartInstance)="onChartInstance($event)"
-    ></highcharts-chart>
-  </div>
-</ng-template>
-
-<ng-template #map>
-  <div class="fhi-highcharts-chart fhi-highcharts-chart--map">
-    <highcharts-chart
-      [Highcharts]="highmaps"
-      [constructorType]="'mapChart'"
-      [options]="highchartsOptions"
-      (chartInstance)="onChartInstance($event)"
-    ></highcharts-chart>
-  </div>
-</ng-template>
-
-<ng-template #table>
-  <div class="table-responsive pt-3">
-    <table class="table table-striped table-sm">
-      <thead>
-        <tr *ngFor="let headerRow of tableData.theadRows">
-          <ng-container *ngFor="let tableCell of headerRow; let i = index">
-            <ng-container *ngIf="tableCell">
-              <td
-                class="text-center"
-                [attr.colspan]="tableCell.colspan ? tableCell.colspan : null"
-                [attr.rowspan]="tableCell.rowspan ? tableCell.rowspan : null"
-                *ngIf="!tableCell.isHeading; else tableHeaderCell"
-              ></td>
-              <ng-template #tableHeaderCell>
-                <th
-                  class="text-center"
-                  [attr.colspan]="tableCell.colspan ? tableCell.colspan : null"
-                  scope="col"
-                  *ngIf="tableCell.isHeading && tableCell.name !== undefined"
-                >
-                  {{ tableCell.name }}
-                </th>
-              </ng-template>
-            </ng-container>
-          </ng-container>
-        </tr>
-      </thead>
-      <tbody>
-        <tr *ngFor="let tableRow of tableData.tbodyRows">
-          <ng-container *ngFor="let tableCell of tableRow; let i = index">
-            <th
-              class="text-nowrap text-start"
-              [attr.rowspan]="tableCell.rowspan ? tableCell.rowspan : null"
-              scope="row"
-              *ngIf="tableCell.isHeading && tableCell.name !== undefined"
-            >
-              {{ tableCell.name }}
-            </th>
-            <td class="text-end" *ngIf="!tableCell.isHeading">
-              <ng-container *ngIf="isNumber(tableCell.data); else tableCellDataIsString">
-                {{ tableCell.data }}
-              </ng-container>
-              <ng-template #tableCellDataIsString>
-                {{ tableCell.data }}
-              </ng-template>
-            </td>
-          </ng-container>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-</ng-template>
-
-<ng-template #diagramFooter>
-  <footer class="mt-3">
-    <p
-      *ngIf="diagramOptionsInternal.units?.length > 0 && activeDiagramTypeGroup?.name === diagramTypeGroupNames?.table"
+  <ng-template #diagramModal>
+    <fhi-modal
+      [openModalButtonClass]="'fhi-btn-icon fhi-btn-icon--circular'"
+      [modalTitle]="diagramOptionsInternal.title"
+      [size]="'fullscreen'"
     >
-      <strong>Måltall </strong>
-      <span>
-        @for (unit of diagramOptionsInternal.units; track unit; let i = $index) {
-          <ng-container *ngIf="i === 0">{{ unit.label }}</ng-container>
-          <ng-container *ngIf="i > 0">, {{ unit.label }}</ng-container>
+      <ng-container fhi-modal.button>
+        <i class="icon-arrows-fullscreen"></i>
+        <span class="visually-hidden">Fullskjerm </span>
+      </ng-container>
+      <ng-container fhi-modal.body>
+        @if (diagramOptionsInternal.description) {
+          <p>{{ diagramOptionsInternal.description }}</p>
         }
-      </span>
-    </p>
-    <p *ngIf="diagramOptionsInternal.footer?.lastUpdated">
-      <strong>Sist oppdatert </strong>{{ diagramOptionsInternal.footer?.lastUpdated }}
-    </p>
-    <p *ngIf="diagramOptionsInternal.footer?.credits">
-      <strong>Kilde </strong>
-      <a href="{{ diagramOptionsInternal.footer.credits.href }}">{{ diagramOptionsInternal.footer.credits.text }}</a>
-    </p>
-    <div
-      class="mb-3"
-      *ngIf="diagramOptionsInternal.footer?.flags && activeDiagramTypeGroup?.name === diagramTypeGroupNames?.table"
-    >
-      <strong>Tegnforklaringer</strong><br />
-      <table>
+        <ng-container *ngTemplateOutlet="diagramAndFooter" />
+      </ng-container>
+    </fhi-modal>
+  </ng-template>
+
+  <ng-template #chart>
+    <div class="fhi-highcharts-chart">
+      <highcharts-chart
+        [Highcharts]="highcharts"
+        [options]="highchartsOptions"
+        (chartInstance)="onChartInstance($event)"
+      ></highcharts-chart>
+    </div>
+  </ng-template>
+
+  <ng-template #map>
+    <div class="fhi-highcharts-chart fhi-highcharts-chart--map">
+      <highcharts-chart
+        [Highcharts]="highmaps"
+        [constructorType]="'mapChart'"
+        [options]="highchartsOptions"
+        (chartInstance)="onChartInstance($event)"
+      ></highcharts-chart>
+    </div>
+  </ng-template>
+
+  <ng-template #table>
+    <div class="table-responsive pt-3">
+      <table class="table table-striped table-sm">
+        <thead>
+          @for (headerRow of tableData.theadRows; track $index) {
+            <tr>
+              @for (tableCell of headerRow; track $index) {
+                @if (tableCell?.isHeading && tableCell?.name !== undefined) {
+                  <th class="text-center" [attr.colspan]="tableCell.colspan ? tableCell.colspan : null" scope="col">
+                    {{ tableCell.name }}
+                  </th>
+                } @else if (tableCell && !tableCell.isHeading) {
+                  <td
+                    class="text-center"
+                    [attr.colspan]="tableCell.colspan ? tableCell.colspan : null"
+                    [attr.rowspan]="tableCell.rowspan ? tableCell.rowspan : null"
+                  ></td>
+                }
+              }
+            </tr>
+          }
+        </thead>
         <tbody>
-          <tr *ngFor="let flag of diagramOptionsInternal.footer?.flags">
-            <td class="pe-2">{{ flag.symbol }}</td>
-            <td>{{ flag.label }}</td>
-          </tr>
+          @for (bodyRow of tableData.tbodyRows; track $index) {
+            <tr>
+              @for (tableCell of bodyRow; track $index) {
+                @if (tableCell?.isHeading && tableCell?.name !== undefined) {
+                  <th
+                    class="text-nowrap text-start"
+                    [attr.rowspan]="tableCell.rowspan ? tableCell.rowspan : null"
+                    scope="row"
+                  >
+                    {{ tableCell.name }}
+                  </th>
+                } @else if (tableCell && !tableCell.isHeading) {
+                  <td class="text-end">
+                    {{ tableCell.data }}
+                  </td>
+                }
+              }
+            </tr>
+          }
         </tbody>
       </table>
     </div>
-    <div
-      class="mb-3"
-      *ngIf="
+  </ng-template>
+
+  <ng-template #diagramFooter>
+    <footer class="mt-3">
+      @if (diagramOptionsInternal.units?.length > 0 && activeDiagramTypeGroup?.name === diagramTypeGroupNames?.table) {
+        <p>
+          <strong>Måltall </strong>
+          <span>
+            @for (unit of diagramOptionsInternal.units; track $index) {
+              <!-- Need *ngIf here, see https://github.com/prettier/prettier/issues/16577 -->
+              <ng-container *ngIf="$index === 0">{{ unit.label }}</ng-container>
+              <ng-container *ngIf="$index > 0">, {{ unit.label }}</ng-container>
+            }
+          </span>
+        </p>
+      }
+      @if (diagramOptionsInternal.footer?.lastUpdated) {
+        <p><strong>Sist oppdatert </strong>{{ diagramOptionsInternal.footer?.lastUpdated }}</p>
+      }
+      @if (diagramOptionsInternal.footer?.credits) {
+        <p>
+          <strong>Kilde </strong>
+          <a href="{{ diagramOptionsInternal.footer.credits.href }}">{{
+            diagramOptionsInternal.footer.credits.text
+          }}</a>
+        </p>
+      }
+      @if (diagramOptionsInternal.footer?.flags && activeDiagramTypeGroup?.name === diagramTypeGroupNames?.table) {
+        <div class="mb-3">
+          <strong>Tegnforklaringer</strong><br />
+          <table>
+            <tbody>
+              @for (flag of diagramOptionsInternal.footer?.flags; track $index) {
+                <tr>
+                  <td class="pe-2">{{ flag.symbol }}</td>
+                  <td>{{ flag.label }}</td>
+                </tr>
+              }
+            </tbody>
+          </table>
+        </div>
+      }
+      @if (
         flaggedSeries.length > 0 &&
         diagramOptionsInternal.footer?.flags &&
         activeDiagramTypeGroup?.name !== diagramTypeGroupNames?.table
-      "
-    >
-      <p>
-        <a
-          role="button"
-          class="fhi-popover-trigger"
-          tabindex="0"
-          triggers="manual"
-          #p="ngbPopover"
-          aria-haspopup="true"
-          (click)="p.toggle()"
-          (keydown.enter)="p.toggle()"
-          [autoClose]="'outside'"
-          [ngbPopover]="popContent"
-          >Diagrammet inneholder data som ikke kan vises</a
-        >
-      </p>
-      <ng-template #popContent>
-        <p>Følgende data vises ikke i diagrammet</p>
-        <ul>
-          <li *ngFor="let dataPoint of getFlaggedDataPoints()">{{ dataPoint }} <br /></li>
-        </ul>
-      </ng-template>
-      <a role="button" class="fhi-link" tabindex="0" (click)="setDiagramTypeGroupToTable()">Vis som tabell</a>
-      for å se flere detaljer.
-    </div>
-    <p class="fst-italic small" *ngIf="diagramOptionsInternal.footer?.disclaimer">
-      {{ diagramOptionsInternal.footer?.disclaimer }}
-    </p>
-    <p>
-      <a
-        class="highcharts-credits"
-        href="{{ getMapCopyright()['url'] }}"
-        *ngIf="!diagramOptionsInternal.openSource && showMap"
-        >Kartdata fra &#169; {{ getMapCopyright()['text'] }}</a
-      >
-    </p>
-  </footer>
-</ng-template>
+      ) {
+        <div class="mb-3">
+          <p>
+            <a
+              role="button"
+              class="fhi-popover-trigger"
+              tabindex="0"
+              triggers="manual"
+              #p="ngbPopover"
+              aria-haspopup="true"
+              (click)="p.toggle()"
+              (keydown.enter)="p.toggle()"
+              [autoClose]="'outside'"
+              [ngbPopover]="popContent"
+            >
+              Diagrammet inneholder data som ikke kan vises
+            </a>
+          </p>
+          <ng-template #popContent>
+            <p>Følgende data vises ikke i diagrammet</p>
+            <ul>
+              @for (dataPoint of getFlaggedDataPoints(); track $index) {
+                <li>{{ dataPoint }} <br /></li>
+              }
+            </ul>
+          </ng-template>
+          <a role="button" class="fhi-link" tabindex="0" (click)="setDiagramTypeGroupToTable()">Vis som tabell</a>
+          for å se flere detaljer.
+        </div>
+      }
+      @if (diagramOptionsInternal.footer?.disclaimer) {
+        <p class="fst-italic small">
+          {{ diagramOptionsInternal.footer?.disclaimer }}
+        </p>
+      }
+      @if (!diagramOptionsInternal.openSource && showMap) {
+        <p>
+          <a class="highcharts-credits" href="{{ getMapCopyright()['url'] }}">
+            Kartdata fra &#169; {{ getMapCopyright()['text'] }}
+          </a>
+        </p>
+      }
+    </footer>
+  </ng-template>
 
-<ng-template #diagramTypeDisabledWarning>
-  <div class="mt-3" id="om-dataene">
-    <div class="alert alert-warning" role="alert">
-      <i class="icon-bell"></i>
-      <p class="mb-0">
-        Diagramtypen kan ikke vises med dette filtervalget.<br />
-        Velg en annen diagramtype eller gjør et annet filtervalg.
-      </p>
+  <ng-template #diagramTypeDisabledWarning>
+    <div class="mt-3" id="om-dataene">
+      <div class="alert alert-warning" role="alert">
+        <i class="icon-bell"></i>
+        <p class="mb-0">
+          Diagramtypen kan ikke vises med dette filtervalget.<br />
+          Velg en annen diagramtype eller gjør et annet filtervalg.
+        </p>
+      </div>
     </div>
-  </div>
-</ng-template>
+  </ng-template>
 
-<ng-template #duplicateSerieNameError>
-  <div class="mt-3" id="om-dataene">
-    <div class="alert alert-error" role="alert">
-      <i class="icon-exclamation-circle"></i>
-      <p class="mb-0">Det finnes duplikate serienavn, diagrammet kan ikke vises.</p>
+  <ng-template #duplicateSerieNameError>
+    <div class="mt-3" id="om-dataene">
+      <div class="alert alert-error" role="alert">
+        <i class="icon-exclamation-circle"></i>
+        <p class="mb-0">Det finnes duplikate serienavn, diagrammet kan ikke vises.</p>
+      </div>
     </div>
-  </div>
-</ng-template>
+  </ng-template>
+}

projects/fhi-angular-highcharts/src/lib/fhi-diagram-type-navs/fhi-diagram-type-nav-default/fhi-diagram-type-nav-default.component.html
@@ -1,82 +1,66 @@
 <div class="btn-group fhi-btn-group" role="group">
-  <ng-container *ngFor="let diagramTypeGroup of diagramTypeGroups">
-    <ng-container *ngIf="diagramTypeGroup.children.length === 1">
+  @for (group of diagramTypeGroups; track group.diagramType) {
+    @if (group.children.length === 1) {
       <button
         type="button"
         class="btn fhi-btn-group__btn"
-        [ngClass]="{ active: diagramTypeGroup.diagramType?.active }"
-        (click)="navigate(diagramTypeGroup.diagramType)"
+        [ngClass]="{ active: group.diagramType?.active }"
+        (click)="navigate(group.diagramType)"
       >
-        <i
-          [class]="'icon-' + diagramTypeGroup.diagramType?.icon"
-          [ngClass]="{ 'icon-white': diagramTypeGroup.diagramType?.active }"
-          *ngIf="diagramTypeGroup.diagramType?.icon"
-        ></i>
-        {{ diagramTypeGroup.name }}
+        <ng-container
+          [ngTemplateOutlet]="icon"
+          [ngTemplateOutletContext]="{ icon: group.diagramType?.icon, active: group.diagramType?.active }"
+        />
+        {{ group.name }}
       </button>
-    </ng-container>
-
-    <ng-container *ngIf="diagramTypeGroup.children.length > 1">
+    } @else {
       <div class="btn-group" role="group">
         <button
           type="button"
           class="btn fhi-btn-group__btn fhi-btn-group__split-btn"
-          [ngClass]="{ active: diagramTypeGroup.diagramType?.active }"
-          (click)="navigate(diagramTypeGroup.diagramType)"
+          [ngClass]="{ active: group.diagramType?.active }"
+          (click)="navigate(group.diagramType)"
         >
           <span>
-            <i
-              [class]="'icon-' + diagramTypeGroup.diagramType?.icon"
-              [ngClass]="{ 'icon-white': diagramTypeGroup.diagramType?.active }"
-              *ngIf="diagramTypeGroup.diagramType?.icon"
-            ></i>
-            {{ diagramTypeGroup.name }}
+            <ng-container
+              [ngTemplateOutlet]="icon"
+              [ngTemplateOutletContext]="{ icon: group.diagramType?.icon, active: group.diagramType?.active }"
+            />
+            {{ group.name }}
           </span>
         </button>
         <div class="btn-group fhi-dropdown" ngbDropdown role="group" aria-label="Button group with nested dropdown">
           <button
             type="button"
             class="btn fhi-btn-group__btn fhi-dropdown__btn fhi-btn-group__split-btn-toggler"
-            [ngClass]="{ active: diagramTypeGroup.diagramType?.active }"
+            [ngClass]="{ active: group.diagramType?.active }"
             aria-label="Velg noe..."
             ngbDropdownToggle
           ></button>
           <div class="dropdown-menu" ngbDropdownMenu>
-            <ng-container *ngFor="let diagramType of diagramTypeGroup.children">
+            @for (diagramType of group.children; track diagramType.id) {
               <button
                 class="text-nowrap"
                 [ngClass]="{ active: diagramType?.active }"
                 ngbDropdownItem
                 (click)="navigate(diagramType)"
               >
-                <i [class]="'icon-' + diagramType?.icon" *ngIf="diagramType?.icon"></i>
+                <ng-container
+                  [ngTemplateOutlet]="icon"
+                  [ngTemplateOutletContext]="{ icon: diagramType.icon, active: false }"
+                />
                 {{ diagramType?.name }}
               </button>
-            </ng-container>
+            }
           </div>
         </div>
       </div>
-    </ng-container>
-  </ng-container>
+    }
+  }
 </div>
 
-<!-- <ul>
-  <li *ngFor="let group2 of diagramTypeGroups">
-    {{ group2.name }}
-    <span> (id:{{ group2.diagramType.id }}) </span>
-    <span *ngIf="group2.diagramType?.active"><strong>[active]</strong></span>
-    <span *ngIf="group2.diagramType.disabled"><strong>[disabled]</strong></span>
-
-    <ng-container *ngIf="group2.children.length > 1">
-      <ul *ngFor="let diagramType2 of group2.children">
-        <li>
-          {{ diagramType2.name }}
-          <span *ngIf="diagramType2?.active"><strong>[active]</strong></span>
-          <span *ngIf="diagramType2.disabled"><strong>[disabled]</strong></span>
-        </li>
-      </ul>
-    </ng-container>
-  </li>
-</ul> -->
-
-<!-- <pre><code>{{ diagramTypeGroups | json }}</code></pre> -->
+<ng-template #icon let-icon="icon" let-active="active">
+  @if (icon) {
+    <i [class]="'icon-' + icon" [ngClass]="{ 'icon-white': active }"></i>
+  }
+</ng-template>

projects/fhi-angular-highcharts/src/lib/fhi-diagram-type-navs/nav-diagram-type-group.model.ts
@@ -1,10 +1,0 @@
-import { DiagramType } from '../models/diagram-type.model';
-
-export interface NavDiagramTypeGroup {
-  diagramType: DiagramType;
-  icon: string;
-  id: string;
-  isDisabled: boolean;
-  name: string;
-  excludeFromMenu?: boolean;
-}

commit b12fae64cdcee4eee79e0b0dc30b57987c5777ba

Fix template bug introduced in #782

projects/fhi-angular-highcharts/src/lib/fhi-angular-highcharts.component.html
@@ -55,7 +55,7 @@
         }
         <div class="flex-fill mb-3 d-flex">
           @if (showMetadataButton) {
-            <button class="btn fhi-btn-icon fhi-diagram-controls__metadata" click="onMetadataButtonClick()">
+            <button class="btn fhi-btn-icon fhi-diagram-controls__metadata" (click)="onMetadataButtonClick()">
               <i class="icon-info-circle"></i>
               <span class="btn__text">Om dataene</span>
             </button>

src/app/views/shared/dynamic-library-examples/example-components/highcharts/highcharts.component.ts
@@ -66,6 +66,8 @@ export class HighchartsComponent implements OnInit {
   }
 
   onMetadataButtonClick() {
+    console.log('onMetadataButtonClick()');
+
     this.viewportScroller.scrollToAnchor('om-dataene');
   }
 