b2b5d09c2c1ebde5bfe1454bfaafc4173efe1416
Bernt Andersen <bernt.andersen@fhi.no>
2025-04-28 14:03:08 +0200

    Add support for title, description and credits in svg download

--- projects/fhi-angular-highcharts/src/lib/services/download.service.ts
+++ projects/fhi-angular-highcharts/src/lib/services/download.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@angular/core';
 import { formatDate } from '@angular/common';
-import { Chart, ExportingMimeTypeValue, ExportingOptions } from 'highcharts';
+import { Chart, ExportingMimeTypeValue, ExportingOptions, Options } from 'highcharts';
+import { FhiDiagramOptions } from '../models/fhi-diagram-options.model';
 
 @Injectable()
 export class DownloadService {
-  downloadImage(chartInstance: Chart, MIMEtype: ExportingMimeTypeValue, title: string) {
+  downloadImage(
+    chartInstance: Chart,
+    MIMEtype: ExportingMimeTypeValue,
+    diagramOptions: FhiDiagramOptions,
+  ) {
     if (!chartInstance) {
       console.warn(`No "chartInstance", can't download chart.`);
       return;
@@ -13,9 +18,41 @@ export class DownloadService {
       type: MIMEtype,
       sourceWidth: 1200,
       sourceHeight: 800,
-      filename: this.getFilename(title),
+      filename: this.getFilename(diagramOptions.title),
     };
-    chartInstance.exportChartLocal(exportingOptions);
+    const chartOptions: Options = {
+      title: (() => {
+        if (diagramOptions.title) {
+          return {
+            align: 'left',
+            text: diagramOptions.title,
+          };
+        }
+        return null;
+      })(),
+
+      subtitle: (() => {
+        if (diagramOptions.description) {
+          return {
+            align: 'left',
+            text: diagramOptions.description,
+          };
+        }
+        return null;
+      })(),
+
+      credits: (() => {
+        if (diagramOptions.footer.credits.text) {
+          return {
+            enabled: true,
+            text: diagramOptions.footer.credits.text,
+          };
+        }
+        return null;
+      })(),
+    };
+
+    chartInstance.exportChartLocal(exportingOptions, chartOptions);
   }
 
   private getFilename(title: string) {
