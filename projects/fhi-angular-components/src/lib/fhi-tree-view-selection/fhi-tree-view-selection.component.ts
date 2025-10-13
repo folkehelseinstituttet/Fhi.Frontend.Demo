ecd63e7a1d59172e8a93733632d6be067d7b7cdb
Robin Zaal <robin.zaal@gmail.com>
2025-10-13 17:11:47 +0200

    Mindre endringer på synlighet av "Velg/Fjern alle" knapper (#872)
    * Update tree view selection behavior and button texts
    
    Refactored button logic in tree view selection component to handle hierarchical and item count-specific conditions. Adjusted text for "Remove all direct hits" for consistency.
    
    * Emit updated items on state change in tree view selection component
    
    * Update readme
    
    * Remove unnecessary `this`
    
    * Refactor "select this level"-button logic from 3 to 4 item threshold.
    
    * Make sure the top level "select all"-button cares about hierarchy

--- projects/fhi-angular-components/src/lib/fhi-tree-view-selection/fhi-tree-view-selection.component.ts
+++ projects/fhi-angular-components/src/lib/fhi-tree-view-selection/fhi-tree-view-selection.component.ts
@@ -33,7 +33,7 @@ enum GenericButtonText {
 
 enum SpecificButtonText {
   SELECT_ALL = 'Velg kun direkte treff',
-  REMOVE_ALL = 'Fjern alle direkte treff',
+  REMOVE_ALL = 'Fjern kun direkte treff',
 }
 
 interface ItemSearchable extends Item {
@@ -75,6 +75,7 @@ export class FhiTreeViewSelectionComponent implements OnInit, OnChanges {
   $searchTerm = new BehaviorSubject<string>('');
   resultListHeight = 'auto';
   resultListMaxHeight!: string;
+  readonly maxItemThreshhold = 4;
 
   constructor(private changeDetector: ChangeDetectorRef) {}
 
@@ -161,6 +162,7 @@ export class FhiTreeViewSelectionComponent implements OnInit, OnChanges {
     const allSelected = this.allItemsChecked(searchedItems);
     this.updateItemsCheckedState(searchedItems, !allSelected);
     this.updateDescendantState(this.items, false);
+    this.itemsChange.emit(this.items as FhiTreeViewSelectionItem[]);
   }
 
   getGenericButtonText(
@@ -195,6 +197,29 @@ export class FhiTreeViewSelectionComponent implements OnInit, OnChanges {
     const searchedItems = this.getFilteredItemsTest(items);
     const allSelected = this.allItemsChecked(searchedItems);
     return allSelected ? SpecificButtonText.REMOVE_ALL : SpecificButtonText.SELECT_ALL;
+  }
+
+  itemHasHierarchy(items: Item[]): boolean {
+    return items.some((item) => item.children?.length > 0);
+  }
+
+  hasManyItems(items: Item[]): boolean {
+    let count = 0;
+    const stack: Item[] = [...items];
+
+    while (stack.length) {
+      const node = stack.pop()!;
+      count++;
+      if (count >= this.maxItemThreshhold) return true;
+
+      if (node.children?.length) {
+        for (const child of node.children) {
+          stack.push(child);
+        }
+      }
+    }
+
+    return false;
   }
 
   private checkAll(items: Item[]) {
