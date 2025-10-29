e92264968722a8bd7760cf47e27233e4e566ca97
HenrikFalla <henrik.falla@fhi.no>
2025-10-29 14:25:53 +0100

    Update 5.dodsfall-hjerte-og-kar-etter-fylke.ts
    - Updated the example data to use dataPoinId

--- src/app/views/shared/dynamic-library-examples/example-components/highcharts/mock-data/5.dodsfall-hjerte-og-kar-etter-fylke.ts
+++ src/app/views/shared/dynamic-library-examples/example-components/highcharts/mock-data/5.dodsfall-hjerte-og-kar-etter-fylke.ts
@@ -5,48 +5,79 @@ export const DodsfallHjerteOgKarEtterFylke = [
       {
         name: 'Agder',
         y: ':',
+        dataPointId: '42',
         // "y": 1698
       },
       {
         name: 'Innlandet',
         y: 3360,
+        dataPointId: '34'
       },
       {
         name: 'Møre og Romsdal',
         y: 1909,
+        dataPointId: '15'
       },
       {
         name: 'Nordland',
         y: 1687,
+        dataPointId: '18'
       },
       {
         name: 'Oslo',
         y: '.',
+        dataPointId: '03',
         // "y": 2814
       },
       {
         name: 'Rogaland',
         y: 2143,
+        dataPointId: '11',
       },
       {
-        name: 'Troms og Finnmark',
+        name: 'Troms',
         y: 1566,
+        dataPointId: '55',
+      },
+      {
+        name: 'Finnmark',
+        y: 824,
+        dataPointId: '56',
       },
       {
         name: 'Trøndelag',
         y: 2839,
+        dataPointId: '50',
       },
       {
-        name: 'Vestfold og Telemark',
+        name: 'Vestfold',
         y: 3060,
+        dataPointId: '39',
+      },
+      {
+        name: 'Telemark',
+        y: 3060,
+        dataPointId: '40',
       },
       {
         name: 'Vestland',
         y: 3625,
+        dataPointId: '46',
       },
       {
-        name: 'Viken',
+        name: 'Akershus',
         y: 6738,
+        dataPointId: '32',
+      },
+      {
+        name: 'Østfold',
+        y: 2584,
+        dataPointId: '31',
+      },
+      {
+        name: 'Buskerud',
+        y: 1759,
+        dataPointId: '33',
       },
     ],
   },
