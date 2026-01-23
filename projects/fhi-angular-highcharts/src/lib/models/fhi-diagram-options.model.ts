f8fed0fef9d4c61310c49f91bfb4c02c2f04068d
HenrikFalla <henrik.falla@fhi.no>
2026-01-23 13:44:25 +0100

    Added customRequirements to the interface

--- projects/fhi-angular-highcharts/src/lib/models/fhi-diagram-options.model.ts
+++ projects/fhi-angular-highcharts/src/lib/models/fhi-diagram-options.model.ts
@@ -5,6 +5,7 @@ import { FhiDiagramUnit } from './fhi-diagram-unit.model';
 import { FhiDiagramControls } from './fhi-diagram-controls.model';
 import { FhiDiagramFooter } from './fhi-diagram-footer.model';
 import { FhiDiagramCategoryAxis } from './fhi-diagram-category-axis.model';
+import { FhiDiagramRequirements } from './fhi-diagram-requirements.model';
 
 export type FhiDiagramTypeIds = keyof typeof DiagramTypeIds;
 export type FhiTableOrientations = keyof typeof TableOrientations;
@@ -20,4 +21,5 @@ export interface FhiDiagramOptions {
   tableOrientation?: FhiTableOrientations;
   title: string;
   units?: FhiDiagramUnit[];
+  customRequirements?: FhiDiagramRequirements[];
 }
