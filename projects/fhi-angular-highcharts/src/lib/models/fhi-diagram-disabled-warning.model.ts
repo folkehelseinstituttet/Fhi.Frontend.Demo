57d1a87e1af9d9ce0defdb3684b802045848d68e
HenrikFalla <henrik.falla@fhi.no>
2026-02-03 13:12:01 +0100

    Update fhi-diagram-disabled-warning.model.ts
    JSDoc for interface

--- projects/fhi-angular-highcharts/src/lib/models/fhi-diagram-disabled-warning.model.ts
+++ projects/fhi-angular-highcharts/src/lib/models/fhi-diagram-disabled-warning.model.ts
@@ -1,7 +1,16 @@
-import { FhiDiagramRequirements } from "./fhi-diagram-requirements.model";
+import { FhiDiagramRequirements } from './fhi-diagram-requirements.model';
 
 export interface FhiDiagramDisabledWarning {
-    title?: string;
-    showRequirements?: boolean;
-    customRequirements?: FhiDiagramRequirements[];
+  /** The title of the disabled warning
+   * If no provided, no title will be displayed
+   */
+  title?: string;
+  /** Whether to show the auto generated requirements
+   * @default false
+   */
+  showRequirements?: boolean;
+  /** Custom requirements
+   * These will be displayed above the auto generated requirements, or generic warning message depending on showRequirements
+   */
+  customRequirements?: FhiDiagramRequirements[];
 }
