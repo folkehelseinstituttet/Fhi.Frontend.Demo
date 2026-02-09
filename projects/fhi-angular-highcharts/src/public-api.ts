a6b19d7a78080824923371a1a6bfe0ccda6dd292
HenrikFalla <henrik.falla@fhi.no>
2026-02-09 14:50:39 +0100

    Update public-api.ts

--- projects/fhi-angular-highcharts/src/public-api.ts
+++ projects/fhi-angular-highcharts/src/public-api.ts
@@ -10,5 +10,11 @@ export * from './lib/models/fhi-diagram-options.model';
 export * from './lib/models/fhi-diagram-serie.model';
 export * from './lib/models/fhi-diagram-serie-data.model';
 export * from './lib/models/fhi-diagram-unit.model';
+export * from './lib/constants-and-enums/diagram-type-ids';
+export * from './lib/constants-and-enums/diagram-type-groups';
+export * from './lib/constants-and-enums/diagram-serie-name-seperator';
+export * from './lib/constants-and-enums/diagram-type-nav-ids';
+export * from './lib/constants-and-enums/table-orientations';
+export * from './lib/constants-and-enums/fhi-diagram-types';
 
 export { FhiDiagramTypes } from './lib/constants-and-enums/fhi-diagram-types';
