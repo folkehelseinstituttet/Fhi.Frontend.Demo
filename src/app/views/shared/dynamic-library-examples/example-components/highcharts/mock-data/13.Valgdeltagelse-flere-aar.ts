db590b1138f0b87a71ab76ba37c9e0503eb82a34
HenrikFalla <henrik.falla@fhi.no>
2026-04-08 09:52:48 +0200

    Updated example data

--- src/app/views/shared/dynamic-library-examples/example-components/highcharts/mock-data/13.Valgdeltagelse-flere-aar.ts
+++ src/app/views/shared/dynamic-library-examples/example-components/highcharts/mock-data/13.Valgdeltagelse-flere-aar.ts
@@ -1,7 +1,7 @@
 export const ValgdeltagelseFlereAar = [
   {
     name: 'Valgdeltagelse flere år',
-    availableYears: ['2013', '2014'],
+    availableYears: ['2013', '2014', '2015', '2016'],
     electionTypes: ['Stortingsvalg', 'Kommunestyrevalg'],
     data: [
       {
@@ -179,6 +179,350 @@ export const ValgdeltagelseFlereAar = [
       {
         name: '2014',
         dataPointId: '2014_2014',
+        data: [
+          {
+            name: 'Stortingsvalg',
+            dataPointId: 'stortingsvalg',
+            data: [
+              {
+                name: 'Oslo (Fylke)',
+                y: 79.9,
+                dataPointId: '03',
+              },
+              {
+                name: 'Agder',
+                y: 77.0,
+                dataPointId: '42',
+                // "y": 1698
+              },
+              {
+                name: 'Innlandet',
+                y: 75.4,
+                dataPointId: '34',
+              },
+              {
+                name: 'Møre og Romsdal',
+                y: 76.1,
+                dataPointId: '15',
+              },
+              {
+                name: 'Nordland',
+                y: 72.5,
+                dataPointId: '18',
+              },
+
+              {
+                name: 'Rogaland',
+                y: 78.1,
+                dataPointId: '11',
+              },
+              {
+                name: 'Troms',
+                y: 74.5,
+                dataPointId: '55',
+              },
+              {
+                name: 'Finnmark',
+                y: 70.6,
+                dataPointId: '56',
+              },
+              {
+                name: 'Trøndelag',
+                y: 78.4,
+                dataPointId: '50',
+              },
+              {
+                name: 'Vestfold',
+                y: 78.6,
+                dataPointId: '39',
+              },
+              {
+                name: 'Telemark',
+                y: 75.8,
+                dataPointId: '40',
+              },
+              {
+                name: 'Vestland',
+                y: 78.0,
+                dataPointId: '46',
+              },
+              {
+                name: 'Akershus',
+                y: 79.9,
+                dataPointId: '32',
+              },
+              {
+                name: 'Østfold',
+                y: 77.7,
+                dataPointId: '31',
+              },
+              {
+                name: 'Buskerud',
+                y: 78.5,
+                dataPointId: '33',
+              },
+            ],
+          },
+          {
+            name: 'Kommunestyrevalg',
+            dataPointId: 'kommunestyrevalg',
+            data: [
+              {
+                name: 'Oslo (Fylke)',
+                y: '..', // %
+                dataPointId: '03',
+              },
+              {
+                name: 'Agder',
+                y: '..',
+                dataPointId: '42',
+                // "y": 1698
+              },
+              {
+                name: 'Innlandet',
+                y: '..',
+                dataPointId: '34',
+              },
+              {
+                name: 'Møre og Romsdal',
+                y: '..',
+                dataPointId: '15',
+              },
+              {
+                name: 'Nordland',
+                y: '..',
+                dataPointId: '18',
+              },
+
+              {
+                name: 'Rogaland',
+                y: '..',
+                dataPointId: '11',
+              },
+              {
+                name: 'Troms',
+                y: '..',
+                dataPointId: '55',
+              },
+              {
+                name: 'Finnmark',
+                y: '..',
+                dataPointId: '56',
+              },
+              {
+                name: 'Trøndelag',
+                y: '..',
+                dataPointId: '50',
+              },
+              {
+                name: 'Vestfold',
+                y: '..',
+                dataPointId: '39',
+              },
+              {
+                name: 'Telemark',
+                y: '..',
+                dataPointId: '40',
+              },
+              {
+                name: 'Vestland',
+                y: '..',
+                dataPointId: '46',
+              },
+              {
+                name: 'Akershus',
+                y: '..',
+                dataPointId: '32',
+              },
+              {
+                name: 'Østfold',
+                y: '..',
+                dataPointId: '31',
+              },
+              {
+                name: 'Buskerud',
+                y: '..',
+                dataPointId: '33',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: '2015',
+        dataPointId: '2015_2015',
+        data: [
+          {
+            name: 'Stortingsvalg',
+            dataPointId: 'stortingsvalg',
+            data: [
+              {
+                name: 'Oslo (Fylke)',
+                y: '..',
+                dataPointId: '03',
+              },
+              {
+                name: 'Agder',
+                y: '..',
+                dataPointId: '42',
+                // "y": 1698
+              },
+              {
+                name: 'Innlandet',
+                y: '..',
+                dataPointId: '34',
+              },
+              {
+                name: 'Møre og Romsdal',
+                y: '..',
+                dataPointId: '15',
+              },
+              {
+                name: 'Nordland',
+                y: '..',
+                dataPointId: '18',
+              },
+
+              {
+                name: 'Rogaland',
+                y: '..',
+                dataPointId: '11',
+              },
+              {
+                name: 'Troms',
+                y: '..',
+                dataPointId: '55',
+              },
+              {
+                name: 'Finnmark',
+                y: '..',
+                dataPointId: '56',
+              },
+              {
+                name: 'Trøndelag',
+                y: '..',
+                dataPointId: '50',
+              },
+              {
+                name: 'Vestfold',
+                y: '..',
+                dataPointId: '39',
+              },
+              {
+                name: 'Telemark',
+                y: '..',
+                dataPointId: '40',
+              },
+              {
+                name: 'Vestland',
+                y: '..',
+                dataPointId: '46',
+              },
+              {
+                name: 'Akershus',
+                y: '..',
+                dataPointId: '32',
+              },
+              {
+                name: 'Østfold',
+                y: '..',
+                dataPointId: '31',
+              },
+              {
+                name: 'Buskerud',
+                y: '..',
+                dataPointId: '33',
+              },
+            ],
+          },
+          {
+            name: 'Kommunestyrevalg',
+            dataPointId: 'kommunestyrevalg',
+            data: [
+              {
+                name: 'Oslo (Fylke)',
+                y: 80.2, // %
+                dataPointId: '03',
+              },
+              {
+                name: 'Agder',
+                y: 77.5,
+                dataPointId: '42',
+                // "y": 1698
+              },
+              {
+                name: 'Innlandet',
+                y: 76.1,
+                dataPointId: '34',
+              },
+              {
+                name: 'Møre og Romsdal',
+                y: 76.7,
+                dataPointId: '15',
+              },
+              {
+                name: 'Nordland',
+                y: 74.6,
+                dataPointId: '18',
+              },
+
+              {
+                name: 'Rogaland',
+                y: 78.1,
+                dataPointId: '11',
+              },
+              {
+                name: 'Troms',
+                y: 74.8,
+                dataPointId: '55',
+              },
+              {
+                name: 'Finnmark',
+                y: 71.3,
+                dataPointId: '56',
+              },
+              {
+                name: 'Trøndelag',
+                y: 78.3,
+                dataPointId: '50',
+              },
+              {
+                name: 'Vestfold',
+                y: 78.4,
+                dataPointId: '39',
+              },
+              {
+                name: 'Telemark',
+                y: 75.9,
+                dataPointId: '40',
+              },
+              {
+                name: 'Vestland',
+                y: 78.1,
+                dataPointId: '46',
+              },
+              {
+                name: 'Akershus',
+                y: 80.2,
+                dataPointId: '32',
+              },
+              {
+                name: 'Østfold',
+                y: 77.7,
+                dataPointId: '31',
+              },
+              {
+                name: 'Buskerud',
+                y: 78.5,
+                dataPointId: '33',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        name: '2016',
+        dataPointId: '2016_2016',
         data: [
           {
             name: 'Stortingsvalg',
